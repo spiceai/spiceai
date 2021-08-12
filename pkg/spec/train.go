71cd1a3d268a9b61e18fa3964a05dfa2d6c04aa9
Luke Kim <80174+lukekim@users.noreply.github.com>
2021-08-12 17:02:52 +0900

    Fix training goal not being passed to AI Engine (#256)

--- pkg/spec/train.go
+++ pkg/spec/train.go
@@ -4,5 +4,5 @@ type TrainSpec struct {
 	EpochTime int64  `json:"epoch_time,omitempty"`
 	Episodes  int    `json:"number_episodes"`
 	FlightId  string `json:"flight"`
-	Goal      string `json:"goal,omitempty"`
+	Goal      string `json:"training_goal,omitempty"`
 }
