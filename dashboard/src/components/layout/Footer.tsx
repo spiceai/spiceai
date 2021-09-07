aa5f3267a1ed59faa1ec0292719c868792dda778
Luke Kim <80174+lukekim@users.noreply.github.com>
2021-09-07 15:35:34 +0900

    Make versions compile time constants (#158)
    * Make versions compile time constants
    Closes #156
    
    * Add test

--- dashboard/src/components/layout/Footer.tsx
+++ dashboard/src/components/layout/Footer.tsx
@@ -1,11 +1,10 @@
 import React from 'react';
-
-const appVersion = process.env.REACT_APP_SPICE_VERSION
+import { SPICE_VERSION } from '../../constants'
 
 const Footer: React.FunctionComponent = () => {
   return (
     <footer className="flex flex-row gap-2 text-sm text-footer m-2 bg-primary-dark text-right text-xs">
-      <div>v{appVersion}</div>
+      <div>v{SPICE_VERSION}</div>
       <div className="flex-grow"></div>
       <div>
         <a href="/api/v0.1/acknowledgements">Acknowledgements</a>
---
+++ dashboard/src/constants.ts
@@ -0,0 +1,2 @@
+export const SPICE_VERSION = '0.1.0-alpha-rc'
+export const SPICE_API_VERSION = 'v0.1'
