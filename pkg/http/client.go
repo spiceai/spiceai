07d1e4fc25434ea7d2811c1e8431c097487c088d
Luke Kim <80174+lukekim@users.noreply.github.com>
2021-09-09 18:35:13 +0900

    Set User-Agent on HTTP client calls (#183)
    * Set User-Agent on HTTP client calls
    
    * Update runtime.go

---
+++ pkg/http/client.go
@@ -0,0 +1,34 @@
+package http
+
+import (
+	"fmt"
+	net_http "net/http"
+	"runtime"
+
+	"github.com/spiceai/spiceai/pkg/version"
+)
+
+var _userAgent string
+
+func Get(url string) (*net_http.Response, error) {
+	req, err := net_http.NewRequest("GET", url, nil)
+	if err != nil {
+		return nil, err
+	}
+	
+	req.Header.Set("User-Agent", userAgent())
+
+	resp, err := net_http.DefaultClient.Do(req)
+	if err != nil {
+		return nil, err
+	}
+
+	return resp, nil
+}
+
+func userAgent() string {
+	if _userAgent == "" {
+		_userAgent = fmt.Sprintf("Spice.ai/%s %s/%s (%s)", version.Version(), version.Component(), version.Version(), runtime.GOOS)
+	}
+	return _userAgent
+}
