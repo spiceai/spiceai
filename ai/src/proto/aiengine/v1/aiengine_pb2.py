00572fc711d41e915730d575be359674a9574cca
Luke Kim <80174+lukekim@users.noreply.github.com>
2021-09-14 05:07:41 +0000

    WIP

--- ai/src/proto/aiengine/v1/aiengine_pb2.py
+++ ai/src/proto/aiengine/v1/aiengine_pb2.py
@@ -20,7 +20,7 @@ DESCRIPTOR = _descriptor.FileDescriptor(
   syntax='proto3',
   serialized_options=b'Z0github.com/spiceai/spiceai/pkg/proto/aiengine_pb',
   create_key=_descriptor._internal_create_key,
-  serialized_pb=b'\n proto/aiengine/v1/aiengine.proto\x12\x08\x61iengine\x1a\x1cproto/common/v1/common.proto\"\x81\x01\n\rDataConnector\x12\x0c\n\x04name\x18\x01 \x01(\t\x12\x33\n\x06params\x18\x02 \x03(\x0b\x32#.aiengine.DataConnector.ParamsEntry\x1a-\n\x0bParamsEntry\x12\x0b\n\x03key\x18\x01 \x01(\t\x12\r\n\x05value\x18\x02 \x01(\t:\x02\x38\x01\"\x9c\x01\n\nDataSource\x12*\n\tconnector\x18\x01 \x01(\x0b\x32\x17.aiengine.DataConnector\x12\x32\n\x07\x61\x63tions\x18\x02 \x03(\x0b\x32!.aiengine.DataSource.ActionsEntry\x1a.\n\x0c\x41\x63tionsEntry\x12\x0b\n\x03key\x18\x01 \x01(\t\x12\r\n\x05value\x18\x02 \x01(\t:\x02\x38\x01\"\xda\x03\n\x0bInitRequest\x12\x0b\n\x03pod\x18\x01 \x01(\t\x12\x0e\n\x06period\x18\x02 \x01(\x03\x12\x10\n\x08interval\x18\x03 \x01(\x03\x12\x13\n\x0bgranularity\x18\x04 \x01(\x03\x12\x12\n\nepoch_time\x18\x05 \x01(\x03\x12\x33\n\x07\x61\x63tions\x18\x06 \x03(\x0b\x32\".aiengine.InitRequest.ActionsEntry\x12>\n\ractions_order\x18\x07 \x03(\x0b\x32\'.aiengine.InitRequest.ActionsOrderEntry\x12\x31\n\x06\x66ields\x18\x08 \x03(\x0b\x32!.aiengine.InitRequest.FieldsEntry\x12\x0c\n\x04laws\x18\t \x03(\t\x12)\n\x0b\x64\x61tasources\x18\n \x03(\x0b\x32\x14.aiengine.DataSource\x1a.\n\x0c\x41\x63tionsEntry\x12\x0b\n\x03key\x18\x01 \x01(\t\x12\r\n\x05value\x18\x02 \x01(\t:\x02\x38\x01\x1a\x33\n\x11\x41\x63tionsOrderEntry\x12\x0b\n\x03key\x18\x01 \x01(\t\x12\r\n\x05value\x18\x02 \x01(\x05:\x02\x38\x01\x1a-\n\x0b\x46ieldsEntry\x12\x0b\n\x03key\x18\x01 \x01(\t\x12\r\n\x05value\x18\x02 \x01(\x01:\x02\x38\x01\":\n\x08Response\x12\x0e\n\x06result\x18\x01 \x01(\t\x12\x0f\n\x07message\x18\x02 \x01(\t\x12\r\n\x05\x65rror\x18\x03 \x01(\x08\"M\n\x11\x45xportModelResult\x12$\n\x08response\x18\x01 \x01(\x0b\x32\x12.aiengine.Response\x12\x12\n\nmodel_path\x18\x02 \x01(\t\"w\n\x14StartTrainingRequest\x12\x0b\n\x03pod\x18\x01 \x01(\t\x12\x17\n\x0fnumber_episodes\x18\x02 \x01(\x03\x12\x0e\n\x06\x66light\x18\x03 \x01(\t\x12\x15\n\rtraining_goal\x18\x04 \x01(\t\x12\x12\n\nepoch_time\x18\x05 \x01(\x03\",\n\x10InferenceRequest\x12\x0b\n\x03pod\x18\x01 \x01(\t\x12\x0b\n\x03tag\x18\x02 \x01(\t\"\x84\x01\n\x0fInferenceResult\x12$\n\x08response\x18\x01 \x01(\x0b\x32\x12.aiengine.Response\x12\r\n\x05start\x18\x02 \x01(\x03\x12\x0b\n\x03\x65nd\x18\x03 \x01(\x03\x12\x0e\n\x06\x61\x63tion\x18\x04 \x01(\t\x12\x12\n\nconfidence\x18\x05 \x01(\x02\x12\x0b\n\x03tag\x18\x06 \x01(\t\"/\n\x0e\x41\x64\x64\x44\x61taRequest\x12\x0b\n\x03pod\x18\x01 \x01(\t\x12\x10\n\x08\x63sv_data\x18\x02 \x01(\t\"Y\n\x19\x41\x64\x64InterpretationsRequest\x12\x0b\n\x03pod\x18\x01 \x01(\t\x12/\n\x0finterpretations\x18\x02 \x03(\x0b\x32\x16.common.Interpretation\"\x0f\n\rHealthRequest\".\n\x12\x45xportModelRequest\x12\x0b\n\x03pod\x18\x01 \x01(\t\x12\x0b\n\x03tag\x18\x02 \x01(\t\"C\n\x12ImportModelRequest\x12\x0b\n\x03pod\x18\x01 \x01(\t\x12\x0b\n\x03tag\x18\x02 \x01(\t\x12\x13\n\x0bimport_path\x18\x03 \x01(\t2\x96\x04\n\x08\x41IEngine\x12\x31\n\x04Init\x12\x15.aiengine.InitRequest\x1a\x12.aiengine.Response\x12\x37\n\x07\x41\x64\x64\x44\x61ta\x12\x18.aiengine.AddDataRequest\x1a\x12.aiengine.Response\x12M\n\x12\x41\x64\x64Interpretations\x12#.aiengine.AddInterpretationsRequest\x1a\x12.aiengine.Response\x12\x43\n\rStartTraining\x12\x1e.aiengine.StartTrainingRequest\x1a\x12.aiengine.Response\x12\x45\n\x0cGetInference\x12\x1a.aiengine.InferenceRequest\x1a\x19.aiengine.InferenceResult\x12\x38\n\tGetHealth\x12\x17.aiengine.HealthRequest\x1a\x12.aiengine.Response\x12H\n\x0b\x45xportModel\x12\x1c.aiengine.ExportModelRequest\x1a\x1b.aiengine.ExportModelResult\x12?\n\x0bImportModel\x12\x1c.aiengine.ImportModelRequest\x1a\x12.aiengine.ResponseB2Z0github.com/spiceai/spiceai/pkg/proto/aiengine_pbb\x06proto3'
+  serialized_pb=b'\n proto/aiengine/v1/aiengine.proto\x12\x08\x61iengine\x1a\x1cproto/common/v1/common.proto\"\x81\x01\n\rDataConnector\x12\x0c\n\x04name\x18\x01 \x01(\t\x12\x33\n\x06params\x18\x02 \x03(\x0b\x32#.aiengine.DataConnector.ParamsEntry\x1a-\n\x0bParamsEntry\x12\x0b\n\x03key\x18\x01 \x01(\t\x12\r\n\x05value\x18\x02 \x01(\t:\x02\x38\x01\"\x9c\x01\n\nDataSource\x12*\n\tconnector\x18\x01 \x01(\x0b\x32\x17.aiengine.DataConnector\x12\x32\n\x07\x61\x63tions\x18\x02 \x03(\x0b\x32!.aiengine.DataSource.ActionsEntry\x1a.\n\x0c\x41\x63tionsEntry\x12\x0b\n\x03key\x18\x01 \x01(\t\x12\r\n\x05value\x18\x02 \x01(\t:\x02\x38\x01\"\xda\x03\n\x0bInitRequest\x12\x0b\n\x03pod\x18\x01 \x01(\t\x12\x0e\n\x06period\x18\x02 \x01(\x03\x12\x10\n\x08interval\x18\x03 \x01(\x03\x12\x13\n\x0bgranularity\x18\x04 \x01(\x03\x12\x12\n\nepoch_time\x18\x05 \x01(\x03\x12\x33\n\x07\x61\x63tions\x18\x06 \x03(\x0b\x32\".aiengine.InitRequest.ActionsEntry\x12>\n\ractions_order\x18\x07 \x03(\x0b\x32\'.aiengine.InitRequest.ActionsOrderEntry\x12\x31\n\x06\x66ields\x18\x08 \x03(\x0b\x32!.aiengine.InitRequest.FieldsEntry\x12\x0c\n\x04laws\x18\t \x03(\t\x12)\n\x0b\x64\x61tasources\x18\n \x03(\x0b\x32\x14.aiengine.DataSource\x1a.\n\x0c\x41\x63tionsEntry\x12\x0b\n\x03key\x18\x01 \x01(\t\x12\r\n\x05value\x18\x02 \x01(\t:\x02\x38\x01\x1a\x33\n\x11\x41\x63tionsOrderEntry\x12\x0b\n\x03key\x18\x01 \x01(\t\x12\r\n\x05value\x18\x02 \x01(\x05:\x02\x38\x01\x1a-\n\x0b\x46ieldsEntry\x12\x0b\n\x03key\x18\x01 \x01(\t\x12\r\n\x05value\x18\x02 \x01(\x01:\x02\x38\x01\":\n\x08Response\x12\x0e\n\x06result\x18\x01 \x01(\t\x12\x0f\n\x07message\x18\x02 \x01(\t\x12\r\n\x05\x65rror\x18\x03 \x01(\x08\"M\n\x11\x45xportModelResult\x12$\n\x08response\x18\x01 \x01(\x0b\x32\x12.aiengine.Response\x12\x12\n\nmodel_path\x18\x02 \x01(\t\"w\n\x14StartTrainingRequest\x12\x0b\n\x03pod\x18\x01 \x01(\t\x12\x17\n\x0fnumber_episodes\x18\x02 \x01(\x03\x12\x0e\n\x06\x66light\x18\x03 \x01(\t\x12\x15\n\rtraining_goal\x18\x04 \x01(\t\x12\x12\n\nepoch_time\x18\x05 \x01(\x03\",\n\x10InferenceRequest\x12\x0b\n\x03pod\x18\x01 \x01(\t\x12\x0b\n\x03tag\x18\x02 \x01(\t\"\x84\x01\n\x0fInferenceResult\x12$\n\x08response\x18\x01 \x01(\x0b\x32\x12.aiengine.Response\x12\r\n\x05start\x18\x02 \x01(\x03\x12\x0b\n\x03\x65nd\x18\x03 \x01(\x03\x12\x0e\n\x06\x61\x63tion\x18\x04 \x01(\t\x12\x12\n\nconfidence\x18\x05 \x01(\x02\x12\x0b\n\x03tag\x18\x06 \x01(\t\"/\n\x0e\x41\x64\x64\x44\x61taRequest\x12\x0b\n\x03pod\x18\x01 \x01(\t\x12\x10\n\x08\x63sv_data\x18\x02 \x01(\t\"i\n\x19\x41\x64\x64InterpretationsRequest\x12\x0b\n\x03pod\x18\x01 \x01(\t\x12?\n\x17indexed_interpretations\x18\x02 \x01(\x0b\x32\x1e.common.IndexedInterpretations\"\x0f\n\rHealthRequest\".\n\x12\x45xportModelRequest\x12\x0b\n\x03pod\x18\x01 \x01(\t\x12\x0b\n\x03tag\x18\x02 \x01(\t\"C\n\x12ImportModelRequest\x12\x0b\n\x03pod\x18\x01 \x01(\t\x12\x0b\n\x03tag\x18\x02 \x01(\t\x12\x13\n\x0bimport_path\x18\x03 \x01(\t2\x96\x04\n\x08\x41IEngine\x12\x31\n\x04Init\x12\x15.aiengine.InitRequest\x1a\x12.aiengine.Response\x12\x37\n\x07\x41\x64\x64\x44\x61ta\x12\x18.aiengine.AddDataRequest\x1a\x12.aiengine.Response\x12M\n\x12\x41\x64\x64Interpretations\x12#.aiengine.AddInterpretationsRequest\x1a\x12.aiengine.Response\x12\x43\n\rStartTraining\x12\x1e.aiengine.StartTrainingRequest\x1a\x12.aiengine.Response\x12\x45\n\x0cGetInference\x12\x1a.aiengine.InferenceRequest\x1a\x19.aiengine.InferenceResult\x12\x38\n\tGetHealth\x12\x17.aiengine.HealthRequest\x1a\x12.aiengine.Response\x12H\n\x0b\x45xportModel\x12\x1c.aiengine.ExportModelRequest\x1a\x1b.aiengine.ExportModelResult\x12?\n\x0bImportModel\x12\x1c.aiengine.ImportModelRequest\x1a\x12.aiengine.ResponseB2Z0github.com/spiceai/spiceai/pkg/proto/aiengine_pbb\x06proto3'
   ,
   dependencies=[proto_dot_common_dot_v1_dot_common__pb2.DESCRIPTOR,])
 
@@ -696,9 +696,9 @@ _ADDINTERPRETATIONSREQUEST = _descriptor.Descriptor(
       is_extension=False, extension_scope=None,
       serialized_options=None, file=DESCRIPTOR,  create_key=_descriptor._internal_create_key),
     _descriptor.FieldDescriptor(
-      name='interpretations', full_name='aiengine.AddInterpretationsRequest.interpretations', index=1,
-      number=2, type=11, cpp_type=10, label=3,
-      has_default_value=False, default_value=[],
+      name='indexed_interpretations', full_name='aiengine.AddInterpretationsRequest.indexed_interpretations', index=1,
+      number=2, type=11, cpp_type=10, label=1,
+      has_default_value=False, default_value=None,
       message_type=None, enum_type=None, containing_type=None,
       is_extension=False, extension_scope=None,
       serialized_options=None, file=DESCRIPTOR,  create_key=_descriptor._internal_create_key),
@@ -715,7 +715,7 @@ _ADDINTERPRETATIONSREQUEST = _descriptor.Descriptor(
   oneofs=[
   ],
   serialized_start=1334,
-  serialized_end=1423,
+  serialized_end=1439,
 )
 
 
@@ -739,8 +739,8 @@ _HEALTHREQUEST = _descriptor.Descriptor(
   extension_ranges=[],
   oneofs=[
   ],
-  serialized_start=1425,
-  serialized_end=1440,
+  serialized_start=1441,
+  serialized_end=1456,
 )
 
 
@@ -778,8 +778,8 @@ _EXPORTMODELREQUEST = _descriptor.Descriptor(
   extension_ranges=[],
   oneofs=[
   ],
-  serialized_start=1442,
-  serialized_end=1488,
+  serialized_start=1458,
+  serialized_end=1504,
 )
 
 
@@ -824,8 +824,8 @@ _IMPORTMODELREQUEST = _descriptor.Descriptor(
   extension_ranges=[],
   oneofs=[
   ],
-  serialized_start=1490,
-  serialized_end=1557,
+  serialized_start=1506,
+  serialized_end=1573,
 )
 
 _DATACONNECTOR_PARAMSENTRY.containing_type = _DATACONNECTOR
@@ -842,7 +842,7 @@ _INITREQUEST.fields_by_name['fields'].message_type = _INITREQUEST_FIELDSENTRY
 _INITREQUEST.fields_by_name['datasources'].message_type = _DATASOURCE
 _EXPORTMODELRESULT.fields_by_name['response'].message_type = _RESPONSE
 _INFERENCERESULT.fields_by_name['response'].message_type = _RESPONSE
-_ADDINTERPRETATIONSREQUEST.fields_by_name['interpretations'].message_type = proto_dot_common_dot_v1_dot_common__pb2._INTERPRETATION
+_ADDINTERPRETATIONSREQUEST.fields_by_name['indexed_interpretations'].message_type = proto_dot_common_dot_v1_dot_common__pb2._INDEXEDINTERPRETATIONS
 DESCRIPTOR.message_types_by_name['DataConnector'] = _DATACONNECTOR
 DESCRIPTOR.message_types_by_name['DataSource'] = _DATASOURCE
 DESCRIPTOR.message_types_by_name['InitRequest'] = _INITREQUEST
@@ -1004,8 +1004,8 @@ _AIENGINE = _descriptor.ServiceDescriptor(
   index=0,
   serialized_options=None,
   create_key=_descriptor._internal_create_key,
-  serialized_start=1560,
-  serialized_end=2094,
+  serialized_start=1576,
+  serialized_end=2110,
   methods=[
   _descriptor.MethodDescriptor(
     name='Init',
--- ai/src/proto/common/v1/common_pb2.py
+++ ai/src/proto/common/v1/common_pb2.py
@@ -19,7 +19,7 @@ DESCRIPTOR = _descriptor.FileDescriptor(
   syntax='proto3',
   serialized_options=b'Z.github.com/spiceai/spiceai/pkg/proto/common_pb',
   create_key=_descriptor._internal_create_key,
-  serialized_pb=b'\n\x1cproto/common/v1/common.proto\x12\x06\x63ommon\"Y\n\x0eInterpretation\x12\r\n\x05start\x18\x01 \x01(\x03\x12\x0b\n\x03\x65nd\x18\x02 \x01(\x03\x12\x0c\n\x04name\x18\x03 \x01(\t\x12\x0f\n\x07\x61\x63tions\x18\x04 \x03(\t\x12\x0c\n\x04tags\x18\x05 \x03(\tB0Z.github.com/spiceai/spiceai/pkg/proto/common_pbb\x06proto3'
+  serialized_pb=b'\n\x1cproto/common/v1/common.proto\x12\x06\x63ommon\"Y\n\x0eInterpretation\x12\r\n\x05start\x18\x01 \x01(\x04\x12\x0b\n\x03\x65nd\x18\x02 \x01(\x04\x12\x0c\n\x04name\x18\x03 \x01(\t\x12\x0f\n\x07\x61\x63tions\x18\x04 \x03(\t\x12\x0c\n\x04tags\x18\x05 \x03(\t\"-\n\x15InterpretationIndices\x12\x14\n\x08indicies\x18\x01 \x03(\rB\x02\x10\x01\"\xd0\x01\n\x16IndexedInterpretations\x12/\n\x0finterpretations\x18\x01 \x03(\x0b\x32\x16.common.Interpretation\x12\x38\n\x05index\x18\x02 \x03(\x0b\x32).common.IndexedInterpretations.IndexEntry\x1aK\n\nIndexEntry\x12\x0b\n\x03key\x18\x01 \x01(\x04\x12,\n\x05value\x18\x02 \x01(\x0b\x32\x1d.common.InterpretationIndices:\x02\x38\x01\x42\x30Z.github.com/spiceai/spiceai/pkg/proto/common_pbb\x06proto3'
 )
 
 
@@ -35,14 +35,14 @@ _INTERPRETATION = _descriptor.Descriptor(
   fields=[
     _descriptor.FieldDescriptor(
       name='start', full_name='common.Interpretation.start', index=0,
-      number=1, type=3, cpp_type=2, label=1,
+      number=1, type=4, cpp_type=4, label=1,
       has_default_value=False, default_value=0,
       message_type=None, enum_type=None, containing_type=None,
       is_extension=False, extension_scope=None,
       serialized_options=None, file=DESCRIPTOR,  create_key=_descriptor._internal_create_key),
     _descriptor.FieldDescriptor(
       name='end', full_name='common.Interpretation.end', index=1,
-      number=2, type=3, cpp_type=2, label=1,
+      number=2, type=4, cpp_type=4, label=1,
       has_default_value=False, default_value=0,
       message_type=None, enum_type=None, containing_type=None,
       is_extension=False, extension_scope=None,
@@ -84,7 +84,122 @@ _INTERPRETATION = _descriptor.Descriptor(
   serialized_end=129,
 )
 
+
+_INTERPRETATIONINDICES = _descriptor.Descriptor(
+  name='InterpretationIndices',
+  full_name='common.InterpretationIndices',
+  filename=None,
+  file=DESCRIPTOR,
+  containing_type=None,
+  create_key=_descriptor._internal_create_key,
+  fields=[
+    _descriptor.FieldDescriptor(
+      name='indicies', full_name='common.InterpretationIndices.indicies', index=0,
+      number=1, type=13, cpp_type=3, label=3,
+      has_default_value=False, default_value=[],
+      message_type=None, enum_type=None, containing_type=None,
+      is_extension=False, extension_scope=None,
+      serialized_options=b'\020\001', file=DESCRIPTOR,  create_key=_descriptor._internal_create_key),
+  ],
+  extensions=[
+  ],
+  nested_types=[],
+  enum_types=[
+  ],
+  serialized_options=None,
+  is_extendable=False,
+  syntax='proto3',
+  extension_ranges=[],
+  oneofs=[
+  ],
+  serialized_start=131,
+  serialized_end=176,
+)
+
+
+_INDEXEDINTERPRETATIONS_INDEXENTRY = _descriptor.Descriptor(
+  name='IndexEntry',
+  full_name='common.IndexedInterpretations.IndexEntry',
+  filename=None,
+  file=DESCRIPTOR,
+  containing_type=None,
+  create_key=_descriptor._internal_create_key,
+  fields=[
+    _descriptor.FieldDescriptor(
+      name='key', full_name='common.IndexedInterpretations.IndexEntry.key', index=0,
+      number=1, type=4, cpp_type=4, label=1,
+      has_default_value=False, default_value=0,
+      message_type=None, enum_type=None, containing_type=None,
+      is_extension=False, extension_scope=None,
+      serialized_options=None, file=DESCRIPTOR,  create_key=_descriptor._internal_create_key),
+    _descriptor.FieldDescriptor(
+      name='value', full_name='common.IndexedInterpretations.IndexEntry.value', index=1,
+      number=2, type=11, cpp_type=10, label=1,
+      has_default_value=False, default_value=None,
+      message_type=None, enum_type=None, containing_type=None,
+      is_extension=False, extension_scope=None,
+      serialized_options=None, file=DESCRIPTOR,  create_key=_descriptor._internal_create_key),
+  ],
+  extensions=[
+  ],
+  nested_types=[],
+  enum_types=[
+  ],
+  serialized_options=b'8\001',
+  is_extendable=False,
+  syntax='proto3',
+  extension_ranges=[],
+  oneofs=[
+  ],
+  serialized_start=312,
+  serialized_end=387,
+)
+
+_INDEXEDINTERPRETATIONS = _descriptor.Descriptor(
+  name='IndexedInterpretations',
+  full_name='common.IndexedInterpretations',
+  filename=None,
+  file=DESCRIPTOR,
+  containing_type=None,
+  create_key=_descriptor._internal_create_key,
+  fields=[
+    _descriptor.FieldDescriptor(
+      name='interpretations', full_name='common.IndexedInterpretations.interpretations', index=0,
+      number=1, type=11, cpp_type=10, label=3,
+      has_default_value=False, default_value=[],
+      message_type=None, enum_type=None, containing_type=None,
+      is_extension=False, extension_scope=None,
+      serialized_options=None, file=DESCRIPTOR,  create_key=_descriptor._internal_create_key),
+    _descriptor.FieldDescriptor(
+      name='index', full_name='common.IndexedInterpretations.index', index=1,
+      number=2, type=11, cpp_type=10, label=3,
+      has_default_value=False, default_value=[],
+      message_type=None, enum_type=None, containing_type=None,
+      is_extension=False, extension_scope=None,
+      serialized_options=None, file=DESCRIPTOR,  create_key=_descriptor._internal_create_key),
+  ],
+  extensions=[
+  ],
+  nested_types=[_INDEXEDINTERPRETATIONS_INDEXENTRY, ],
+  enum_types=[
+  ],
+  serialized_options=None,
+  is_extendable=False,
+  syntax='proto3',
+  extension_ranges=[],
+  oneofs=[
+  ],
+  serialized_start=179,
+  serialized_end=387,
+)
+
+_INDEXEDINTERPRETATIONS_INDEXENTRY.fields_by_name['value'].message_type = _INTERPRETATIONINDICES
+_INDEXEDINTERPRETATIONS_INDEXENTRY.containing_type = _INDEXEDINTERPRETATIONS
+_INDEXEDINTERPRETATIONS.fields_by_name['interpretations'].message_type = _INTERPRETATION
+_INDEXEDINTERPRETATIONS.fields_by_name['index'].message_type = _INDEXEDINTERPRETATIONS_INDEXENTRY
 DESCRIPTOR.message_types_by_name['Interpretation'] = _INTERPRETATION
+DESCRIPTOR.message_types_by_name['InterpretationIndices'] = _INTERPRETATIONINDICES
+DESCRIPTOR.message_types_by_name['IndexedInterpretations'] = _INDEXEDINTERPRETATIONS
 _sym_db.RegisterFileDescriptor(DESCRIPTOR)
 
 Interpretation = _reflection.GeneratedProtocolMessageType('Interpretation', (_message.Message,), {
@@ -94,6 +209,30 @@ Interpretation = _reflection.GeneratedProtocolMessageType('Interpretation', (_me
   })
 _sym_db.RegisterMessage(Interpretation)
 
+InterpretationIndices = _reflection.GeneratedProtocolMessageType('InterpretationIndices', (_message.Message,), {
+  'DESCRIPTOR' : _INTERPRETATIONINDICES,
+  '__module__' : 'proto.common.v1.common_pb2'
+  # @@protoc_insertion_point(class_scope:common.InterpretationIndices)
+  })
+_sym_db.RegisterMessage(InterpretationIndices)
+
+IndexedInterpretations = _reflection.GeneratedProtocolMessageType('IndexedInterpretations', (_message.Message,), {
+
+  'IndexEntry' : _reflection.GeneratedProtocolMessageType('IndexEntry', (_message.Message,), {
+    'DESCRIPTOR' : _INDEXEDINTERPRETATIONS_INDEXENTRY,
+    '__module__' : 'proto.common.v1.common_pb2'
+    # @@protoc_insertion_point(class_scope:common.IndexedInterpretations.IndexEntry)
+    })
+  ,
+  'DESCRIPTOR' : _INDEXEDINTERPRETATIONS,
+  '__module__' : 'proto.common.v1.common_pb2'
+  # @@protoc_insertion_point(class_scope:common.IndexedInterpretations)
+  })
+_sym_db.RegisterMessage(IndexedInterpretations)
+_sym_db.RegisterMessage(IndexedInterpretations.IndexEntry)
+
 
 DESCRIPTOR._options = None
+_INTERPRETATIONINDICES.fields_by_name['indicies']._options = None
+_INDEXEDINTERPRETATIONS_INDEXENTRY._options = None
 # @@protoc_insertion_point(module_scope)
