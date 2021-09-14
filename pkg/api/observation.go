85d12b83111a9cdb5b2f8c48c81f0d31e9701d63
Phillip LeBlanc <phillip@spiceai.io>
2021-09-14 12:16:11 +0900

    Move format logic to data-processor

--- pkg/api/observation.go
+++ pkg/api/observation.go
@@ -2,12 +2,7 @@ package api
 
 import (
 	_ "embed"
-	"encoding/json"
-	"fmt"
-	"time"
 
-	"github.com/spiceai/spiceai/pkg/observations"
-	"github.com/spiceai/spiceai/pkg/state"
 	"github.com/spiceai/spiceai/pkg/util"
 )
 
@@ -27,55 +22,8 @@ type Observation struct {
 	Tags []string           `json:"tags,omitempty"`
 }
 
-type ObservationJsonFormat struct {
-}
-
-func (s *ObservationJsonFormat) GetSchema() *[]byte {
+func GetSchema() *[]byte {
 	return &schema
-}
-
-func (s *ObservationJsonFormat) GetObservations(data *[]byte) ([]observations.Observation, error) {
-	var observationPoints []Observation = make([]Observation, 0)
-
-	err := json.Unmarshal(*data, &observationPoints)
-	if err != nil {
-		return nil, err
-	}
-
-	var newObservations []observations.Observation
-	for _, point := range observationPoints {
-		var ts int64
-		var err error
-
-		if point.Time.Integer != nil {
-			ts = *point.Time.Integer
-		} else if point.Time.String != nil {
-			var t time.Time
-			t, err = time.Parse(time.RFC3339, *point.Time.String)
-			if err != nil {
-				// This should never happen as the schema validation would have caught this
-				return nil, fmt.Errorf("observation time format is invalid: %s", *point.Time.String)
-			}
-			ts = t.Unix()
-		} else {
-			// This should never happen as the schema validation would have caught this
-			return nil, fmt.Errorf("observation did not include a time component")
-		}
-
-		observation := observations.Observation{
-			Time: ts,
-			Data: point.Data,
-		}
-
-		newObservations = append(newObservations, observation)
-	}
-
-	return newObservations, nil
-}
-
-func (s *ObservationJsonFormat) GetState(data []byte, validFields *[]string) ([]*state.State, error) {
-	// TODO
-	return nil, nil
 }
 
 func (x *Time) UnmarshalJSON(data []byte) error {
