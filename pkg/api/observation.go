ef08254d9d4a948e466e262086b2d8d02b686084
Phillip LeBlanc <phillip@spiceai.io>
2021-09-14 12:45:39 +0900

    Adding unit tests and moving packages

--- pkg/api/observation.go
+++ pkg/api/observation.go
@@ -2,8 +2,7 @@ package api
 
 import (
 	_ "embed"
-
-	"github.com/spiceai/spiceai/pkg/util"
+	"time"
 )
 
 var (
@@ -11,29 +10,12 @@ var (
 	jsonSchema []byte
 )
 
-type Time struct {
-	Integer *int64
-	String  *string
-}
-
 type Observation struct {
-	Time *Time              `json:"time"`
+	Time *time.Time         `json:"time"`
 	Data map[string]float64 `json:"data"`
 	Tags []string           `json:"tags,omitempty"`
 }
 
 func JsonSchema() *[]byte {
 	return &jsonSchema
-}
-
-func (x *Time) UnmarshalJSON(data []byte) error {
-	err := util.UnmarshalUnion(data, &x.Integer, &x.String)
-	if err != nil {
-		return err
-	}
-	return nil
-}
-
-func (x *Time) MarshalJSON() ([]byte, error) {
-	return util.MarshalUnion(x.Integer, x.String)
 }
