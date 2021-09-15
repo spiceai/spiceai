5ccf43dff7057043c1ba07ece8227df86fcd3c7a
Luke Kim <80174+lukekim@users.noreply.github.com>
2021-09-15 12:30:45 +0000

    WIP

--- dashboard/src/components/flights/FlightChart.tsx
+++ dashboard/src/components/flights/FlightChart.tsx
@@ -8,6 +8,17 @@ interface FlightChartProps {
   flight: Flight;
 }
 
+const colors = [
+  "#022132",
+  "#1c3155",
+  "#4e3d71",
+  "#88417e",
+  "#bf4578",
+  "#ea5562",
+  "#ff7840",
+  "#ffa600",
+]
+
 const FlightChart: React.FunctionComponent<FlightChartProps> = (props) => {
   const chartRef = useRef() as React.MutableRefObject<HTMLCanvasElement>;
   const chart = useRef<Chart<'line'>>();
@@ -23,6 +34,11 @@ const FlightChart: React.FunctionComponent<FlightChartProps> = (props) => {
         options: {
           responsive: true,
           animation: false,
+          interaction: {
+            mode: 'index',
+            intersect: false,
+          },
+          stacked: false,
           scales: {
             x: {
               title: {
@@ -32,13 +48,37 @@ const FlightChart: React.FunctionComponent<FlightChartProps> = (props) => {
               type: 'category',
             },
             y1: {
+              id: "rewards",
+              display: true,
               title: {
                 display: true,
                 text: 'Rewards',
               },
+              position: "left",
               type: 'linear',
-              grace: '10%',
+              weight: 100,
+              grid: {
+                display: false,
+              },
+              ticks: {
+                display: false,
+              }
             },
+            y2: {
+              id: "actions",
+              title: {
+                display: true,
+                text: "Action Count",
+              },
+              type: "linear",
+              position: "right",
+              grid: {
+                display: false,
+              },
+              ticks: {
+                display: false,
+              }
+            }
           },
           plugins: {
             title: {
@@ -62,22 +102,52 @@ const FlightChart: React.FunctionComponent<FlightChartProps> = (props) => {
             {
               label: 'rewards',
               data: props.flight.episodes as any,
+              backgroundColor: 'rgba(00, 22, 132, 0.5)',
               borderColor: 'rgba(00, 22, 132, 1)',
               borderWidth: 2,
-              pointRadius: 5,
+              pointRadius: 4,
               parsing: {
                 xAxisKey: 'episode',
                 yAxisKey: 'reward',
               },
-            },
+              yAxisID: 'rewards',
+              normalized: true,
+            }
           ],
         },
       };
 
+      if (chartConfig.data.datasets.length == 1 && props.flight.episodes.length) {
+        const ep = props.flight.episodes[0]
+        let numActions = 0
+        for (const actionName of Object.keys(ep.actions_taken)) {
+          if (numActions >= colors.length) {
+            // Only show the first colors.length actions. TODO: Support action selector/filter
+            break
+          }
+          const dataset = {
+            label: actionName,
+            data: props.flight.episodes as any,
+            backgroundColor: colors[numActions] + "22",
+            borderColor: colors[numActions] + "DD",
+            borderWidth: 1,
+            pointRadius: 2,
+            parsing: {
+              xAxisKey: 'episode',
+              yAxisKey: `actions_taken.${actionName}`,
+            },
+            yAxisID: 'actions',
+            normalized: true,
+          }
+          chartConfig.data.datasets.push(dataset)
+          numActions++
+        }
+      }
+
       if (chart.current) {
         chart.current.options = chartConfig.options as any;
         chart.current.data = chartConfig.data;
-        chart.current.update();
+        chart.current.update("resize");
       } else {
         chart.current = new Chart(chartCanvas, chartConfig);
       }
