b4df27a862b8b2ef88fc9116cfd394a9cf5bd113
Phillip LeBlanc <phillip@spiceai.io>
2021-09-17 12:06:58 +0900

    Accept either string or float64 as value for Observation json format (#229)
    * Unmarshal into union
    
    * dev mod
    
    * better union handling
    
    * reference good version of data-components-contrib

--- pkg/api/observation/observation.go
+++ pkg/api/observation/observation.go
@@ -3,6 +3,7 @@ package observation
 import (
 	_ "embed"
 
+	spice_json "github.com/spiceai/spiceai/pkg/json"
 	spice_time "github.com/spiceai/spiceai/pkg/time"
 )
 
@@ -11,12 +12,29 @@ var (
 	jsonSchema []byte
 )
 
+type ObservationValue struct {
+	String  *string
+	Float64 *float64
+}
+
 type Observation struct {
-	Time *spice_time.Time   `json:"time"`
-	Data map[string]float64 `json:"data"`
-	Tags []string           `json:"tags,omitempty"`
+	Time *spice_time.Time             `json:"time"`
+	Data map[string]*ObservationValue `json:"data"`
+	Tags []string                     `json:"tags,omitempty"`
 }
 
 func JsonSchema() []byte {
 	return jsonSchema
+}
+
+func (x *ObservationValue) UnmarshalJSON(data []byte) error {
+	err := spice_json.UnmarshalUnion(data, nil, &x.String, &x.Float64)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (x *ObservationValue) MarshalJSON() ([]byte, error) {
+	return spice_json.MarshalUnion(nil, x.String, x.Float64)
 }
