590b18debac1cf214708fbdd1dbfa81ee9f19b52
Luke Kim <80174+lukekim@users.noreply.github.com>
2021-09-27 09:32:44 +0000

    Add read lock around interpretations

--- pkg/interpretations/interpretations.go
+++ pkg/interpretations/interpretations.go
@@ -40,6 +40,9 @@ func (store *InterpretationsStore) Intervals() int64 {
 }
 
 func (store *InterpretationsStore) All() []Interpretation {
+	store.interpretationsMutex.RLock()
+	defer store.interpretationsMutex.RUnlock()
+	
 	return store.interpretations
 }
 
