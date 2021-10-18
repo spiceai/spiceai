1c2f4a82ede434b14a438adbe5cc8f169e341099
Luke Kim <80174+lukekim@users.noreply.github.com>
2021-10-18 11:49:51 +0000

    WIP

--- dashboard/src/components/datagrid/DataGrid.tsx
+++ dashboard/src/components/datagrid/DataGrid.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 
 import { useObservations } from '../../models/observation';
 import { Pod } from '../../models/pod';
-import GridItem from './GridItem';
 
 interface DataGridProps {
   pod: Pod;
@@ -11,28 +10,67 @@ interface DataGridProps {
 const DataGrid: React.FunctionComponent<DataGridProps> = (props) => {
   const {
     data: observationData,
-    isLoading: observationsLoading,
     error: observationError,
+    isLoading: observationsLoading,
   } = useObservations(props.pod.name);
 
   if (observationError) {
     return (
-      <div className="p-2">Error loading observation data: {JSON.stringify(observationError)}</div>
+      <div className="p-4">Error loading observations: {JSON.stringify(observationError)}</div>
     );
   }
 
-  return (
-    <div className="h-120 bg-white rounded rounded-b-none border border-b-0 border-gray-300 overflow-hidden">
-      <div className="flex bg-gradient-to-r from-sidebar-gradient-start to-sidebar-gradient-end p-1">
-        <span className="uppercase text-white font-heading text-sm justify-items-center">
-          Coinbase: BTCUSD
-        </span>
-      </div>
+  if (observationsLoading) {
+    return <div className="p-4">Loading observations...</div>;
+  }
 
+  return (
+    <div className="bg-white rounded rounded-b-none border border-gray-300">
       {observationData.length === 0 ? (
-        <div>No data</div>
+        <div className="p-4">No data</div>
       ) : (
-        <div className="grid grid-cols-7 justify-items-center overflow-scroll"></div>
+        <table className="text-sm table-auto">
+          <thead className="bg-gradient-to-r from-sidebar-gradient-start to-sidebar-gradient-end p-1 uppercase text-white font-heading">
+            <tr>
+              <th>time</th>
+              {observationData[0].measurements &&
+                Object.keys(observationData[0].measurements).map((h, i) => {
+                  return <th key={i}>{h}</th>;
+                })}
+              {observationData[0].categories &&
+                Object.keys(observationData[0].categories).map((h, i) => {
+                  return <th key={i}>{h}</th>;
+                })}
+              <th>tags</th>
+            </tr>
+          </thead>
+          <tbody className="block h-120 overflow-scroll">
+            {observationData.map((o, i) => {
+              return (
+                <tr key={i}>
+                  <td className="border-r">{new Date(o.time * 1000).toLocaleString()}</td>
+                  {observationData[0].measurements &&
+                    Object.values(observationData[0].measurements).map((h, i) => {
+                      return (
+                        <td key={i} className="px-1">
+                          {h}
+                        </td>
+                      );
+                    })}
+                  {observationData[0].categories &&
+                    Object.values(observationData[0].categories).map((h, i) => {
+                      return (
+                        <td key={i} className="px-1">
+                          {h}
+                        </td>
+                      );
+                    })}
+                  {observationData[0].tags && <td>{observationData[0].tags.join(' ')}</td>}
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
       )}
     </div>
   );
