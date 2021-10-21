88e56bba49a10d0516fae6e01f399f4ddbf5beca
Phillip LeBlanc <phillip@spiceai.io>
2021-10-21 20:43:04 +0900

    get release from version.txt

--- .github/scripts/get_release_version.py
+++ .github/scripts/get_release_version.py
@@ -1,34 +1,24 @@
 import os
-import sys
 
 gitRef = os.getenv("GITHUB_REF")
-tagRefPrefix = "refs/tags/v"
 
 with open(os.getenv("GITHUB_ENV"), "a") as githubEnv:
-
+    with open("version.txt") as f:
+            version = f.read()
+    releaseVersion = version.strip()
     is_prerelease = True
 
-    if gitRef is None or not gitRef.startswith(tagRefPrefix):
-        githubEnv.write("REL_VERSION=edge\n")
-        print ("This is a daily build from {}...".format(gitRef))
-        sys.exit(0)
-
-    releaseVersion = gitRef[len(tagRefPrefix):]
     releaseNotePath = "docs/release_notes/v{}.md".format(releaseVersion)
 
-    if gitRef.find("-rc") > 0:
-        print ("Release Candidate build from {}...".format(gitRef))
+    print ("Checking if {} exists".format(releaseNotePath))
+    if os.path.exists(releaseNotePath):
+        print ("Found {}".format(releaseNotePath))
+        # Set LATEST_RELEASE to true
+        githubEnv.write("LATEST_RELEASE=true\n")
+        is_prerelease = False
     else:
-        print ("Checking if {} exists".format(releaseNotePath))
-        if os.path.exists(releaseNotePath):
-            print ("Found {}".format(releaseNotePath))
-            # Set LATEST_RELEASE to true
-            githubEnv.write("LATEST_RELEASE=true\n")
-            is_prerelease = False
-        else:
-            print ("{} is not found".format(releaseNotePath))
-            sys.exit(1)
-        print ("Release build from {}...".format(gitRef))
+        print ("{} is not found".format(releaseNotePath))
+    print ("Release build from v{}...".format(releaseVersion))
 
     if is_prerelease:
         githubEnv.write("PRE_RELEASE=true\n")
