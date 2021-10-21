08908fc2143f68a2afb62f97430f8592e39620d1
Phillip LeBlanc <phillip@spiceai.io>
2021-10-21 20:44:00 +0900

    fix space

--- .github/scripts/get_release_version.py
+++ .github/scripts/get_release_version.py
@@ -10,15 +10,15 @@ with open(os.getenv("GITHUB_ENV"), "a") as githubEnv:
 
     releaseNotePath = "docs/release_notes/v{}.md".format(releaseVersion)
 
-    print ("Checking if {} exists".format(releaseNotePath))
+    print("Checking if {} exists".format(releaseNotePath))
     if os.path.exists(releaseNotePath):
-        print ("Found {}".format(releaseNotePath))
+        print("Found {}".format(releaseNotePath))
         # Set LATEST_RELEASE to true
         githubEnv.write("LATEST_RELEASE=true\n")
         is_prerelease = False
     else:
-        print ("{} is not found".format(releaseNotePath))
-    print ("Release build from v{}...".format(releaseVersion))
+        print("{} is not found".format(releaseNotePath))
+    print("Release build from v{}...".format(releaseVersion))
 
     if is_prerelease:
         githubEnv.write("PRE_RELEASE=true\n")
