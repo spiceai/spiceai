626298a5d251307b540199366eb76fb840bc2181
Luke Kim <80174+lukekim@users.noreply.github.com>
2021-10-27 21:12:22 +0900

    revert

--- dashboard/src/models/observation.ts
+++ dashboard/src/models/observation.ts
@@ -1,48 +1,48 @@
-import { useState, useEffect } from 'react'
-import useSWR from 'swr'
+import { useState, useEffect } from 'react';
+import useSWR from 'swr';
 
-import { getApiPath } from '../util/api'
+import { getApiPath } from '../util/api';
 
 export interface Observation {
-  time: number
-  measurements: { [key: string]: number }
-  categories: { [key: string]: string }
-  _tags: string[]
+  time: number;
+  measurements: { [key: string]: number };
+  categories: { [key: string]: string };
+  tags: string[];
 }
 
 export interface ObservationsResponse {
-  data: Observation[]
-  isLoading: boolean
-  error: any
+  data: Observation[];
+  isLoading: boolean;
+  error: any;
 }
 
 const requestInit = {
   headers: new Headers({
     Accept: 'application/json',
   }),
-}
+};
 
 const fetcher = async (url: string): Promise<any> => {
-  const response = await fetch(url, requestInit)
-  return response.json()
-}
+  const response = await fetch(url, requestInit);
+  return response.json();
+};
 
 export function useObservations(podName: string): ObservationsResponse {
-  const path = getApiPath(`/pods/${podName}/observations`)
+  const path = getApiPath(`/pods/${podName}/observations`);
   const { data, error } = useSWR(path, fetcher, {
     refreshInterval: 100,
-  })
-  const [observations, setObservations] = useState<Observation[]>([])
+  });
+  const [observations, setObservations] = useState<Observation[]>([]);
 
   useEffect(() => {
     if (data) {
-      setObservations(data)
+      setObservations(data);
     }
-  }, [data])
+  }, [data]);
 
   return {
     data: observations,
     isLoading: !error && !data,
     error,
-  }
+  };
 }
