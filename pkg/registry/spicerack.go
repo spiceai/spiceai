5001cf0d671b8366c1daf51742ec624ad496ec1f
Luke Kim <80174+lukekim@users.noreply.github.com>
2021-11-17 18:47:25 +0900

    Fix sanitize order

--- pkg/registry/spicerack.go
+++ pkg/registry/spicerack.go
@@ -86,6 +86,12 @@ func (r *SpiceRackRegistry) GetPod(podFullPath string) (string, error) {
 
 	for _, f := range zipReader.File {
 		fpath := filepath.Join(podsPath, f.Name)
+
+		err = util.SanitizeExtractPath(fpath, podsPath)
+		if err != nil {
+			return "", err
+		}
+
 		if f.FileInfo().IsDir() {
 			err := os.MkdirAll(fpath, podsPerm)
 			if err != nil {
@@ -94,13 +100,7 @@ func (r *SpiceRackRegistry) GetPod(podFullPath string) (string, error) {
 			continue
 		}
 
-		extractDir := filepath.Dir(fpath)
-		err = util.SanitizeExtractPath(fpath, extractDir)
-		if err != nil {
-			return "", err
-		}
-
-		err = os.MkdirAll(extractDir, podsPerm)
+		err = os.MkdirAll(podsPath, podsPerm)
 		if err != nil {
 			return "", err
 		}
