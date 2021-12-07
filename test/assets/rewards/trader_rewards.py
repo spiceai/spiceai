cd1ddf068b28bd6662b72922e310ca151f4fcd82
Phillip LeBlanc <phillip@spiceai.io>
2021-12-07 09:02:19 +0900

    Consistent reward function parameters (#476)
    * Tweak reward function
    
    * change tests
    
    * fix lint
    
    * fix commit hash

--- test/assets/rewards/trader_rewards.py
+++ test/assets/rewards/trader_rewards.py
@@ -1,16 +1,16 @@
-def calculate_price_change(curr_state: dict, new_state: dict):
+def calculate_price_change(curr_state: dict, next_state: dict):
     curr_price = curr_state["coinbase_btcusd_close"]
-    next_price = new_state["coinbase_btcusd_close"]
+    next_price = next_state["coinbase_btcusd_close"]
     return curr_price - next_price
 
 
-def buy(curr_state: dict, prev_interps, new_state: dict, new_interps):
-    return calculate_price_change(curr_state=curr_state, new_state=new_state)
+def buy(curr_state: dict, prev_interps, next_state: dict, new_interps):
+    return calculate_price_change(curr_state=curr_state, next_state=next_state)
 
 
-def sell(curr_state: dict, prev_interps, new_state: dict, new_interps):
-    return -calculate_price_change(curr_state=curr_state, new_state=new_state)
+def sell(curr_state: dict, prev_interps, next_state: dict, new_interps):
+    return -calculate_price_change(curr_state=curr_state, next_state=next_state)
 
 
-def hold(curr_state: dict, prev_interps, new_state: dict, new_interps):
+def hold(curr_state: dict, prev_interps, next_state: dict, new_interps):
     return -0.1
