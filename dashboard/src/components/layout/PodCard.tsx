37eebca9f1b1caea2b4bb4acef69972636b225b5
Luke Kim <80174+lukekim@users.noreply.github.com>
2022-06-20 15:12:41 +0900

    Fixes for updates

--- dashboard/src/components/layout/PodCard.tsx
+++ dashboard/src/components/layout/PodCard.tsx
@@ -1,37 +1,40 @@
-import { useHistory } from 'react-router-dom';
-import { Sparklines, SparklinesLine } from 'react-sparklines';
+import { useHistory } from 'react-router-dom'
+import { Sparklines, SparklinesLine } from 'react-sparklines'
 import { useFlights } from '../../models/flight'
 
 interface PodCardProps {
-  isLoading: boolean;
-  podName: string;
-  fqid: string;
-  route: string;
+  isLoading: boolean
+  podName: string
+  fqid: string
+  route: string
+  children?: React.ReactNode
 }
 
 const PodCard: React.FunctionComponent<PodCardProps> = (props) => {
-  const { isLoading: podIsLoading, podName, route, fqid } = props;
-  const history = useHistory();
+  const { isLoading: podIsLoading, podName, route, fqid } = props
+  const history = useHistory()
 
   const { data: flights, isLoading: flightsLoading } = useFlights(props.podName)
   const hasFlightData = !flightsLoading && flights && flights.length > 0
 
   const sparkData = hasFlightData
-    ? flights.map(flight => {
+    ? flights.map((flight) => {
         return flight.episodes.length > 0
-          ? flight.episodes.map(ep => ep.reward).reduce((total, reward) => {
-              return total + reward
-            }, 0) / flight.episodes.length
+          ? flight.episodes
+              .map((ep) => ep.reward)
+              .reduce((total, reward) => {
+                return total + reward
+              }, 0) / flight.episodes.length
           : 0
       })
     : [5, 10, 6, 20, 15, 10, 20, 25, 25]
 
   function handleClick() {
-    history.push(route);
+    history.push(route)
   }
 
   if (podIsLoading) {
-    <div className="border p-10 rounded shadow bg-white">
+    ;<div className="border p-10 rounded shadow bg-white">
       <div className="animate-pulse flex items-center justify-items-center gap-4">
         <div className="h-12 w-12 bg-primary rounded-full"></div>
         <div className="flex flex-col gap-2 items-start">
@@ -39,11 +42,14 @@ const PodCard: React.FunctionComponent<PodCardProps> = (props) => {
           <div className="h-4 w-36 bg-gray-400 rounded"></div>
         </div>
       </div>
-    </div>;
+    </div>
   }
 
   return (
-    <div className="flex flex-row border p-6 h-32 rounded shadow bg-white cursor-pointer" onClick={handleClick}>
+    <div
+      className="flex flex-row border p-6 h-32 rounded shadow bg-white cursor-pointer"
+      onClick={handleClick}
+    >
       <div className="flex items-center justify-items-center gap-4">
         <div className="flex flex-col items-start">
           <p className="text-xl text-link font-semibold">{podName}</p>
@@ -58,7 +64,7 @@ const PodCard: React.FunctionComponent<PodCardProps> = (props) => {
         </Sparklines>
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default PodCard;
+export default PodCard
