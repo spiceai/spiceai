0fe3902090214364e12f2aa8f34a1f67e7acd33f
Phillip LeBlanc <phillip@leblanc.tech>
2024-01-19 21:20:16 +0900

    use existing tableref

--- crates/runtime/src/databackend.rs
+++ crates/runtime/src/databackend.rs
@@ -1,39 +1,15 @@
-use std::{any::Any, sync::Arc};
-
 use arrow::{datatypes::SchemaRef, record_batch::RecordBatch};
-use datafusion::{
-    datasource::TableProvider,
-    execution::context::SessionState,
-    logical_expr::{Expr, TableType},
-    physical_plan::ExecutionPlan,
-};
+use datafusion::datasource::MemTable;
 
 pub struct InMemoryBackend {
-    pub schema: SchemaRef,
-    pub data: Vec<RecordBatch>,
+    pub mem_table: MemTable,
 }
 
-impl TableProvider for InMemoryBackend {
-    fn as_any(&self) -> &dyn Any {
-        return self;
-    }
-
-    fn schema(&self) -> SchemaRef {
-        return self.schema.clone();
-    }
-
-    fn table_type(&self) -> TableType {
-        return TableType::Base;
-    }
-
-    #[allow(clippy::type_complexity, clippy::type_repetition_in_bounds)]
-    fn scan(
-        &self,
-        state: &SessionState,
-        projection: Option<&Vec<usize>>,
-        filters: &[Expr],
-        limit: Option<usize>,
-    ) -> Result<Arc<dyn ExecutionPlan>, Box<dyn std::error::Error>> {
-        todo!()
+impl InMemoryBackend {
+    #[must_use]
+    pub fn new(schema: SchemaRef, records: Vec<RecordBatch>) -> Self {
+        InMemoryBackend {
+            mem_table: MemTable::try_new(schema, vec![records]),
+        }
     }
 }
