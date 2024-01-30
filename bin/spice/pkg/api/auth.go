61c4d916ee1ffe7e32f9fd1f1b2911892d237917
Mitch <88671039+mitchdevenport@users.noreply.github.com>
2024-01-30 18:10:46 -0500

    Add support for spice login dremio (#631)
    * Add support for Dremio as a data source
    
    * doc
    
    * simplify
    
    * shared flight data source
    
    * doc
    
    * undo
    
    * Add support for spice login dremio
    
    * revert

--- bin/spice/pkg/api/auth.go
+++ bin/spice/pkg/api/auth.go
@@ -2,9 +2,12 @@ package api
 
 const (
 	AUTH_TYPE_SPICE_AI = "spice.ai"
+	AUTH_TYPE_DREMIO   = "dremio"
 )
 
 type Auth struct {
-	ProviderType string `json:"provider_type,omitempty" csv:"provider_type" toml:"provider_type"`
-	Key          string `json:"key,omitempty" csv:"key" toml:"key"`
+	ProviderType string `json:"provider_type,omitempty" csv:"provider_type" toml:"provider_type,omitempty"`
+	Key          string `json:"key,omitempty" csv:"key" toml:"key,omitempty"`
+	Username     string `json:"username,omitempty" csv:"username" toml:"username,omitempty"`
+	Password     string `json:"password,omitempty" csv:"password" toml:"password,omitempty"`
 }
--- bin/spice/pkg/cli/cmd/login.go
+++ bin/spice/pkg/cli/cmd/login.go
@@ -8,11 +8,12 @@ import (
 	toml "github.com/pelletier/go-toml"
 	"github.com/spf13/cobra"
 	"github.com/spiceai/spiceai/bin/spice/pkg/api"
-	"github.com/spiceai/spiceai/bin/spice/pkg/util"
 )
 
 const (
-	apiKeyFlag = "key"
+	apiKeyFlag   = "key"
+	usernameFlag = "username"
+	passwordFlag = "password"
 )
 
 var loginCmd = &cobra.Command{
@@ -24,12 +25,6 @@ spice login
 # See more at: https://docs.spiceai.org/
 `,
 	Run: func(cmd *cobra.Command, args []string) {
-
-		err := checkLatestCliReleaseVersion()
-		if err != nil && util.IsDebug() {
-			cmd.PrintErrf("failed to check for latest CLI release version: %s\n", err.Error())
-		}
-
 		key, err := cmd.Flags().GetString(apiKeyFlag)
 		if err != nil {
 			cmd.Println(err.Error())
@@ -41,47 +36,108 @@ spice login
 			os.Exit(1)
 		}
 
-		homeDir, err := os.UserHomeDir()
-		if err != nil {
-			cmd.Println(err.Error())
-			os.Exit(1)
-		}
-
-		authConfig := map[string]*api.Auth{
-			api.AUTH_TYPE_SPICE_AI: {
-				Key:          key,
-				ProviderType: api.AUTH_TYPE_SPICE_AI,
-			},
-		}
-		authConfigBytes, err := toml.Marshal(authConfig)
-		if err != nil {
-			cmd.Println(err.Error())
-			os.Exit(1)
-		}
-
-		spiceDir := fmt.Sprintf("%s/.spice", homeDir)
-		err = os.MkdirAll(spiceDir, 0644)
-		if err != nil {
-			cmd.Println(err.Error())
-			os.Exit(1)
-		}
-		f, err := os.Create(fmt.Sprintf("%s/auth", spiceDir))
-		if err != nil {
-			cmd.Println(err.Error())
-			os.Exit(1)
-		}
-
-		_, err = f.Write(authConfigBytes)
-		if err != nil {
-			cmd.Println(err.Error())
-			os.Exit(1)
-		}
+		mergeAuthConfig(cmd, api.AUTH_TYPE_SPICE_AI, &api.Auth{
+			Key:          key,
+			ProviderType: api.AUTH_TYPE_SPICE_AI,
+		})
 
 		cmd.Println(aurora.BrightGreen("Successfully logged in to Spice.ai"))
 	},
 }
 
+var dremioCmd = &cobra.Command{
+	Use:   "dremio",
+	Short: "Login to a Dremio instance",
+	Example: `
+spice login dremio --username <username> --password <password>
+
+# See more at: https://docs.spiceai.org/
+`,
+	Run: func(cmd *cobra.Command, args []string) {
+
+		username, err := cmd.Flags().GetString(usernameFlag)
+		if err != nil {
+			cmd.Println(err.Error())
+			os.Exit(1)
+		}
+
+		if username == "" {
+			cmd.Println("No username provided, use --username or -u to provide a username")
+			os.Exit(1)
+		}
+
+		password, err := cmd.Flags().GetString(passwordFlag)
+		if err != nil {
+			cmd.Println(err.Error())
+			os.Exit(1)
+		}
+
+		if password == "" {
+			cmd.Println("No username provided, use --password or -p to provide a password")
+			os.Exit(1)
+		}
+
+		mergeAuthConfig(cmd, api.AUTH_TYPE_DREMIO, &api.Auth{
+			Password:     password,
+			Username:     username,
+			ProviderType: api.AUTH_TYPE_DREMIO,
+		},
+		)
+
+		cmd.Println(aurora.BrightGreen("Successfully logged in to Spice.ai"))
+	},
+}
+
+func mergeAuthConfig(cmd *cobra.Command, updatedAuthName string, updatedAuthConfig *api.Auth) {
+	homeDir, err := os.UserHomeDir()
+	if err != nil {
+		cmd.Println(err.Error())
+		os.Exit(1)
+	}
+	spiceDir := fmt.Sprintf("%s/.spice", homeDir)
+	authFilePath := fmt.Sprintf("%s/auth", spiceDir)
+
+	err = os.MkdirAll(spiceDir, 0644)
+	if err != nil {
+		cmd.Println(err.Error())
+		os.Exit(1)
+	}
+
+	authConfig := map[string]*api.Auth{}
+	if _, err := os.Stat(authFilePath); !os.IsNotExist(err) {
+		authConfigBytes, err := os.ReadFile(authFilePath)
+		if err != nil {
+			cmd.Println(err)
+			os.Exit(1)
+		}
+
+		err = toml.Unmarshal(authConfigBytes, &authConfig)
+		if err != nil {
+			cmd.Println(err)
+			os.Exit(1)
+		}
+	}
+
+	authConfig[updatedAuthName] = updatedAuthConfig
+	updatedAuthConfigBytes, err := toml.Marshal(authConfig)
+	if err != nil {
+		cmd.Println(err.Error())
+		os.Exit(1)
+	}
+
+	err = os.WriteFile(authFilePath, updatedAuthConfigBytes, 0644)
+	if err != nil {
+		cmd.Println(err.Error())
+		os.Exit(1)
+	}
+}
+
 func init() {
+	dremioCmd.Flags().BoolP("help", "h", false, "Print this help message")
+	dremioCmd.Flags().StringP(usernameFlag, "u", "", "Username")
+	dremioCmd.Flags().StringP(passwordFlag, "p", "", "Password")
+	loginCmd.AddCommand(dremioCmd)
+
 	loginCmd.Flags().BoolP("help", "h", false, "Print this help message")
 	loginCmd.Flags().StringP(apiKeyFlag, "k", "", "API key")
 	RootCmd.AddCommand(loginCmd)
