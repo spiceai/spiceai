cfc0329f987fd813eb16dc52458d8ab75b407238
Mitch Devenport <mitch@spice.ai>
2024-01-30 17:12:04 -0500

    shared convesion

--- crates/runtime/src/databackend/memtable.rs
+++ crates/runtime/src/databackend/memtable.rs
@@ -24,7 +24,7 @@ impl MemTableBackend {
     pub fn new(ctx: Arc<SessionContext>, name: &str) -> Self {
         MemTableBackend {
             ctx,
-            name: name.replace('.', "_").replace('"', ""),
+            name: crate::datafusion::get_internal_dataset_name(name),
         }
     }
 }
