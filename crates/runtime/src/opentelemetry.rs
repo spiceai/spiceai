b776215fe8ddb17a578c96e8ea79941e9fa0a552
Mitch Devenport <mitch@spice.ai>
2024-02-08 15:46:30 -0500

    Write Telegraf metrics to databackends

--- crates/runtime/src/opentelemetry.rs
+++ crates/runtime/src/opentelemetry.rs
@@ -1,17 +1,30 @@
 use std::net::SocketAddr;
+use std::sync::Arc;
 
+use arrow::array::Float64Array;
+use arrow::datatypes::DataType;
+use arrow::datatypes::Field;
+use arrow::datatypes::Schema;
+use arrow::record_batch::RecordBatch;
 use opentelemetry_proto::tonic::collector::metrics::v1::metrics_service_server::MetricsService;
 use opentelemetry_proto::tonic::collector::metrics::v1::metrics_service_server::MetricsServiceServer;
 use opentelemetry_proto::tonic::collector::metrics::v1::ExportMetricsPartialSuccess;
 use opentelemetry_proto::tonic::collector::metrics::v1::ExportMetricsServiceRequest;
 use opentelemetry_proto::tonic::collector::metrics::v1::ExportMetricsServiceResponse;
+use opentelemetry_proto::tonic::metrics::v1::metric::Data;
+use opentelemetry_proto::tonic::metrics::v1::number_data_point::Value;
+use opentelemetry_proto::tonic::metrics::v1::NumberDataPoint;
 use snafu::prelude::*;
-use tonic::async_trait;
+use tonic_0_9_0::async_trait;
 use tonic_0_9_0::codec::CompressionEncoding;
 use tonic_0_9_0::transport::Server;
 use tonic_0_9_0::Request;
 use tonic_0_9_0::Response;
 use tonic_0_9_0::Status;
+
+use crate::datafusion::DataFusion;
+use crate::dataupdate::DataUpdate;
+use crate::dataupdate::UpdateType;
 
 type Result<T, E = Error> = std::result::Result<T, E>;
 
@@ -21,9 +34,17 @@ pub enum Error {
     UnableToServe {
         source: tonic_0_9_0::transport::Error,
     },
+
+    #[snafu(display("Failed to build record batch"))]
+    FailedToBuildRecordBatch { source: arrow::error::ArrowError },
+
+    #[snafu(display("Unsupported metric data type"))]
+    UnsupportedMetricDataType {},
 }
 
-pub struct Service;
+pub struct Service {
+    data_fusion: Arc<DataFusion>,
+}
 
 #[async_trait]
 impl MetricsService for Service {
@@ -33,25 +54,52 @@ impl MetricsService for Service {
     ) -> std::result::Result<Response<ExportMetricsServiceResponse>, Status> {
         let mut rejected_data_points = 0;
         let mut total_data_points = 0;
+        let mut results = Vec::new();
         for resource_metric in request.into_inner().resource_metrics {
             for scope_metric in resource_metric.scope_metrics {
                 for metric in scope_metric.metrics {
-                    total_data_points += 1;
-                    tracing::debug!("Metric: {:?}", metric.name);
-                    match metric.data {
-                        Some(data) => {
-                            // TODO: Check if localhost table exists and write to Databackend
-                            tracing::debug!("Data: {:?}", data);
-                        }
-                        None => {
-                            rejected_data_points += 1;
+                    if let Some(data) = metric.data {
+                        let (record_batch_result, accepted_points, rejected_points) =
+                            metric_data_to_record_batch(&data);
+                        total_data_points += accepted_points + rejected_points;
+                        rejected_data_points += rejected_points;
+
+                        let Some(backend) = self.data_fusion.get_backend(metric.name.as_str())
+                        else {
+                            tracing::warn!("No backend found for metric {}", metric.name);
+                            rejected_data_points += accepted_points;
+                            continue;
+                        };
+
+                        if let Ok(record_batch) = record_batch_result {
+                            tracing::info!("Original {} {:?}", metric.name, data);
+                            tracing::info!(
+                                "Adding data to backend {} {:?}",
+                                metric.name,
+                                record_batch
+                            );
+                            results.push((
+                                backend.add_data(DataUpdate {
+                                    log_sequence_number: None,
+                                    data: vec![record_batch],
+                                    update_type: UpdateType::Append,
+                                }),
+                                accepted_points,
+                            ));
                         }
                     }
                 }
             }
         }
 
-        if rejected_data_points == total_data_points {
+        for (result, accepted_points) in results {
+            if let Err(e) = result.await {
+                rejected_data_points += accepted_points;
+                tracing::error!("Failed to add OpenTelemetry data to backend: {}", e);
+            }
+        }
+
+        if rejected_data_points >= total_data_points {
             return Err(Status::invalid_argument("All data points were rejected"));
         }
 
@@ -69,8 +117,60 @@ impl MetricsService for Service {
     }
 }
 
-pub async fn start(bind_address: SocketAddr) -> Result<()> {
-    let service = Service;
+pub fn metric_data_to_record_batch(data: &Data) -> (Result<RecordBatch>, i64, i64) {
+    match data {
+        Data::Gauge(gauge) => number_data_points_to_record_batch(&gauge.data_points),
+        Data::Sum(sum) => number_data_points_to_record_batch(&sum.data_points),
+        // Data::Histogram(histogram) => histogram.data_points,
+        // Data::ExponentialHistogram(exponential_histogram) => exponential_histogram.data_points,
+        // Data::Summary(summary) => summary.data_points,
+        _ => (UnsupportedMetricDataTypeSnafu.fail(), 0, 0),
+    }
+}
+
+fn number_data_points_to_record_batch(
+    data_points: &Vec<NumberDataPoint>,
+) -> (Result<RecordBatch>, i64, i64) {
+    let mut accepted_data_points = 0;
+    let mut rejected_data_points = 0;
+
+    let mut values = Vec::new();
+    for data_point in data_points {
+        if let Some(value) = &data_point.value {
+            accepted_data_points += 1;
+            match value {
+                Value::AsDouble(double_value) => {
+                    values.push(*double_value);
+                }
+                Value::AsInt(int_value) => {
+                    values.push(*int_value as f64);
+                }
+            }
+        } else {
+            rejected_data_points += 1;
+            continue;
+        }
+    }
+
+    match RecordBatch::try_new(
+        Arc::new(Schema::new(vec![Field::new(
+            "value",
+            DataType::Float64,
+            false,
+        )])),
+        vec![Arc::new(Float64Array::from(values))],
+    ) {
+        Ok(record_batch) => (Ok(record_batch), accepted_data_points, rejected_data_points),
+        Err(e) => (
+            Err(e).context(FailedToBuildRecordBatchSnafu),
+            0,
+            accepted_data_points + rejected_data_points,
+        ),
+    }
+}
+
+pub async fn start(bind_address: SocketAddr, data_fusion: Arc<DataFusion>) -> Result<()> {
+    let service = Service { data_fusion };
     let svc = MetricsServiceServer::new(service).accept_compressed(CompressionEncoding::Gzip);
 
     tracing::info!("Spice Runtime OpenTelemetry listening on {bind_address}");
