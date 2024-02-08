3554541b09ba765babf9ef45722b88f2c556b018
Mitch <88671039+mitchdevenport@users.noreply.github.com>
2024-02-07 21:03:59 -0500

    Basic OpenTelemetry Server (#657)
    * basic server
    
    * static datafusion
    
    * revert
    
    * remove
    
    * comment
    
    * revert

--- crates/runtime/src/lib.rs
+++ crates/runtime/src/lib.rs
@@ -17,6 +17,7 @@ pub mod dataupdate;
 mod flight;
 mod http;
 pub mod modelformat;
+mod opentelemetry;
 
 #[derive(Debug, Snafu)]
 pub enum Error {
@@ -25,6 +26,9 @@ pub enum Error {
 
     #[snafu(display("Unable to start Flight server"))]
     UnableToStartFlightServer { source: flight::Error },
+
+    #[snafu(display("Unable to start OpenTelemetry server"))]
+    UnableToStartOpenTelemetryServer { source: opentelemetry::Error },
 }
 
 pub type Result<T, E = Error> = std::result::Result<T, E>;
@@ -48,10 +52,13 @@ impl Runtime {
     pub async fn start_servers(&self) -> Result<()> {
         let http_server_future = http::start(self.config.http_bind_address, self.app.clone());
         let flight_server_future = flight::start(self.config.flight_bind_address, self.df.clone());
+        let open_telemetry_server_future =
+            opentelemetry::start(self.config.open_telemetry_bind_address);
 
         tokio::select! {
             http_res = http_server_future => http_res.context(UnableToStartHttpServerSnafu),
             flight_res = flight_server_future => flight_res.context(UnableToStartFlightServerSnafu),
+            open_telemetry_res = open_telemetry_server_future => open_telemetry_res.context(UnableToStartOpenTelemetryServerSnafu),
             () = shutdown_signal() => {
                 tracing::info!("Goodbye!");
                 Ok(())
