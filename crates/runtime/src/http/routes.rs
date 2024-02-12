c15b5f1c56868cf6c2c60641f51dd5af314ae5ae
yfu <fevin86@gmail.com>
2024-02-12 15:42:33 +1100

    update routes

--- crates/runtime/src/http/routes.rs
+++ crates/runtime/src/http/routes.rs
@@ -14,7 +14,7 @@ pub(crate) fn routes(app: Arc<App>, df: Arc<DataFusion>) -> Router {
     Router::new()
         .route("/health", get(|| async { "ok\n" }))
         .route("/v1/datasets", get(v1::datasets::get))
-        .route("/v1/models/:name/inference", get(v1::inference::get))
+        .route("/v1/models/:name/predictions", get(v1::inference::get))
         .layer(Extension(app))
         .layer(Extension(df))
 }
