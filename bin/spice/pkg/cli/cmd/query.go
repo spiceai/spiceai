dd61f3ac70e095a83bd89857194f5417b7fc3a55
Phillip LeBlanc <phillip@leblanc.tech>
2024-02-15 12:36:26 +0900

    Update example

--- bin/spice/pkg/cli/cmd/query.go
+++ bin/spice/pkg/cli/cmd/query.go
@@ -12,7 +12,20 @@ var queryCmd = &cobra.Command{
 	Use:   "query",
 	Short: "Start an interactive SQL query session against the Spice.ai runtime",
 	Example: `
-spice query
+$ spice query
+Welcome to the interactive Spice.ai SQL Query Utility! Type 'help' for help.
+
+show tables;  -- list available tables
+sql> show tables
++---------------+--------------------+---------------+------------+
+| table_catalog | table_schema       | table_name    | table_type |
++---------------+--------------------+---------------+------------+
+| datafusion    | public             | tmp_view_test | VIEW       |
+| datafusion    | information_schema | tables        | VIEW       |
+| datafusion    | information_schema | views         | VIEW       |
+| datafusion    | information_schema | columns       | VIEW       |
+| datafusion    | information_schema | df_settings   | VIEW       |
++---------------+--------------------+---------------+------------+
 `,
 	Run: func(cmd *cobra.Command, args []string) {
 		rtcontext := context.NewContext()
