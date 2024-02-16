f80be29c0b313c2b39bcf4c1159d99dc769bfaf0
Roy Rotstein <112157037+Roee-87@users.noreply.github.com>
2024-02-15 21:26:12 -0800

    multithread simulation (#702)
    * multithread simulation
    
    * modified concurrent query simulation
    
    * updated sim to include duckdb and memtable
    
    * Update runtime_demo.py
    
    ---------
    
    Co-authored-by: Phillip LeBlanc <phillip@leblanc.tech>

--- examples/runtime_demo.py
+++ examples/runtime_demo.py
@@ -1,25 +1,30 @@
 import time
 from time import sleep
 from spicepy import Client
+import threading
+
+API_KEY = 'YOUR_API_KEY_HERE'
 
 ###########################
 #   Spice AI Platform     #
 ###########################
 
-# use API key from SpiceAI app to instantiate a client
-client = Client('353231|e9dd434b7ba54fc99b027a7c6d326ecd')
+# use API key from Spice.ai app to instantiate a client
+client = Client(API_KEY)
 
 startTime = time.time()
 data = client.query('SELECT * FROM eth.recent_traces trace JOIN eth.recent_transactions trans ON trace.transaction_hash = trans.hash ORDER BY trans.block_number DESC;')
+pd = data.read_chunk()
 endTime = time.time()
-pd = data.read_all()
 
-print(pd)
 print("Query Time: " + str(endTime - startTime) + " seconds\n")
 
 exit()
 
-client = Client('353231|e9dd434b7ba54fc99b027a7c6d326ecd', 'grpc://127.0.0.1:50051')
+########################################
+#   DO NOT COMMENT OUT THE LINE BELOW  #
+########################################
+client = Client(API_KEY, 'grpc://127.0.0.1:50051')
 
 ###########################
 #   Spice AI Datasource   #
@@ -27,22 +32,16 @@ client = Client('353231|e9dd434b7ba54fc99b027a7c6d326ecd', 'grpc://127.0.0.1:500
 
 while True:
     startTime = time.time()
-    data = client.query('SELECT * FROM eth_recent_traces trace JOIN eth_recent_transactions trans ON trace.transaction_hash = trans.hash ORDER BY trans.block_number DESC;')
+    data = client.query('SELECT trace.block_number FROM eth_recent_traces trace JOIN eth_recent_transactions trans ON trace.transaction_hash = trans.hash ORDER BY trans.block_number DESC;')
+    pd = data.read_pandas()
     endTime = time.time()
-    pd = data.read_all()
 
-    print(pd)
+    print(pd.head(5))
+ 
     print("Query Time: " + str(endTime - startTime) + " seconds\n")
 
-    # startTime = time.time()
-    # data = client.query('SELECT * FROM eth.recent_traces trace JOIN eth.recent_transactions trans ON trace.transaction_hash = trans.hash ORDER BY trans.block_number DESC;')
-    # endTime = time.time()
-    # pd = data.read_all()
+    sleep(1)
 
-    # print(pd)
-    # print("Query Time: " + str(endTime - startTime) + " seconds\n")
-
-    sleep(5)
 
 ###########################
 #    Dremio Datasource    #
@@ -52,20 +51,18 @@ while True:
     startTime = time.time()
     data = client.query('SELECT * FROM taxi_trips ORDER BY pickup_datetime DESC LIMIT 100')
     endTime = time.time()
-    pd = data.read_all()
+    pd = data.read_pandas()
 
-    print(pd)
+    print(pd.to_string() + "\n")
     print("Query Time: " + str(endTime - startTime) + " seconds\n")
 
     startTime = time.time()
     data = client.query('SELECT count(*) FROM taxi_trips')
     endTime = time.time()
-    pd = data.read_all()
+    pd = data.read_pandas()
 
-    print(pd)
+    print(pd.to_string() + "\n")
     print("Query Time: " + str(endTime - startTime) + " seconds\n")
-
-    sleep(5)
 
 ###########################
 # Spice/Dremio Datasource #
@@ -84,9 +81,88 @@ while True:
         LIMIT 10
         """)
     endTime = time.time()
-    pd = data.read_all()
+    pd = data.read_pandas()
 
-    print(pd)
+    print(pd.to_string() + "\n")
     print("Query Time: " + str(endTime - startTime) + " seconds\n")
 
     sleep(5)
+
+
+#####################################
+#    High-RPS Queries Simulation    #
+#####################################
+
+# update the yaml before running this code
+
+def simulate_runtime_duckdb(user_id):
+    #print("User " + str(user_id) + " started")
+    # make a new client for each user
+    client = Client(API_KEY, 'grpc://127.0.0.1:50051')
+    start = time.time()
+    # make a query
+    data = client.query('SELECT * FROM eth_recent_blocks_duckdb DESC;')
+    pd = data.read_all()
+    end = time.time()
+    total = end - start
+    
+    print("User " + str(user_id) + ": " + str(total) + " seconds")
+
+def simulate_runtime_arrow_mem(user_id):
+    #print("User " + str(user_id) + " started")
+    # make a new client for each user
+    client = Client(API_KEY, 'grpc://127.0.0.1:50051')
+    start = time.time()
+    # make a query
+    data = client.query('SELECT * FROM eth_recent_blocks DESC;')
+    pd = data.read_all()
+    end = time.time()
+    total = end - start
+    
+    print("User " + str(user_id) + ": " + str(total) + " seconds")
+
+def simulate_sdk(user_id):
+    #print("User " + str(user_id) + " started")
+    # make a new client for each user
+    client = Client(API_KEY)
+    start = time.time()
+    # make a query
+    data = client.query('SELECT * FROM eth.recent_blocks DESC;')
+    pd = data.read_all()
+    end = time.time()
+    total = end - start
+    
+    print("User " + str(user_id) + ": " + str(total) + " seconds")
+ 
+# simulate the number of users
+def simulate_concurrent_queries(num_users, function_name):
+    threads = []
+ 
+    start_time = time.time()
+ 
+    for user_id in range(num_users):
+        t = threading.Thread(target=function_name, args=(user_id,))
+        threads.append(t)
+        t.start()
+ 
+    for thread in threads:
+        thread.join()
+ 
+    end_time = time.time()
+    total_time = end_time - start_time
+ 
+    print("\n")
+
+    return total_time
+
+total_users = 50
+time_sdk = simulate_concurrent_queries(total_users, simulate_sdk)
+time_runtime_duckdb = simulate_concurrent_queries(total_users, simulate_runtime_duckdb)
+time_runtime_arrow_mem = simulate_concurrent_queries(total_users, simulate_runtime_arrow_mem)
+
+print("Simulating " + str(total_users) + " concurrent users making queries..")
+print("Total Time for SDK: " + str(time_sdk) + " seconds")
+print("Total Time for In Memory Runtime: " + str(time_runtime_arrow_mem) + " seconds")
+print("Total Time for DuckDB Runtime: " + str(time_runtime_duckdb) + " seconds")
+
+exit()
