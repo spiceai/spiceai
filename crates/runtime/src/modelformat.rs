7da5ad7b1f01ac498574e9fca9c3b5d8abc8a572
yfu <fevin86@gmail.com>
2024-02-19 15:48:08 +1100

    remove blank line

--- crates/runtime/src/modelformat.rs
+++ crates/runtime/src/modelformat.rs
@@ -3,7 +3,6 @@ pub mod onnx;
 /// A `ModelFormat` specifies the supported format of a model artifacts.
 ///
 /// Currently, only `onnx` is supported.
-
 pub enum ModelFormat {
     Onnx(onnx::Onnx),
 }
