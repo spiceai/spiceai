a6d7b4bb9bf614c1678f3f07b64a153936caa115
Mitch Devenport <mitch@spice.ai>
2024-02-20 13:46:07 -0500

    linter

--- crates/sql_provider_datafusion/src/dbconnectionpool/duckdb.rs
+++ crates/sql_provider_datafusion/src/dbconnectionpool/duckdb.rs
@@ -11,7 +11,10 @@ pub struct DuckDbConnectionPool {
 }
 
 impl DbConnectionPool<DuckdbConnectionManager, DuckDbConnection> for DuckDbConnectionPool {
-    fn new(name: &str, mode: Mode, params: Arc<Option<HashMap<String, String>>>) -> Result<Self> {
+    fn new(name: &str, mode: Mode, params: Arc<Option<HashMap<String, String>>>) -> Result<Self>
+    where
+        Self: Sized,
+    {
         let manager = match mode {
             Mode::Memory => DuckdbConnectionManager::memory().context(DuckDBSnafu)?,
             Mode::File => DuckdbConnectionManager::file(get_duckdb_file(name, &params))
