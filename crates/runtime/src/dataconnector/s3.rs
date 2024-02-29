7c7f161252c9731863ce8ae657c14c61c6cb0aa1
Jack Eadie <jack@spice.ai>
2024-02-28 23:21:42 -1000

    get S3 params from spicepod, authprovider (#756)
    * get S3 params from spicepod, authprovider
    
    * linting
    
    * fix path problem
    
    * and http

--- crates/runtime/src/dataconnector/s3.rs
+++ crates/runtime/src/dataconnector/s3.rs
@@ -1,8 +1,7 @@
 use async_trait::async_trait;
 use datafusion::execution::context::SessionContext;
 use datafusion::execution::options::ParquetReadOptions;
-use object_store::aws::AmazonS3Builder;
-use object_store::ObjectStore;
+use object_store::aws::{AmazonS3, AmazonS3Builder};
 use std::pin::Pin;
 use std::sync::Arc;
 use std::{collections::HashMap, future::Future};
@@ -28,32 +27,51 @@ pub enum Error {
 }
 
 pub struct S3 {
-    _auth_provider: AuthProvider,
+    _s3: Arc<AmazonS3>,
+}
+impl S3 {
+    #[must_use]
+    pub fn get_from_params(
+        params: &Arc<Option<HashMap<String, String>>>,
+        key: &str,
+    ) -> Option<String> {
+        params
+            .as_ref()
+            .as_ref()
+            .and_then(|params| params.get(key).cloned())
+    }
 }
 
 #[async_trait]
 impl DataConnector for S3 {
     fn new(
         auth_provider: AuthProvider,
-        _params: Arc<Option<HashMap<String, String>>>,
+        params: Arc<Option<HashMap<String, String>>>,
     ) -> Pin<Box<dyn Future<Output = super::Result<Self>> + Send>>
     where
         Self: Sized,
     {
         Box::pin(async move {
-            // let path: String = params
-            //     .as_ref()
-            //     .as_ref()
-            //     .and_then(|params| params.get("path").cloned())
-            //     .ok_or_else(|| super::Error::UnableToCreateDataConnector {
-            //         source: "Missing required parameter: path".into(),
-            //     })?;
+            let mut s3_builder = AmazonS3Builder::new().with_bucket_name("").with_allow_http(true);
 
-            // let cred_builder = DefaultCredentialsChain::builder().build();
+            if let Some(region) = Self::get_from_params(&params, "region") {
+                s3_builder = s3_builder.with_region(region);
+            }
+            if let Some(endpoint) = Self::get_from_params(&params, "endpoint") {
+                s3_builder = s3_builder.with_endpoint(endpoint);
+            }
 
-            Ok(Self {
-                _auth_provider: auth_provider,
-            })
+            if let Some(key) = auth_provider.get_param("key") {
+                s3_builder = s3_builder.with_access_key_id(key);
+            };
+            if let Some(secret) = auth_provider.get_param("secret") {
+                s3_builder = s3_builder.with_secret_access_key(secret);
+            };
+            let s3 = s3_builder
+                .build()
+                .map_err(|e| super::Error::UnableToCreateDataConnector { source: e.into() })?;
+
+            Ok(Self { _s3: Arc::new(s3) })
         })
     }
 
@@ -72,47 +90,21 @@ impl DataConnector for S3 {
         true
     }
 
-    fn has_object_store(&self) -> bool {
-        true
-    }
-
-    fn get_object_store(
-        &self,
-        dataset: &Dataset,
-    ) -> std::result::Result<(Url, Arc<dyn ObjectStore + 'static>), super::Error> {
-        let region = "";
-        let s3 = AmazonS3Builder::new()
-            .with_region(region)
-            .with_bucket_name("mldataplatform")
-            .with_access_key_id("minio")
-            .with_secret_access_key("minio123")
-            .with_endpoint("http://localhost:9000")
-            .with_allow_http(true)
-            .build()
-            .map_err(|e| super::Error::UnableToCreateDataConnector { source: e.into() })?;
-
-        let s3_url = Url::parse(&dataset.from)
-            .map_err(|e| super::Error::UnableToGetTableProvider { source: e.into() })?;
-
-        Ok((s3_url, Arc::new(s3)))
-    }
-
     async fn get_table_provider(
         &self,
         dataset: &Dataset,
     ) -> std::result::Result<Arc<dyn datafusion::datasource::TableProvider>, super::Error> {
-        let path = dataset.path();
-
         let ctx = SessionContext::new();
 
-        let (url, store) = self
-            .get_object_store(dataset)
+        let url = Url::parse(&dataset.from)
             .map_err(|e| super::Error::UnableToGetTableProvider { source: e.into() })?;
 
-        let _ = ctx.runtime_env().register_object_store(&url, store);
+        let _ = ctx
+            .runtime_env()
+            .register_object_store(&url, self._s3.clone());
 
         let df = ctx
-            .read_parquet(format!("s3:{path}"), ParquetReadOptions::default())
+            .read_parquet(&dataset.from, ParquetReadOptions::default())
             .await
             .map_err(|e| super::Error::UnableToGetTableProvider { source: e.into() })?;
 
