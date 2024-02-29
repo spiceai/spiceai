a4536db292c92c5a8ba5b822631e3368c6df7e86
Evgenii Khramkov <evgenii@spice.ai>
2024-03-01 02:53:14 +0900

    implement keyring secret store

--- crates/runtime/src/secretstore/keyring.rs
+++ crates/runtime/src/secretstore/keyring.rs
@@ -1,16 +1,39 @@
-use keyring::Result;
+use std::collections::HashMap;
+
+use keyring::Entry;
+
+use super::{Result, Secret, SecretStore};
 
 pub struct KeyringSecretStore;
 
 impl KeyringSecretStore {
-    fn get(&self, name: &str) -> Result<()> {
-        // let entry = Entry::new(name, "my_app");
+    pub fn new() -> Self {
+        Self {}
+    }
+}
 
-        Ok(())
+impl SecretStore for KeyringSecretStore {
+    #[must_use]
+    fn get_secret(&self, key: &str) -> Secret {
+        let entry = match Entry::new(key, "spiced") {
+            Ok(entry) => entry,
+            Err(e) => return Secret::new(HashMap::new()),
+        };
+
+        let secret = match entry.get_password() {
+            Ok(secret) => secret,
+            Err(e) => {
+                return Secret::new(HashMap::new());
+            }
+        };
+
+        let mut data: HashMap<String, String> = HashMap::new();
+        data.insert("data".to_string(), secret);
+
+        Secret::new(data)
     }
 
     fn init(&mut self) -> Result<()> {
-        // TBD:
         Ok(())
     }
 }
