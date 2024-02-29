fee84c2bd58939d184a546857c9479330668c874
Phillip LeBlanc <phillip@leblanc.tech>
2024-02-28 19:48:11 -1000

    fix

--- crates/runtime/src/dataconnector/databricks.rs
+++ crates/runtime/src/dataconnector/databricks.rs
@@ -1,6 +1,7 @@
 use async_trait::async_trait;
 use datafusion::execution::context::SessionContext;
-use deltalake::open_table_with_storage_options;
+use deltalake::protocol::SaveMode;
+use deltalake::{open_table_with_storage_options, DeltaOps};
 use serde::Deserialize;
 use snafu::prelude::*;
 use std::pin::Pin;
@@ -10,9 +11,12 @@ use std::{collections::HashMap, future::Future};
 use spicepod::component::dataset::Dataset;
 
 use crate::auth::AuthProvider;
+use crate::datapublisher::{AddDataResult, DataPublisher};
+use crate::dataupdate::DataUpdate;
 
 use super::DataConnector;
 
+#[derive(Clone)]
 pub struct Databricks {
     auth_provider: AuthProvider,
 }
@@ -78,15 +82,48 @@ impl DataConnector for Databricks {
     ) -> std::result::Result<Arc<dyn datafusion::datasource::TableProvider>, super::Error> {
         get_table_provider(self.auth_provider.clone(), dataset).await
     }
+
+    fn get_data_publisher(&self) -> Option<Box<dyn DataPublisher>> {
+        Some(Box::new(self.clone()))
+    }
+}
+
+impl DataPublisher for Databricks {
+    fn add_data(&self, dataset: Arc<Dataset>, data_update: DataUpdate) -> AddDataResult {
+        let auth_providers = self.auth_provider.clone();
+        Box::pin(async move {
+            let delta_table = get_delta_table(auth_providers, &dataset).await?;
+
+            let _ = DeltaOps(delta_table)
+                .write(data_update.data)
+                .with_save_mode(SaveMode::Append)
+                .await?;
+
+            Ok(())
+        })
+    }
+
+    fn name(&self) -> &str {
+        "Databricks"
+    }
 }
 
 async fn get_table_provider(
     auth_provider: AuthProvider,
     dataset: &Dataset,
 ) -> std::result::Result<Arc<dyn datafusion::datasource::TableProvider>, super::Error> {
-    
+    let delta_table: deltalake::DeltaTable = get_delta_table(auth_provider, dataset).await?;
 
-    let table_uri = resolve_table_uri(&dataset).await.context(super::UnableToGetTableProviderSnafu)?;
+    Ok(Arc::new(delta_table))
+}
+
+async fn get_delta_table(
+    auth_provider: AuthProvider,
+    dataset: &Dataset,
+) -> std::result::Result<deltalake::DeltaTable, super::Error> {
+    let table_uri = resolve_table_uri(dataset)
+        .await
+        .context(super::UnableToGetTableProviderSnafu)?;
 
     let mut storage_options = HashMap::new();
     for (key, value) in auth_provider.iter() {
@@ -96,13 +133,12 @@ async fn get_table_provider(
         storage_options.insert(key.to_string(), value.to_string());
     }
 
-    let delta_table: deltalake::DeltaTable =
-        open_table_with_storage_options(table_uri, storage_options)
-            .await
-            .boxed()
-            .context(super::UnableToGetTableProviderSnafu)?;
+    let delta_table = open_table_with_storage_options(table_uri, storage_options)
+        .await
+        .boxed()
+        .context(super::UnableToGetTableProviderSnafu)?;
 
-    Ok(Arc::new(delta_table))
+    Ok(delta_table)
 }
 
 #[derive(Deserialize)]
@@ -110,7 +146,9 @@ struct DatabricksTablesApiResponse {
     storage_location: String,
 }
 
-pub async fn resolve_table_uri(dataset: &Dataset) -> Result<String, Box<dyn std::error::Error + Send + Sync>> {
+pub async fn resolve_table_uri(
+    dataset: &Dataset,
+) -> Result<String, Box<dyn std::error::Error + Send + Sync>> {
     let endpoint = match &dataset.params {
         None => return Err("Dataset params not found".into()),
         Some(params) => match params.get("endpoint") {
@@ -123,7 +161,9 @@ pub async fn resolve_table_uri(dataset: &Dataset) -> Result<String, Box<dyn std:
 
     let auth_provider = crate::load_auth_providers().get("databricks");
 
-    let token = auth_provider.get_param("token").ok_or("Token not found in auth provider")?;
+    let token = auth_provider
+        .get_param("token")
+        .ok_or("Token not found in auth provider")?;
 
     let url = format!(
         "{}/api/2.1/unity-catalog/tables/{}",
@@ -139,6 +179,10 @@ pub async fn resolve_table_uri(dataset: &Dataset) -> Result<String, Box<dyn std:
         Ok(api_response.storage_location)
     } else {
         // Handle different status codes here if needed
-        Err(format!("Failed to retrieve databricks table URI. Status: {}", response.status()).into())
+        Err(format!(
+            "Failed to retrieve databricks table URI. Status: {}",
+            response.status()
+        )
+        .into())
     }
 }
