d5ef579a3ec0b13d13c2a99b372d4a4504be034d
Evgenii Khramkov <evgenii@spice.ai>
2024-03-06 14:27:50 +0900

    add missing import

--- crates/runtime/src/dataconnector/debug.rs
+++ crates/runtime/src/dataconnector/debug.rs
@@ -2,6 +2,8 @@ use std::collections::HashMap;
 use std::{future::Future, pin::Pin};
 use std::{sync::Arc, time::Duration};
 
+use crate::secrets::Secret;
+
 use super::{DataConnector, DataUpdate, UpdateType};
 use arrow::{
     array::{Int32Array, StringArray},
