aa54f7066132a58ce80ebde6e4dc6843658eea1a
Sevenannn <qianqliu@uw.edu>
2024-03-07 17:08:58 -0800

    Add host, user, db, password, port param for postgres

--- crates/sql_provider_datafusion/src/dbconnectionpool/postgrespool.rs
+++ crates/sql_provider_datafusion/src/dbconnectionpool/postgrespool.rs
@@ -42,8 +42,30 @@ impl
         _mode: Mode,
         _params: Arc<Option<HashMap<String, String>>>,
     ) -> Result<Self> {
-        println!("{:?}", _params);
-        let connection_string = "host=localhost user=postgres password=postgres dbname=postgres";
+        let mut host = "localhost";
+        let mut user = "postrgres";
+        let mut dbname = "postgres";
+        let mut connection_string = String::new();
+
+        if let Some(params) = _params.as_ref() {
+            if let Some(pg_host) = params.get("pg_host") {
+                host = pg_host;
+            }
+            if let Some(pg_user) = params.get("pg_user") {
+                user = pg_user;
+            }
+            if let Some(pg_db) = params.get("pg_db") {
+                dbname = pg_db;
+            }
+            if let Some(pg_pass) = params.get("pg_pass") {
+                connection_string.push_str(format!("password={pg_pass} ").as_str());
+            }
+            if let Some(pg_port) = params.get("pg_port") {
+                connection_string.push_str(format!("port={pg_port} ").as_str());
+            }
+        }
+
+        connection_string.push_str(format!("host={host} user={user} dbname={dbname}").as_str());
 
         let manager = PostgresConnectionManager::new_from_stringlike(connection_string, NoTls)
             .context(ConnectionPoolSnafu)?;
