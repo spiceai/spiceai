301c40132f743b38f755ad29af87750400b7de59
jeadie <jack@spice.ai>
2024-03-14 20:49:35 +1000

    add flightsql as usable data connector in spicepod

--- crates/runtime/src/lib.rs
+++ crates/runtime/src/lib.rs
@@ -284,6 +284,16 @@ impl Runtime {
                     data_connector: source,
                 })?,
             ))),
+            "flightsql" => Ok(Some(Box::new(
+                dataconnector::flightsql::FlightSQL::new(
+                    secrets_provider.get_secret(source).await,
+                    params,
+                )
+                .await
+                .context(UnableToInitializeDataConnectorSnafu {
+                    data_connector: source,
+                })?,
+            ))),
             "postgres" => Ok(Some(Box::new(
                 dataconnector::postgres::Postgres::new(
                     secrets_provider.get_secret(source).await,
