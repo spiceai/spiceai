fc038632881271bcc5325e957b92cb6e627ca40a
jeadie <jack@spice.ai>
2024-03-14 13:29:55 +1000

    for datasets, add CSV format and get status of table from datafusion

--- crates/runtime/src/http/v1.rs
+++ crates/runtime/src/http/v1.rs
@@ -1,3 +1,32 @@
+use csv::Writer;
+use serde::{Deserialize, Serialize};
+
+#[derive(Default, Debug, Serialize, Deserialize)]
+#[serde(rename_all = "lowercase")]
+pub enum Format {
+    #[default]
+    Json,
+    Csv,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+#[serde(rename_all = "lowercase")]
+pub enum Status {
+    Initializing,
+    Disabled,
+    Ready,
+    Error,
+}
+
+fn convert_entry_to_csv<T: Serialize>(entries: &[T]) -> Result<String, Box<dyn std::error::Error>> {
+    let mut w = Writer::from_writer(vec![]);
+    for e in entries {
+        w.serialize(e)?;
+    }
+    w.flush()?;
+    Ok(String::from_utf8(w.into_inner()?)?)
+}
+
 pub(crate) mod query {
     use std::sync::Arc;
 
@@ -71,10 +100,20 @@ pub(crate) mod datasets {
     use std::sync::Arc;
 
     use app::App;
-    use axum::{extract::Query, Extension, Json};
-    use serde::Deserialize;
+    use axum::{
+        extract::Query,
+        http::status,
+        response::{IntoResponse, Response},
+        Extension, Json,
+    };
+    use serde::{Deserialize, Serialize};
     use spicepod::component::dataset::Dataset;
     use tokio::sync::RwLock;
+    use tract_core::tract_data::itertools::Itertools;
+
+    use crate::datafusion::DataFusion;
+
+    use super::{convert_entry_to_csv, Format};
 
     #[derive(Debug, Deserialize)]
     pub(crate) struct QueryParams {
@@ -83,17 +122,38 @@ pub(crate) mod datasets {
         #[serde(default)]
         remove_views: bool,
         // TODO: Implement status checking.
-        // #[serde(default)]
-        // status: bool,
+        #[serde(default)]
+        status: bool,
+
+        #[serde(default)]
+        format: Format,
+    }
+
+    #[derive(Debug, Serialize, Deserialize)]
+    #[serde(rename_all = "lowercase")]
+    pub(crate) struct DatasetResponseItem {
+        pub from: String,
+        pub name: String,
+        pub replication_enabled: bool,
+        pub acceleration_enabled: bool,
+        pub depends_on: Option<String>,
+
+        #[serde(skip_serializing_if = "Option::is_none")]
+        pub status: Option<super::Status>,
     }
 
     pub(crate) async fn get(
         Extension(app): Extension<Arc<RwLock<Option<App>>>>,
+        Extension(df): Extension<Arc<RwLock<DataFusion>>>,
         Query(params): Query<QueryParams>,
-    ) -> Json<Vec<Dataset>> {
+    ) -> Response {
         let app_lock = app.read().await;
         let Some(readable_app) = &*app_lock else {
-            return Json(vec![]);
+            return (
+                status::StatusCode::INTERNAL_SERVER_ERROR,
+                Json::<Vec<DatasetResponseItem>>(vec![]),
+            )
+                .into_response();
         };
 
         let mut datasets: Vec<Dataset> = match params.source {
@@ -110,7 +170,42 @@ pub(crate) mod datasets {
             datasets.retain(|d| !d.is_view());
         }
 
-        Json(datasets)
+        let df_read = df.read().await;
+
+        let resp = datasets
+            .iter()
+            .map(|d| DatasetResponseItem {
+                from: d.from.clone(),
+                name: d.name.clone(),
+                replication_enabled: d.replication.as_ref().is_some_and(|f| f.enabled),
+                acceleration_enabled: d.acceleration.as_ref().is_some_and(|f| f.enabled),
+                depends_on: if d.depends_on.is_empty() {
+                    None
+                } else {
+                    Some(d.depends_on.join(", "))
+                },
+                status: if params.status {
+                    if df_read.table_exists(d.name.as_str()) {
+                        Some(super::Status::Ready)
+                    } else {
+                        Some(super::Status::Error)
+                    }
+                } else {
+                    None
+                },
+            })
+            .collect_vec();
+
+        match params.format {
+            Format::Json => (status::StatusCode::OK, Json(resp)).into_response(),
+            Format::Csv => match convert_entry_to_csv(&resp) {
+                Ok(csv) => (status::StatusCode::OK, csv).into_response(),
+                Err(e) => {
+                    tracing::error!("Error converting to CSV: {e}");
+                    (status::StatusCode::INTERNAL_SERVER_ERROR, e.to_string()).into_response()
+                }
+            },
+        }
     }
 }
 
@@ -129,12 +224,14 @@ pub(crate) mod models {
 
     use crate::model::Model;
 
+    use super::Format;
+
     #[derive(Debug, Deserialize)]
     pub(crate) struct QueryParams {
         // TODO: Implement status checking.
         // #[serde(default)]
         // _status: bool,
-        #[serde(default = "default_format")]
+        #[serde(default)]
         format: Format,
     }
 
@@ -144,17 +241,6 @@ pub(crate) mod models {
         pub name: String,
         pub from: String,
         pub datasets: Option<Vec<String>>,
-    }
-
-    #[derive(Debug, Serialize, Deserialize)]
-    #[serde(rename_all = "lowercase")]
-    pub enum Format {
-        Json,
-        Csv,
-    }
-
-    fn default_format() -> Format {
-        Format::Json
     }
 
     pub(crate) async fn get(
