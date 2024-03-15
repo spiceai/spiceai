308c564607060ea51edd0a4fc893a45688f2e521
Phillip LeBlanc <phillip@leblanc.tech>
2024-03-15 14:31:48 +0900

    consistent dev versioning

--- bin/spiced/src/main.rs
+++ bin/spiced/src/main.rs
@@ -17,11 +17,17 @@ fn main() {
         if cfg!(feature = "release") {
             println!("v{}", env!("CARGO_PKG_VERSION"));
         } else {
-            println!(
+            print!(
                 "v{}-rc.{}",
                 env!("CARGO_PKG_VERSION"),
                 env!("GIT_COMMIT_HASH")
             );
+
+            if cfg!(feature = "dev") {
+                print!("-dev");
+            }
+
+            println!();
         };
 
         return;
