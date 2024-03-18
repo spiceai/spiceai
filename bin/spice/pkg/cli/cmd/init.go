7edb062d953125389a7c93f31110d1ad813698cd
Luke Kim <80174+lukekim@users.noreply.github.com>
2024-03-18 10:15:18 +0900

    Make `spice init` work like other tools - Create a subdirectory of the app name - If no argument supplied, detect working directory and default to that name

--- bin/spice/pkg/cli/cmd/init.go
+++ bin/spice/pkg/cli/cmd/init.go
@@ -3,6 +3,7 @@ package cmd
 import (
 	"fmt"
 	"os"
+	"path"
 	"strings"
 
 	"github.com/logrusorgru/aurora"
@@ -15,14 +16,52 @@ var initCmd = &cobra.Command{
 	Use:   "init",
 	Short: "Initialize Spice app - initializes a new Spice app",
 	Example: `
+spice init
 spice init <spice app name>
 spice init my_app
 `,
-	Args: cobra.MinimumNArgs(1),
 	Run: func(cmd *cobra.Command, args []string) {
-		podName := args[0]
-		podPath := "./spicepod.yaml"
-		if _, err := os.Stat(podPath); !os.IsNotExist(err) {
+		var spicepodName string
+		spicepodDir := "."
+
+		if len(args) < 1 {
+			wd, err := os.Getwd()
+			if err != nil {
+				cmd.PrintErrf("Error getting current working directory: %s\n", err.Error())
+				return
+			}
+			dirName := path.Base(wd)
+
+			cmd.Printf("name: (%s)? ", dirName)
+			fmt.Scanf("%s", &spicepodName)
+			if strings.TrimSpace(spicepodName) == "" {
+				spicepodName = dirName
+			}
+		} else {
+			spicepodName = args[0]
+			fs, err := os.Stat(spicepodName)
+			if err != nil {
+				if os.IsNotExist(err) {
+					err = os.Mkdir(spicepodName, 0766)
+					if err != nil {
+						cmd.PrintErrf("Error creating directory: %s\n", err.Error())
+						return
+					}
+				} else {
+					cmd.PrintErrf("Error checking if directory exists: %s\n", err.Error())
+					return
+				}
+			} else {
+				if !fs.IsDir() {
+					cmd.PrintErrf("Error: %s exists and is not a directory\n", spicepodName)
+					return
+				}
+			}
+			spicepodDir = spicepodName
+		}
+
+		spicepodPath := path.Join(spicepodDir, "spicepod.yaml")
+		if _, err := os.Stat(spicepodPath); !os.IsNotExist(err) {
 			cmd.Println("spicepod.yaml already exists. Replace (y/n)?")
 			var confirm string
 			fmt.Scanf("%s", &confirm)
@@ -32,7 +71,7 @@ spice init my_app
 		}
 
 		skeletonPod := &api.Spicepod{
-			Name:    podName,
+			Name:    spicepodName,
 			Version: "v1beta1",
 			Kind:    "Spicepod",
 		}
@@ -43,7 +82,7 @@ spice init my_app
 			return
 		}
 
-		err = os.WriteFile(podPath, skeletonPodContentBytes, 0766)
+		err = os.WriteFile(spicepodPath, skeletonPodContentBytes, 0766)
 		if err != nil {
 			cmd.Println(err)
 			return
