77f70ec36564c1c357bbe4fc6f974467ee92b083
Luke Kim <80174+lukekim@users.noreply.github.com>
2024-03-18 13:21:24 +0900

    `spice add` automatically creates spicepod.yaml if it does not exist

---
+++ bin/spice/pkg/spicepod/spicepod.go
@@ -0,0 +1,51 @@
+package spicepod
+
+import (
+	"fmt"
+	"os"
+	"path"
+
+	"github.com/spiceai/spiceai/bin/spice/pkg/api"
+	"gopkg.in/yaml.v2"
+)
+
+func CreateManifest(name string, spicepodDir string) (string, error) {
+	fs, err := os.Stat(name)
+	if err != nil {
+		if os.IsNotExist(err) {
+			err = os.Mkdir(name, 0766)
+			if err != nil {
+				return "", fmt.Errorf("Error creating directory: %w", err)
+			}
+			fs, err = os.Stat(name)
+			if err != nil {
+				return "", fmt.Errorf("Error checking if directory exists: %w", err)
+			}
+		} else {
+			return "", fmt.Errorf("Error checking if directory exists: %w", err)
+		}
+	}
+
+	if !fs.IsDir() {
+		return "", fmt.Errorf("Error: %s exists and is not a directory", name)
+	}
+
+	skeletonPod := &api.Spicepod{
+		Name:    name,
+		Version: "v1beta1",
+		Kind:    "Spicepod",
+	}
+
+	skeletonPodContentBytes, err := yaml.Marshal(skeletonPod)
+	if err != nil {
+		return "", fmt.Errorf("Error marshalling spicepod: %w", err)
+	}
+
+	spicepodPath := path.Join(spicepodDir, "spicepod.yaml")
+	err = os.WriteFile(spicepodPath, skeletonPodContentBytes, 0766)
+	if err != nil {
+		return "", fmt.Errorf("Error writing spicepod.yaml: %w", err)
+	}
+
+	return spicepodPath, nil
+}
