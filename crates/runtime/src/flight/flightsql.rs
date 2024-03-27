5d44389652d54cefda3a871c002970f14aaa6044
Sergei Grebnov <sergei.grebnov@gmail.com>
2024-03-27 00:26:03 -0700

    Add CommandGetTableTypes for Tableau min support (#965)

--- crates/runtime/src/flight/flightsql.rs
+++ crates/runtime/src/flight/flightsql.rs
@@ -1,6 +1,7 @@
 pub(crate) mod get_catalogs;
 pub(crate) mod get_schemas;
 pub(crate) mod get_sql_info;
+pub(crate) mod get_table_types;
 pub(crate) mod get_tables;
 pub(crate) mod prepared_statement_query;
 pub(crate) mod statement_query;
