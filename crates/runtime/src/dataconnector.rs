14c6fa68370ad6082212a3a9e93bbf581efcdab7
Phillip LeBlanc <phillip@leblanc.tech>
2024-04-09 17:44:26 +0900

    S3 data connector

--- crates/runtime/src/dataconnector.rs
+++ crates/runtime/src/dataconnector.rs
@@ -44,7 +44,7 @@ pub mod localhost;
 // pub mod mysql;
 // #[cfg(feature = "postgres")]
 // pub mod postgres;
-// pub mod s3;
+pub mod s3;
 // pub mod spiceai;
 
 #[derive(Debug, Snafu)]
@@ -111,7 +111,7 @@ pub async fn register_all() {
     register_connector_factory("databricks", Databricks::create).await;
     //register_connector_factory("dremio", Dremio::create).await;
     // register_connector_factory("flightsql", flightsql::FlightSQL::create).await;
-    // register_connector_factory("s3", s3::S3::create).await;
+    register_connector_factory("s3", s3::S3::create).await;
     // register_connector_factory("spiceai", spiceai::SpiceAI::create).await;
     // #[cfg(feature = "mysql")]
     // register_connector_factory("mysql", mysql::MySQL::create).await;
--- crates/runtime/src/dataconnector/s3.rs
+++ crates/runtime/src/dataconnector/s3.rs
@@ -14,20 +14,22 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
+use super::{AnyErrorResult, DataConnector, DataConnectorFactory};
 use async_trait::async_trait;
+use datafusion::datasource::TableProvider;
+use datafusion::error::DataFusionError;
 use datafusion::execution::context::SessionContext;
 use datafusion::execution::options::ParquetReadOptions;
 use object_store::aws::AmazonS3Builder;
+use object_store::ObjectStore;
 use secrets::Secret;
+use snafu::prelude::*;
+use spicepod::component::dataset::Dataset;
+use std::any::Any;
 use std::pin::Pin;
 use std::sync::Arc;
 use std::{collections::HashMap, future::Future};
 use url::Url;
-
-use spicepod::component::dataset::Dataset;
-
-use super::{DataConnector, DataConnectorFactory};
-use snafu::prelude::*;
 
 #[derive(Debug, Snafu)]
 pub enum Error {
@@ -37,25 +39,39 @@ pub enum Error {
     #[snafu(display("No AWS access key provided for credentials"))]
     NoAccessKey,
 
-    #[snafu(display("Unable to parse URL: {url}"))]
-    UnableToParseURL { url: String },
+    #[snafu(display("Unable to parse URL {url}: {source}"))]
+    UnableToParseURL {
+        url: String,
+        source: url::ParseError,
+    },
+
+    UnableToGetReadProvider {
+        source: Box<dyn std::error::Error + Send + Sync>,
+    },
+
+    UnableToGetReadWriteProvider {
+        source: Box<dyn std::error::Error + Send + Sync>,
+    },
+
+    UnableToBuildObjectStore {
+        source: object_store::Error,
+    },
+
+    #[snafu(display("The S3 URL is missing a forward slash: {url}"))]
+    MissingForwardSlash {
+        url: String,
+    },
+
+    ObjectStoreNotImplemented,
+
+    UnableToBuildLogicalPlan {
+        source: DataFusionError,
+    },
 }
 
 pub struct S3 {
     secret: Option<Secret>,
     params: HashMap<String, String>,
-}
-impl S3 {
-    #[must_use]
-    pub fn get_from_params(
-        params: &Arc<Option<HashMap<String, String>>>,
-        key: &str,
-    ) -> Option<String> {
-        params
-            .as_ref()
-            .as_ref()
-            .and_then(|params| params.get(key).cloned())
-    }
 }
 
 impl DataConnectorFactory for S3 {
@@ -68,126 +84,78 @@ impl DataConnectorFactory for S3 {
                 secret,
                 params: params.as_ref().clone().map_or_else(HashMap::new, |x| x),
             };
-            Ok(Box::new(s3) as Box<dyn DataConnector>)
+            Ok(Arc::new(s3) as Arc<dyn DataConnector>)
         })
     }
 }
 
 #[async_trait]
 impl DataConnector for S3 {
-    fn has_object_store(&self) -> bool {
-        true
+    fn as_any(&self) -> &dyn Any {
+        self
     }
 
     fn get_object_store(
         &self,
         dataset: &Dataset,
-    ) -> std::result::Result<(Url, Arc<dyn object_store::ObjectStore + 'static>), super::Error>
-    {
-        let from = dataset.from.clone();
-        let parts = from.clone().replace("s3://", "");
+    ) -> Option<AnyErrorResult<(Url, Arc<dyn ObjectStore + 'static>)>> {
+        let result: AnyErrorResult<(Url, Arc<dyn ObjectStore + 'static>)> = (|| {
+            let url = dataset.from.clone();
+            let parts = url.clone().replace("s3://", "");
 
-        let bucket =
-            parts
+            let bucket = parts
                 .split('/')
                 .next()
-                .ok_or_else(|| super::Error::UnableToGetTableProvider {
-                    source: Error::UnableToParseURL {
-                        url: dataset.from.clone(),
-                    }
-                    .into(),
-                })?;
+                .ok_or_else(|| MissingForwardSlashSnafu { url: url.clone() }.build())?;
 
-        let mut s3_builder = AmazonS3Builder::new()
-            .with_bucket_name(bucket)
-            .with_allow_http(true);
+            let mut s3_builder = AmazonS3Builder::new()
+                .with_bucket_name(bucket)
+                .with_allow_http(true);
 
-        if let Some(region) = self.params.get("region") {
-            s3_builder = s3_builder.with_region(region);
-        }
-        if let Some(endpoint) = self.params.get("endpoint") {
-            s3_builder = s3_builder.with_endpoint(endpoint);
-        }
-        if let Some(secret) = &self.secret {
-            if let Some(key) = secret.get("key") {
-                s3_builder = s3_builder.with_access_key_id(key);
-            };
-            if let Some(secret) = secret.get("secret") {
-                s3_builder = s3_builder.with_secret_access_key(secret);
-            };
-        } else {
-            s3_builder = s3_builder.with_skip_signature(true);
-        };
-
-        let s3 = s3_builder
-            .build()
-            .map_err(|e| super::Error::UnableToCreateDataConnector { source: e.into() })?;
-
-        let s3_url = Url::parse(&dataset.from)
-            .map_err(|e| super::Error::UnableToGetTableProvider { source: e.into() })?;
-
-        Ok((s3_url, Arc::new(s3)))
-    }
-
-    fn supports_data_streaming(&self, _dataset: &Dataset) -> bool {
-        false
-    }
-
-    fn get_all_data(
-        &self,
-        dataset: &Dataset,
-    ) -> Pin<Box<dyn Future<Output = Vec<arrow::record_batch::RecordBatch>> + Send>> {
-        let path = dataset.path();
-
-        let ctx = SessionContext::new();
-        if let Ok((url, store)) = self.get_object_store(dataset) {
-            let _ = ctx.runtime_env().register_object_store(&url, store);
-        } else {
-            return Box::pin(async move { vec![] });
-        }
-
-        Box::pin(async move {
-            match ctx
-                .read_parquet(format!("s3:{path}"), ParquetReadOptions::default())
-                .await
-            {
-                Ok(df) => match df.collect().await {
-                    Ok(batches) => batches,
-                    Err(e) => {
-                        tracing::error!("Failed to retrieve data from S3: {e}");
-                        vec![]
-                    }
-                },
-                Err(e) => {
-                    tracing::error!("Failed to read parquet from S3: {path}: {e}");
-                    vec![]
-                }
+            if let Some(region) = self.params.get("region") {
+                s3_builder = s3_builder.with_region(region);
             }
-        })
+            if let Some(endpoint) = self.params.get("endpoint") {
+                s3_builder = s3_builder.with_endpoint(endpoint);
+            }
+            if let Some(secret) = &self.secret {
+                if let Some(key) = secret.get("key") {
+                    s3_builder = s3_builder.with_access_key_id(key);
+                };
+                if let Some(secret) = secret.get("secret") {
+                    s3_builder = s3_builder.with_secret_access_key(secret);
+                };
+            } else {
+                s3_builder = s3_builder.with_skip_signature(true);
+            };
+
+            let s3 = s3_builder.build().context(UnableToBuildObjectStoreSnafu)?;
+
+            let s3_url = Url::parse(&url).context(UnableToParseURLSnafu { url: url.clone() })?;
+
+            Ok((s3_url, Arc::new(s3) as Arc<dyn ObjectStore>))
+        })();
+
+        Some(result)
     }
 
-    fn has_table_provider(&self) -> bool {
-        true
-    }
-
-    async fn get_table_provider(
+    async fn read_provider(
         &self,
         dataset: &Dataset,
-    ) -> std::result::Result<Arc<dyn datafusion::datasource::TableProvider>, super::Error> {
+    ) -> super::AnyErrorResult<Arc<dyn TableProvider>> {
         let ctx = SessionContext::new();
 
         let (url, s3) = self
             .get_object_store(dataset)
-            .map_err(|e| super::Error::UnableToGetTableProvider { source: e.into() })?;
+            .ok_or_else(|| ObjectStoreNotImplementedSnafu.build())?
+            .context(UnableToGetReadProviderSnafu)?;
 
         let _ = ctx.runtime_env().register_object_store(&url, s3);
 
         let df = ctx
             .read_parquet(&dataset.from, ParquetReadOptions::default())
             .await
-            .map_err(|e: datafusion::error::DataFusionError| {
-                super::Error::UnableToGetTableProvider { source: e.into() }
-            })?;
+            .context(UnableToBuildLogicalPlanSnafu)?;
 
         Ok(df.into_view())
     }
