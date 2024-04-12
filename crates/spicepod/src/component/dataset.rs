db79c6f8012c577423cf33d97ad7675d2331a782
yfu <fevin86@gmail.com>
2024-04-12 14:18:18 +1000

    retention configuration

--- crates/spicepod/src/component/dataset.rs
+++ crates/spicepod/src/component/dataset.rs
@@ -258,6 +258,15 @@ pub mod acceleration {
         File,
     }
 
+    #[derive(Debug, Clone, Serialize, Deserialize, PartialEq, Default)]
+    #[serde(rename_all = "snake_case")]
+    pub enum TimeUnit {
+        #[default]
+        UnixSeconds,
+        UnixMillis,
+        Iso8601,
+    }
+
     impl Display for Mode {
         fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
             write!(f, "{self:?}")
@@ -292,9 +301,28 @@ pub mod acceleration {
 
         #[serde(default, skip_serializing_if = "Option::is_none")]
         pub engine_secret: Option<String>,
+
+        #[serde(default, skip_serializing_if = "Option::is_none")]
+        pub time_column: Option<String>,
+
+        #[serde(default)]
+        pub time_format: TimeUnit,
+
+        #[serde(default, skip_serializing_if = "Option::is_none")]
+        pub retention_period: Option<String>,
+
+        #[serde(default, skip_serializing_if = "Option::is_none")]
+        pub retention_check_interval: Option<String>,
+
+        #[serde(default = "default_false")]
+        pub retention_enabled: bool,
     }
 
     const fn default_true() -> bool {
+        true
+    }
+
+    const fn default_false() -> bool {
         true
     }
 
