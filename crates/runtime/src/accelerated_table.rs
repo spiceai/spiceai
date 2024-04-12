88c1cc1bf27ac341fda2217680de0ca745d8ca7d
Phillip LeBlanc <phillip@leblanc.tech>
2024-04-12 17:58:46 +0900

    Upgrade Arrow, DataFusion, Tonic

--- crates/runtime/src/accelerated_table.rs
+++ crates/runtime/src/accelerated_table.rs
@@ -8,7 +8,7 @@ use datafusion::error::Result as DataFusionResult;
 use datafusion::execution::context::SessionState;
 use datafusion::logical_expr::TableProviderFilterPushDown;
 use datafusion::physical_plan::union::UnionExec;
-use datafusion::physical_plan::{collect, ExecutionPlan};
+use datafusion::physical_plan::{collect, ExecutionPlan, ExecutionPlanProperties};
 use datafusion::{
     datasource::{TableProvider, TableType},
     execution::context::SessionContext,
