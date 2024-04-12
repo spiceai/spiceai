b38bc95226770fd1799d59b4f410ea01d2af57e0
yfu <fevin86@gmail.com>
2024-04-12 14:48:09 +1000

    retention configuration in spicepod (#1093)
    * retention configuration
    
    * update the time format enum
    
    * no need a default false
    
    * make time format optional too

--- crates/spicepod/src/component/dataset.rs
+++ crates/spicepod/src/component/dataset.rs
@@ -258,6 +258,15 @@ pub mod acceleration {
         File,
     }
 
+    #[derive(Debug, Clone, Serialize, Deserialize, PartialEq, Default)]
+    #[serde(rename_all = "snake_case")]
+    pub enum TimeFormat {
+        #[default]
+        UnixSeconds,
+        UnixMillis,
+        Iso8601,
+    }
+
     impl Display for Mode {
         fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
             write!(f, "{self:?}")
@@ -292,6 +301,21 @@ pub mod acceleration {
 
         #[serde(default, skip_serializing_if = "Option::is_none")]
         pub engine_secret: Option<String>,
+
+        #[serde(default, skip_serializing_if = "Option::is_none")]
+        pub time_column: Option<String>,
+
+        #[serde(default, skip_serializing_if = "Option::is_none")]
+        pub time_format: Option<TimeFormat>,
+
+        #[serde(default, skip_serializing_if = "Option::is_none")]
+        pub retention_period: Option<String>,
+
+        #[serde(default, skip_serializing_if = "Option::is_none")]
+        pub retention_check_interval: Option<String>,
+
+        #[serde(default)]
+        pub retention_enabled: bool,
     }
 
     const fn default_true() -> bool {
