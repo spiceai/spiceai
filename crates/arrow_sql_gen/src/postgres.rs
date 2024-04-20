8f3da59a15f2478417668302c5bd5548b6464a2a
ahirner <a.hirner@gmail.com>
2024-04-20 12:16:39 +0200

    support TIMESTAMPTZ, not tested if tokio-postgres does it right

--- crates/arrow_sql_gen/src/postgres.rs
+++ crates/arrow_sql_gen/src/postgres.rs
@@ -284,7 +284,7 @@ pub fn rows_to_arrow(rows: &[Row]) -> Result<RecordBatch> {
                     };
                     dec_builder.append_value(v_i128);
                 }
-                Type::TIMESTAMP => {
+                ref pg_type @ (Type::TIMESTAMP | Type::TIMESTAMPTZ) => {
                     let Some(builder) = builder else {
                         return NoBuilderForIndexSnafu { index: i }.fail();
                     };
@@ -297,11 +297,11 @@ pub fn rows_to_arrow(rows: &[Row]) -> Result<RecordBatch> {
                         }
                         .fail();
                     };
-                    let v = row.try_get::<usize, Option<SystemTime>>(i).context(
-                        FailedToGetRowValueSnafu {
-                            pg_type: Type::TIMESTAMP,
-                        },
-                    )?;
+                    let v = row
+                        .try_get::<usize, Option<SystemTime>>(i)
+                        .with_context(|_| FailedToGetRowValueSnafu {
+                            pg_type: pg_type.clone(),
+                        })?;
 
                     match v {
                         Some(v) => {
@@ -449,7 +449,9 @@ fn map_column_type_to_data_type(column_type: &Type) -> Option<DataType> {
         // Inspect the scale from the first row. Precision will always be 38 for Decimal128.
         Type::NUMERIC => None,
         // We get a SystemTime that we can always convert into milliseconds
-        Type::TIMESTAMP => Some(DataType::Timestamp(TimeUnit::Millisecond, None)),
+        Type::TIMESTAMP | Type::TIMESTAMPTZ => {
+            Some(DataType::Timestamp(TimeUnit::Millisecond, None))
+        }
         Type::DATE => Some(DataType::Date32),
         Type::UUID => Some(DataType::FixedSizeBinary(16)),
         Type::INT2_ARRAY => Some(DataType::List(Arc::new(Field::new(
