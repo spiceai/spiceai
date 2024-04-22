fc6543ad04c9de9044de7cd4a00e92c6273189aa
sgrebnov <sergei.grebnov@gmail.com>
2024-04-21 20:47:12 -0700

    Add `windows-static` to include vcruntime libraries

--- bin/spiced/build.rs
+++ bin/spiced/build.rs
@@ -26,4 +26,7 @@ fn main() {
         );
 
     println!("cargo:rustc-env=GIT_COMMIT_HASH={git_hash}");
+
+    #[cfg(all(windows, feature = "windows-static"))]
+    static_vcruntime::metabuild();
 }
