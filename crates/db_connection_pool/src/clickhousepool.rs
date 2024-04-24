5a7432e5496bacd2314fe38dcac150a049282321
Kirill Khramkov <hramkir@gmail.com>
2024-04-24 22:22:57 +0400

    Clickhouse refactor connection code and set secure option

--- crates/db_connection_pool/src/clickhousepool.rs
+++ crates/db_connection_pool/src/clickhousepool.rs
@@ -14,7 +14,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-use std::{collections::HashMap, str::FromStr, sync::Arc};
+use std::{collections::HashMap, str::FromStr, sync::Arc, time::Duration};
 
 use async_trait::async_trait;
 use clickhouse_rs::{ClientHandle, Options, Pool};
@@ -42,8 +42,14 @@ pub enum Error {
         source: clickhouse_rs::errors::Error,
     },
 
-    #[snafu(display("Invalid parameter: {parameter_name}"))]
-    InvalidParameterError { parameter_name: String },
+    #[snafu(display("ParametersEmptyError"))]
+    ParametersEmptyError {},
+
+    #[snafu(display("Required parameter was not provided: {parameter_name}"))]
+    NotEnoughParametersForConnection { parameter_name: String },
+
+    #[snafu(display("Invalid root cert path: {path}"))]
+    InvalidRootCertPathError { path: String },
 }
 
 pub struct ClickhouseConnectionPool {
@@ -61,45 +67,11 @@ impl ClickhouseConnectionPool {
         params: Arc<Option<HashMap<String, String>>>,
         secret: Option<Secret>,
     ) -> Result<Self> {
-        let mut options = Options::default();
-        let mut host: &str = "localhost";
-        let mut user: &str = "default";
-        let mut db: &str = "default";
-        let mut pass: String = "password".to_string();
-        let mut tcp_port: &str = "9000";
-
-        if let Some(params) = params.as_ref() {
-            if let Some(clickhouse_connection_string) = get_secret_or_param(
-                params,
-                &secret,
-                "clickhouse_connection_string_key",
-                "clickhouse_connection_string",
-            ) {
-                options = Options::from_str(&clickhouse_connection_string)
-                    .context(InvalidConnectionStringSnafu)?;
-            } else {
-                if let Some(clickhouse_host) = params.get("clickhouse_host") {
-                    host = clickhouse_host;
-                }
-                if let Some(clickhouse_user) = params.get("clickhouse_user") {
-                    user = clickhouse_user;
-                }
-                if let Some(clickhouse_db) = params.get("clickhouse_db") {
-                    db = clickhouse_db;
-                }
-                if let Some(clickhouse_pass) =
-                    get_secret_or_param(params, &secret, "clickhouse_pass_key", "clickhouse_pass")
-                {
-                    pass = clickhouse_pass;
-                }
-                if let Some(clickhouse_tcp_port) = params.get("clickhouse_tcp_port") {
-                    tcp_port = clickhouse_tcp_port;
-                }
-                let connection_string = format!("tcp://{user}:{pass}@{host}:{tcp_port}/{db}");
-                options =
-                    Options::from_str(&connection_string).context(InvalidConnectionStringSnafu)?;
-            }
-        }
+        let params = match params.as_ref() {
+            Some(params) => params,
+            None => ParametersEmptySnafu {}.fail()?,
+        };
+        let options = get_config_from_params(params, &secret)?;
 
         let pool = Pool::new(options);
 
@@ -107,6 +79,87 @@ impl ClickhouseConnectionPool {
             pool: Arc::new(pool),
         })
     }
+}
+
+fn get_config_from_params(
+    params: &HashMap<String, String>,
+    secret: &Option<Secret>,
+) -> Result<Options> {
+    let options: Option<Options>;
+    if let Some(clickhouse_connection_string) = get_secret_or_param(
+        params,
+        &secret,
+        "clickhouse_connection_string_key",
+        "clickhouse_connection_string",
+    ) {
+        let mut new_options = Options::from_str(&clickhouse_connection_string)
+            .context(InvalidConnectionStringSnafu)?;
+        if clickhouse_connection_string
+            .find("connection_timeout")
+            .is_none()
+        {
+            // Default timeouf of 500ms is not enough in some cases.
+            new_options = new_options.connection_timeout(Duration::from_secs(5));
+        }
+        options = Some(new_options);
+    } else {
+        let mut connection_opts: Vec<String> = vec![];
+        let keys = vec![
+            "clickhouse_user",
+            "clickhouse_host",
+            "clickhouse_tcp_port",
+            "clickhouse_db",
+        ];
+        for key in keys.iter() {
+            let value = params.get(*key);
+            match value {
+                Some(value) => connection_opts.push(value.clone()),
+                None => NotEnoughParametersForConnectionSnafu {
+                    parameter_name: key.to_string(),
+                }
+                .fail()?,
+            }
+        }
+        let password =
+            get_secret_or_param(params, &secret, "clickhouse_pass_key", "clickhouse_pass");
+        match password {
+            Some(password) => connection_opts.insert(1, password),
+            None => NotEnoughParametersForConnectionSnafu {
+                parameter_name: "clickhouse_pass".to_string(),
+            }
+            .fail()?,
+        }
+        let connection_string = format!(
+            "tcp://{}:{}@{}:{}/{}",
+            connection_opts[0],
+            connection_opts[1],
+            connection_opts[2],
+            connection_opts[3],
+            connection_opts[4]
+        );
+        // Default timeouf of 500ms is not enough
+        let new_options = Options::from_str(&connection_string)
+            .context(InvalidConnectionStringSnafu)?
+            .connection_timeout(Duration::from_secs(5));
+        options = Some(new_options);
+    }
+    let mut options = options.ok_or(Error::NotEnoughParametersForConnection {
+        parameter_name: "clickhouse_connection_string".to_string(),
+    })?;
+    if let Some(clickhouse_secure) = params.get("clickhouse_secure") {
+        options = options.secure(clickhouse_secure.parse().unwrap_or(false));
+    }
+
+    if let Some(clickhouse_sslrootcert) = params.get("clickhouse_sslrootcert") {
+        if !std::path::Path::new(clickhouse_sslrootcert).exists() {
+            InvalidRootCertPathSnafu {
+                path: clickhouse_sslrootcert,
+            }
+            .fail()?;
+        }
+    }
+
+    Ok(options)
 }
 
 #[must_use]
