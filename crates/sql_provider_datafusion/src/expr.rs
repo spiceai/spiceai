b83303d0fb064d6a0da2477aeb0da1f2f708bae4
David Stancu <dstancu@barracuda.com>
2024-05-08 00:09:14 -0400

    sql_provider_datafusion: Support TimestampMicrosecond, Date32, Date64 (#1329)
    * sql_provider_datafusion / ScalarValue::TimestampMicrosecond
    
    * support arrow date32 and date64
    
    * Fix linting
    
    ---------
    
    Co-authored-by: Luke Kim <80174+lukekim@users.noreply.github.com>
    Co-authored-by: Aurash Behbahani <aurashb@gmail.com>
    Co-authored-by: Phillip LeBlanc <phillip@leblanc.tech>

--- crates/sql_provider_datafusion/src/expr.rs
+++ crates/sql_provider_datafusion/src/expr.rs
@@ -14,7 +14,12 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-use datafusion::{logical_expr::Expr, scalar::ScalarValue};
+use datafusion::{
+    logical_expr::{Cast, Expr},
+    scalar::ScalarValue,
+};
+
+pub const SECONDS_IN_DAY: i32 = 86_400;
 
 #[derive(Debug, snafu::Snafu)]
 pub enum Error {
@@ -35,7 +40,6 @@ pub enum Engine {
     ODBC,
 }
 
-#[allow(clippy::needless_pass_by_value)]
 pub fn to_sql_with_engine(expr: &Expr, engine: Option<Engine>) -> Result<String> {
     match expr {
         Expr::BinaryExpr(binary_expr) => {
@@ -59,38 +63,24 @@ pub fn to_sql_with_engine(expr: &Expr, engine: Option<Engine>) -> Result<String>
             Some(Engine::Spark | Engine::ODBC) => Ok(format!("{name}")),
             _ => Ok(format!("\"{name}\"")),
         },
-        Expr::Cast(cast) => {
-            match cast.data_type {
-                arrow::datatypes::DataType::Timestamp(_, Some(_) | None) => match engine {
-                    None => Ok(format!(
-                        "CAST({} AS TIMESTAMPTZ)",
-                        to_sql_with_engine(&cast.expr, engine)?,
-                    )),
-                    Some(Engine::ODBC) => Ok(format!(
-                        "CAST({} AS TIMESTAMP)",
-                        to_sql_with_engine(&cast.expr, engine)?,
-                    )),
-                    // This needs to match the timestamp conversion below
-                    Some(Engine::DuckDB) => Ok(format!(
-                        "TO_TIMESTAMP(EPOCH(CAST({} AS TIMESTAMP)))",
-                        to_sql_with_engine(&cast.expr, engine)?,
-                    )),
-                    Some(Engine::SQLite) => Ok(format!(
-                        "datetime({}, 'subsec', 'utc')",
-                        to_sql_with_engine(&cast.expr, engine)?,
-                    )),
-                    Some(Engine::Spark) => EngineNotSupportedForExpressionSnafu {
-                        engine: "Spark".to_string(),
-                        expr: format!("{expr}"),
-                    }
-                    .fail()?,
-                },
-                _ => Err(Error::UnsupportedFilterExpr {
-                    expr: format!("{expr}"),
-                }),
-            }
-        }
+        Expr::Cast(cast) => handle_cast(cast, engine, expr),
         Expr::Literal(value) => match value {
+            ScalarValue::Date32(Some(value)) => match engine {
+                Some(Engine::SQLite) => {
+                    Ok(format!("datetime({}, 'unixepoch')", value * SECONDS_IN_DAY))
+                }
+                _ => Ok(format!("TO_TIMESTAMP({})", value * SECONDS_IN_DAY)),
+            },
+            ScalarValue::Date64(Some(value)) => match engine {
+                Some(Engine::SQLite) => Ok(format!(
+                    "datetime({}, 'unixepoch')",
+                    value * i64::from(SECONDS_IN_DAY)
+                )),
+                _ => Ok(format!(
+                    "TO_TIMESTAMP({})",
+                    value * i64::from(SECONDS_IN_DAY)
+                )),
+            },
             ScalarValue::Null => Ok(value.to_string()),
             ScalarValue::Int16(Some(value)) => Ok(value.to_string()),
             ScalarValue::Int32(Some(value)) => Ok(value.to_string()),
@@ -112,6 +102,10 @@ pub fn to_sql_with_engine(expr: &Expr, engine: Option<Engine>) -> Result<String>
                 }
                 _ => Ok(format!("TO_TIMESTAMP({})", value / 1_000_000_000)),
             },
+            ScalarValue::TimestampMicrosecond(Some(value), None | Some(_)) => match engine {
+                Some(Engine::SQLite) => Ok(format!("datetime({}, 'unixepoch')", value / 1_000_000)),
+                _ => Ok(format!("TO_TIMESTAMP({})", value / 1_000_000)),
+            },
             ScalarValue::TimestampMillisecond(Some(value), None | Some(_)) => match engine {
                 Some(Engine::SQLite) => Ok(format!("datetime({}, 'unixepoch')", value / 1000)),
                 _ => Ok(format!("TO_TIMESTAMP({})", value / 1000)),
@@ -132,4 +126,36 @@ pub fn to_sql_with_engine(expr: &Expr, engine: Option<Engine>) -> Result<String>
 
 pub fn to_sql(expr: &Expr) -> Result<String> {
     to_sql_with_engine(expr, None)
+}
+
+fn handle_cast(cast: &Cast, engine: Option<Engine>, expr: &Expr) -> Result<String> {
+    match cast.data_type {
+        arrow::datatypes::DataType::Timestamp(_, Some(_) | None) => match engine {
+            None => Ok(format!(
+                "CAST({} AS TIMESTAMPTZ)",
+                to_sql_with_engine(&cast.expr, engine)?,
+            )),
+            Some(Engine::ODBC) => Ok(format!(
+                "CAST({} AS TIMESTAMP)",
+                to_sql_with_engine(&cast.expr, engine)?,
+            )),
+            // This needs to match the timestamp conversion below
+            Some(Engine::DuckDB) => Ok(format!(
+                "TO_TIMESTAMP(EPOCH(CAST({} AS TIMESTAMP)))",
+                to_sql_with_engine(&cast.expr, engine)?,
+            )),
+            Some(Engine::SQLite) => Ok(format!(
+                "datetime({}, 'subsec', 'utc')",
+                to_sql_with_engine(&cast.expr, engine)?,
+            )),
+            Some(Engine::Spark) => EngineNotSupportedForExpressionSnafu {
+                engine: "Spark".to_string(),
+                expr: format!("{expr}"),
+            }
+            .fail()?,
+        },
+        _ => Err(Error::UnsupportedFilterExpr {
+            expr: format!("{expr}"),
+        }),
+    }
 }
