dee0309738f21e331426ae9d4869500bad29316c
Phillip LeBlanc <phillip@leblanc.tech>
2024-05-17 00:01:23 +0900

    Add federation to FlightTable based DataConnectors (#1401)
    * Add a template for settings.json instructing how to configure IDE for working with non-default features
    
    * Add federation to FlightTable
    
    * Use fixed version of federation

---
+++ crates/data_components/src/flight/federation.rs
@@ -0,0 +1,84 @@
+use async_trait::async_trait;
+use datafusion_federation::{FederatedTableProviderAdaptor, FederatedTableSource};
+use datafusion_federation_sql::{SQLExecutor, SQLFederationProvider, SQLTableSource};
+use std::sync::Arc;
+
+use datafusion::{
+    arrow::datatypes::SchemaRef,
+    error::{DataFusionError, Result as DataFusionResult},
+    physical_plan::{stream::RecordBatchStreamAdapter, SendableRecordBatchStream},
+    sql::{
+        sqlparser::dialect::{Dialect, GenericDialect},
+        TableReference,
+    },
+};
+
+use super::{query_to_stream, FlightTable};
+
+impl FlightTable {
+    fn create_federated_table_source(
+        self: Arc<Self>,
+    ) -> DataFusionResult<Arc<dyn FederatedTableSource>> {
+        let table_name = self.table_reference.to_string();
+        tracing::trace!(
+            table_name,
+            %self.table_reference,
+            "create_federated_table_source"
+        );
+        let schema = Arc::clone(&self.schema);
+        let fed_provider = Arc::new(SQLFederationProvider::new(self));
+        Ok(Arc::new(SQLTableSource::new_with_schema(
+            fed_provider,
+            table_name,
+            schema,
+        )?))
+    }
+
+    pub fn create_federated_table_provider(
+        self: Arc<Self>,
+    ) -> DataFusionResult<FederatedTableProviderAdaptor> {
+        let table_source = Self::create_federated_table_source(Arc::clone(&self))?;
+        Ok(FederatedTableProviderAdaptor::new_with_provider(
+            table_source,
+            self,
+        ))
+    }
+}
+
+#[async_trait]
+impl SQLExecutor for FlightTable {
+    fn name(&self) -> &str {
+        self.name
+    }
+
+    fn compute_context(&self) -> Option<String> {
+        Some(self.join_push_down_context.clone())
+    }
+
+    fn dialect(&self) -> Arc<dyn Dialect> {
+        Arc::new(GenericDialect {})
+    }
+
+    fn execute(
+        &self,
+        query: &str,
+        schema: SchemaRef,
+    ) -> DataFusionResult<SendableRecordBatchStream> {
+        Ok(Box::pin(RecordBatchStreamAdapter::new(
+            schema,
+            query_to_stream(self.client.clone(), query),
+        )))
+    }
+
+    async fn table_names(&self) -> DataFusionResult<Vec<String>> {
+        Err(DataFusionError::NotImplemented(
+            "table inference not implemented".to_string(),
+        ))
+    }
+
+    async fn get_table_schema(&self, table_name: &str) -> DataFusionResult<SchemaRef> {
+        FlightTable::get_schema(self.client.clone(), &TableReference::bare(table_name))
+            .await
+            .map_err(|e| DataFusionError::Execution(e.to_string()))
+    }
+}
