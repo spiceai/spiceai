cc1e9c500f6ee32a633fd3fe09602645980f8e7a
yfu <fevin86@gmail.com>
2024-05-17 15:43:04 +1000

    support more common csv options

--- crates/runtime/src/dataconnector.rs
+++ crates/runtime/src/dataconnector.rs
@@ -340,20 +340,32 @@ pub trait ListingTableConnector: DataConnector {
         &self,
         params: &HashMap<String, String>,
     ) -> DataConnectorResult<Arc<CsvFormat>> {
-        let compression_type = params.get("compression_type").map_or("", |f| f);
         let has_header = params.get("has_header").map_or(true, |f| f == "true");
+        let quote = params
+            .get("quote")
+            .map_or(b'"', |f| *f.as_bytes().first().unwrap_or(&b'"'));
+        let escape = params
+            .get("escape")
+            .and_then(|f| f.as_bytes().first().copied());
+        let schema_infer_max_rec = params
+            .get("schema_infer_max_records")
+            .map_or_else(|| 1000, |f| usize::from_str(f).map_or(1000, |f| f));
         let delimiter = params
             .get("delimiter")
             .map_or(b',', |f| *f.as_bytes().first().unwrap_or(&b','));
+        let compression_type = params.get("compression_type").map_or("", |f| f);
 
         Ok(Arc::new(
             CsvFormat::default()
                 .with_has_header(has_header)
+                .with_quote(quote)
+                .with_escape(escape)
+                .with_schema_infer_max_rec(schema_infer_max_rec)
+                .with_delimiter(delimiter)
                 .with_file_compression_type(
                     FileCompressionType::from_str(compression_type)
                         .context(UnsupportedCsvCompressionTypeSnafu { compression_type })?,
-                )
-                .with_delimiter(delimiter),
+                ),
         ))
     }
 }
