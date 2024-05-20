4241ea2bc3c8c98b6cb5ae5e82963a69e07de303
Phillip LeBlanc <phillip@leblanc.tech>
2024-05-20 15:46:58 +0900

    wip

--- crates/runtime/tests/federation/mod.rs
+++ crates/runtime/tests/federation/mod.rs
@@ -1,3 +1,4 @@
+use app::App;
 use spicepod::component::dataset::Dataset;
 
 fn make_spiceai_dataset(path: &str, name: &str) -> Dataset {
@@ -8,4 +9,6 @@ fn make_spiceai_dataset(path: &str, name: &str) -> Dataset {
 fn basic_federation_push_down() -> Result<(), String> {
     let eth_blocks_ds = make_spiceai_dataset("eth.recent_blocks", "blocks");
     let eth_tx_ds = make_spiceai_dataset("eth.recent_transactions", "tx");
+
+    let app = App::new()
 }
