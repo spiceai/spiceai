a08c1232cfa2b8453170ddef50d8ad42ed11699f
Phillip LeBlanc <phillip@leblanc.tech>
2024-05-24 21:25:21 +0900

    Handle registered the schema for accelerated tables

--- crates/runtime/src/accelerated_table/refresh.rs
+++ crates/runtime/src/accelerated_table/refresh.rs
@@ -4,6 +4,7 @@ use std::time::{Duration, SystemTime, UNIX_EPOCH};
 use crate::component::dataset::acceleration::RefreshMode;
 use crate::component::dataset::TimeFormat;
 use crate::datafusion::filter_converter::TimestampFilterConvert;
+use crate::datafusion::schema;
 use crate::object_store_registry::default_runtime_env;
 use crate::{
     dataconnector::get_data,
@@ -457,6 +458,16 @@ impl Refresher {
             ),
             default_runtime_env(),
         );
+
+        let ctx_state = ctx.state();
+        let default_catalog = &ctx_state.config_options().catalog.default_catalog;
+        match schema::ensure_schema_exists(&ctx, default_catalog, &self.dataset_name) {
+            Ok(()) => (),
+            Err(_) => {
+                unreachable!("The default catalog should always exist");
+            }
+        };
+
         if let Err(e) = ctx.register_table(self.dataset_name.clone(), Arc::clone(&self.federated)) {
             tracing::error!("Unable to register federated table: {e}");
         }
