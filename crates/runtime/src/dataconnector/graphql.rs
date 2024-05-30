eec232ce5717cc1c281bf99057f6e7c48cc8cb93
Kirill Khramkov <hramkir@gmail.com>
2024-05-30 19:44:58 +0400

    Refactor

--- crates/runtime/src/dataconnector/graphql.rs
+++ crates/runtime/src/dataconnector/graphql.rs
@@ -15,15 +15,21 @@ limitations under the License.
 */
 
 use arrow::array::RecordBatch;
-use arrow::datatypes::Schema;
+use arrow::datatypes::{Schema, SchemaRef};
 use arrow_json::reader::infer_json_schema_from_iterator;
 use arrow_json::ReaderBuilder;
 use async_trait::async_trait;
-use datafusion::execution::context::SessionContext;
-use secrets::Secret;
+use datafusion::execution::context::SessionState;
+use datafusion::logical_expr::Expr;
+use datafusion::physical_plan::ExecutionPlan;
+use itertools::Itertools;
+use reqwest::header::{HeaderMap, HeaderValue, AUTHORIZATION};
+use reqwest::header::{CONTENT_TYPE, USER_AGENT};
+use secrets::{get_secret_or_param, Secret};
+use serde_json::Value;
 
 use crate::component::dataset::Dataset;
-use datafusion::datasource::{MemTable, TableProvider};
+use datafusion::datasource::{MemTable, TableProvider, TableType};
 use snafu::prelude::*;
 use std::any::Any;
 use std::io::Cursor;
@@ -55,42 +61,121 @@ impl DataConnectorFactory for GraphQL {
     }
 }
 
-impl GraphQL {
-    pub async fn connect(&self, dataset: &Dataset) -> (Arc<Schema>, Vec<Vec<RecordBatch>>) {
-        let host = dataset.path();
-        let client = reqwest::Client::new();
+struct GraphQLClient {
+    client: reqwest::Client,
+    endpoint: String,
+    query: String,
+    json_path: String,
+}
 
-        let response = client
-            .post(host)
-            .header("Content-Type", "application/json")
-            .header("User-Agent", "spice")
-            .bearer_auth("token")
-            .body(r#"{"query": "query {viewer {repositories(first: 100) {nodes {id, name}}}}"}"#)
+impl GraphQLClient {
+    async fn execute(&self) -> (Vec<Vec<RecordBatch>>, SchemaRef) {
+        let body = format!(r#"{{"query": "{}"}}"#, self.query.lines().join(" "));
+        let response = self
+            .client
+            .post(&self.endpoint)
+            .body(body)
             .send()
             .await
             .unwrap();
-        let response: serde_json::Value = response.json().await.unwrap();
-        let response = &response["data"]["viewer"]["repositories"]["nodes"];
-        let first = response[0].clone();
 
-        let schema =
-            Arc::new(infer_json_schema_from_iterator(vec![Ok(first.clone())].into_iter()).unwrap());
-
-        let mut res = vec![];
-        if let serde_json::Value::Array(value) = response {
-            for v in value {
-                let buf = v.to_string();
-                let batch = ReaderBuilder::new(schema.clone())
-                    .with_batch_size(1024)
-                    .build(Cursor::new(buf.as_bytes()))
-                    .unwrap()
-                    .collect::<Result<Vec<_>, _>>()
-                    .unwrap();
-                res.push(batch);
-            }
+        let mut response: serde_json::Value = response.json().await.unwrap();
+        for key in self.json_path.split('.') {
+            response = response[key].clone();
         }
 
-        (schema, res)
+        let unwraped = match response {
+            Value::Array(val) => val.clone(),
+            obj @ Value::Object(_) => vec![obj.clone()],
+            _ => unimplemented!(),
+        };
+
+        let schema = Arc::new(
+            infer_json_schema_from_iterator(unwraped.clone().into_iter().map(Result::Ok)).unwrap(),
+        );
+
+        let mut res = vec![];
+        for v in unwraped {
+            let buf = v.to_string();
+            let batch = ReaderBuilder::new(schema.clone())
+                .with_batch_size(1024)
+                .build(Cursor::new(buf.as_bytes()))
+                .unwrap()
+                .collect::<Result<Vec<_>, _>>()
+                .unwrap();
+            res.push(batch);
+        }
+
+        (res, schema)
+    }
+}
+
+impl GraphQL {
+    async fn get_client(&self, dataset: &Dataset) -> GraphQLClient {
+        let mut client_builder = reqwest::Client::builder();
+        let token = get_secret_or_param(&self.params, &self.secret, "auth_token_key", "auth_token")
+            .unwrap();
+        let query = self.params.get("query").unwrap().clone();
+        let endpoint = dataset.path().clone();
+        let json_path = self.params.get("json_path").unwrap().clone();
+
+        let mut headers = HeaderMap::new();
+        headers.append(USER_AGENT, HeaderValue::from_static("spice"));
+        headers.append(CONTENT_TYPE, HeaderValue::from_static("application/json"));
+        headers.append(
+            AUTHORIZATION,
+            HeaderValue::from_str(&format!("bearer {token}")).unwrap(),
+        );
+
+        client_builder = client_builder.default_headers(headers);
+
+        GraphQLClient {
+            client: client_builder.build().unwrap(),
+            query,
+            endpoint,
+            json_path,
+        }
+    }
+}
+
+struct GraphQLTableProvider {
+    client: GraphQLClient,
+    schema: SchemaRef,
+}
+
+impl GraphQLTableProvider {
+    pub async fn new(client: GraphQLClient) -> Result<Self> {
+        let (_, schema) = client.execute().await;
+
+        Ok(Self { client, schema })
+    }
+}
+
+#[async_trait]
+impl TableProvider for GraphQLTableProvider {
+    fn as_any(&self) -> &dyn Any {
+        self
+    }
+
+    fn schema(&self) -> Arc<Schema> {
+        self.schema.clone()
+    }
+
+    fn table_type(&self) -> TableType {
+        TableType::Base
+    }
+
+    async fn scan(
+        &self,
+        state: &SessionState,
+        projection: Option<&Vec<usize>>,
+        filters: &[Expr],
+        limit: Option<usize>,
+    ) -> datafusion::error::Result<Arc<dyn ExecutionPlan>> {
+        let (res, schema) = self.client.execute().await;
+        let table = MemTable::try_new(schema, res).unwrap();
+
+        table.scan(state, projection, filters, limit).await
     }
 }
 
@@ -104,12 +189,8 @@ impl DataConnector for GraphQL {
         &self,
         dataset: &Dataset,
     ) -> super::DataConnectorResult<Arc<dyn TableProvider>> {
-        let ctx = SessionContext::new();
-        let (schema, partitions) = self.connect(dataset).await;
-        let table = Arc::new(MemTable::try_new(schema, partitions).unwrap());
+        let client = self.get_client(&dataset).await;
 
-        let df = ctx.read_table(table).unwrap();
-
-        Ok(df.into_view())
+        Ok(Arc::new(GraphQLTableProvider::new(client).await.unwrap()))
     }
 }
