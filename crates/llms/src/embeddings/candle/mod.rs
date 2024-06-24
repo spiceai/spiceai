2417ac0fcaadbfe01a728ac993679a91317b6b94
jeadie <jack@spice.ai>
2024-06-25 07:23:00 +1000

    initial compile

--- crates/llms/src/embeddings/candle/mod.rs
+++ crates/llms/src/embeddings/candle/mod.rs
@@ -1,121 +1,158 @@
-use std::path::PathBuf;
-
 /*
 Copyright 2024 The Spice.ai OSS Authors
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
-     https://www.apache.org/licenses/LICENSE-2.0
+https://www.apache.org/licenses/LICENSE-2.0
 Unless required by applicable law or agreed to in writing, software
 distributed under the License is distributed on an "AS IS" BASIS,
 WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 */
-use super::{Embed, FailedToInstantiateEmbeddingModelSnafu, FailedToPrepareInputSnafu, Result};
+#![allow(clippy::module_name_repetitions)]
+#![allow(clippy::missing_errors_doc)]
+
+use std::{fs, path::{Path, PathBuf}, sync::{Arc, Mutex}};
+use super::{
+    Embed, FailedToCreateEmbeddingSnafu, FailedToInstantiateEmbeddingModelSnafu,
+    FailedToPrepareInputSnafu, Result,
+};
 
 use async_openai::types::EmbeddingInput;
-use snafu::ResultExt;
-use tei_core::{Backend, Batch, ModelType, Pool, tokenization::ValidEncoding};
-use tokenizers::Tokenizer;
-use tei_candle::CandleBackend;
+use async_trait::async_trait;
 use candle_core::DType;
+use hf_hub::api::sync::ApiBuilder;
+use hf_hub::{Repo, RepoType};
+use serde::Deserialize;
+use snafu::ResultExt;
+use tei_backend_core::{ModelType, Pool, Backend};
+use tei_candle::{batch, sort_embeddings, CandleBackend};
+use tokenizers::{Encoding, Tokenizer};
 
 pub struct CandleEmbedding {
-    backend: CandleBackend,
+    backend: Arc<Mutex<CandleBackend>>,
     tok: Tokenizer,
-    output_size: Option<i32>, // Lazily compute output_size based on [`embed`]
+    model_cfg: ModelConfig,
+}
+
+/// Important fields from a model's `config.json`
+#[derive(Debug, Deserialize)]
+pub struct ModelConfig {
+    pub hidden_size: i32,
 }
 
 impl CandleEmbedding {
-     pub fn try_new(model_root: PathBuf, dtype: DType) -> Result<Self> {
-          let str_dtype = if dtype == DType::F32 {
-               "float32"
-           } else if dtype == DType::F32 {
-               "float16"
-           } else {
-               dtype.as_str()
-           };
-
-          let b = CandleBackend::new(model_root.clone(), str_dtype.to_string(), ModelType::Embedding(Pool::Cls)).boxed().context(FailedToInstantiateEmbeddingModelSnafu)?;
-          let tok = Tokenizer::from_file(model_root.join("tokenizer.json")).context(FailedToInstantiateEmbeddingModelSnafu)?;
-          Ok(Self {
-               backend: b,
-               tok: tok,
-               output_size: None,
-          })
-     }
-
-     pub fn tokenise(&self, s: String, add_special_tokens: bool) -> Result<ValidEncoding> {
-        let encoding = self.tok
-            .encode::<String>(s, add_special_tokens).context(FailedToPrepareInputSnafu)?;
-
-        let seq_len = encoding.len();
-        Ok(ValidEncoding {
-            input_ids: encoding.get_ids().to_vec(),
-            token_type_ids: encoding.get_type_ids().to_vec(),
-            position_ids: (position_offset as u32..(seq_len + position_offset) as u32)
-                .collect::<Vec<_>>(),
+    pub fn try_new(model_root: &Path, dtype: DType) -> Result<Self> {
+        Ok(Self {
+            backend: Arc::new(Mutex::new(CandleBackend::new(
+                model_root.to_path_buf(),
+                Self::convert_dtype(dtype),
+                ModelType::Embedding(Pool::Cls),
+            )
+            .boxed()
+            .context(FailedToInstantiateEmbeddingModelSnafu)?)),
+            tok: Tokenizer::from_file(model_root.join("tokenizer.json"))
+                .context(FailedToInstantiateEmbeddingModelSnafu)?,
+            model_cfg: Self::model_config(model_root)?,
         })
-     }
-     
+    }
+
+    fn convert_dtype(dtype: DType) -> String {
+        let dtype_str = if dtype == DType::F32 {
+            "float32"
+        } else if dtype == DType::F16 {
+            "float16"
+        } else {
+            dtype.as_str()
+        };
+        dtype_str.to_string()
+    }
+
+    fn model_config(model_root: &Path) -> Result<ModelConfig> {
+        let config_path = model_root.join("config.json");
+        let config = fs::read_to_string(config_path)
+            .boxed()
+            .context(FailedToInstantiateEmbeddingModelSnafu)?;
+        let config: ModelConfig = serde_json::from_str(&config)
+            .boxed()
+            .context(FailedToInstantiateEmbeddingModelSnafu)?;
+        Ok(config)
+    }
 }
 
-pub fn e(b: Backend) -> Result<()> {
-    b.embed(Batch::)
-}
-
-/// For a given HuggingFace repo, download the needed files to create a `CandleEmbedding`.
-pub fn download_hf_artifacts(
-     model_id: &'static str,
-     revision: Option<&'static str>,
- ) -> Result<PathBuf> {
-     let builder = ApiBuilder::new().with_progress(false);
- 
-     let api = builder.build().unwrap();
-     let api_repo = if let Some(revision) = revision {
-         api.repo(Repo::with_revision(
-             model_id.to_string(),
-             RepoType::Model,
-             revision.to_string(),
-         ))
-     } else {
-         api.repo(Repo::new(model_id.to_string(), RepoType::Model))
-     };
- 
-     api_repo.get("config.json")?;
-     api_repo.get("tokenizer.json")?;
- 
-     let model_root = match api_repo.get("model.safetensors") {
-         Ok(p) => p,
-         Err(_) => {
-             let p = api_repo.get("pytorch_model.bin")?;
-             tracing::warn!("`model.safetensors` not found. Using `pytorch_model.bin` instead. Model loading will be significantly slower.");
-             p
-         }
-     }
-         .parent().unwrap()
-         .to_path_buf();
-     Ok(model_root)
- }
- 
-
+#[async_trait]
 impl Embed for CandleEmbedding {
-     async fn embed(&mut self, input: EmbeddingInput) -> Result<Vec<Vec<f32>>> {
-        e(self.backend)?;
-          
-     }
- 
-     /// Returns the size of the embedding vector returned by the model.
-     fn size(&self) -> i32 {
-          match self.output_size {
-               Some(size) => size,
-               None => {
-                    let output_size = ;
-                    self.output_size = Some(output_size);
-                    output_size
-               }
-          }
-     }
+    async fn embed(&mut self, input: EmbeddingInput) -> Result<Vec<Vec<f32>>> {
+        let add_special_tokens = true;
 
+        let encodings: Vec<Encoding> = match input {
+            EmbeddingInput::String(s) => vec![self
+                .tok
+                .encode::<String>(s, add_special_tokens)
+                .context(FailedToPrepareInputSnafu)?],
+            EmbeddingInput::StringArray(arr) => arr
+                .into_iter()
+                .map(|s| {
+                    self.tok
+                        .encode::<String>(s, add_special_tokens)
+                        .context(FailedToPrepareInputSnafu)
+                })
+                .collect::<Result<Vec<_>>>()?,
+            _ => {
+                return Err(super::Error::FailedToPrepareInput {
+                    source: "Unsupported input type".into(),
+                })
+            }
+        };
+
+        #[allow(clippy::cast_possible_truncation)]
+        let pooled_idx = (0..=encodings.len()).map(|i| i as u32).collect::<Vec<_>>();
+        let b = batch(encodings, pooled_idx, vec![]);
+
+        let (pooled_embeddings, _raw_embeddings) = match self.backend.lock() {
+            Ok(r) => sort_embeddings(r.embed(b).boxed().context(FailedToCreateEmbeddingSnafu)?),
+            Err(e) => {
+                tracing::error!("Failed to lock backend: {:?}", e);
+                return Err(super::Error::FailedToCreateEmbedding {
+                    source: "Failed to lock backend".into(),
+                });
+            }
+        };
+
+        Ok(pooled_embeddings)
+    }
+
+    fn size(&self) -> i32 {
+        self.model_cfg.hidden_size
+    }
+}
+
+/// For a given `HuggingFace` repo, download the needed files to create a `CandleEmbedding`.
+pub fn download_hf_artifacts(
+    model_id: &'static str,
+    revision: Option<&'static str>,
+) -> Result<PathBuf> {
+    let builder = ApiBuilder::new().with_progress(false);
+
+    let api = builder.build().boxed().context(FailedToInstantiateEmbeddingModelSnafu)?;
+    let api_repo = if let Some(revision) = revision {
+        api.repo(Repo::with_revision(
+            model_id.to_string(),
+            RepoType::Model,
+            revision.to_string(),
+        ))
+    } else {
+        api.repo(Repo::new(model_id.to_string(), RepoType::Model))
+    };
+
+    api_repo.get("config.json").boxed().context(FailedToInstantiateEmbeddingModelSnafu)?;
+    api_repo.get("tokenizer.json").boxed().context(FailedToInstantiateEmbeddingModelSnafu)?;
+
+    let model = if let Ok(p) = api_repo.get("model.safetensors") { p } else {
+        let p = api_repo.get("pytorch_model.bin").boxed().context(FailedToInstantiateEmbeddingModelSnafu)?;
+        tracing::warn!("`model.safetensors` not found. Using `pytorch_model.bin` instead. Model loading will be significantly slower.");
+        p
+    };
+    Ok(model.parent().ok_or("".into()).context(FailedToInstantiateEmbeddingModelSnafu)?.to_path_buf())
 }
