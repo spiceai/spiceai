2d05478e8e64799626a508f1a017b4e73c219804
Phillip LeBlanc <phillip@leblanc.tech>
2024-07-09 12:32:56 +0900

    Add back metadata to Spicepod spec

--- bin/spice/cmd/login.go
+++ bin/spice/cmd/login.go
@@ -111,6 +111,9 @@ spice login
 			var spicePod spec.SpicepodSpec
 			err = yaml.Unmarshal(spicepodBytes, &spicePod)
 			if err == nil {
+				if spicePod.Metadata != nil {
+					orgName = spicePod.Metadata["org"]
+				}
 				appName = spicePod.Name
 			}
 		}
