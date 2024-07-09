8e5fb31eb27da43b6d1b3d382edc0f098b226de3
Phillip LeBlanc <phillip@leblanc.tech>
2024-07-09 17:31:45 +0900

    Add catalog component to `Runtime` (#1906)
    * Add support for parsing `catalog` from Spicepod. Also remove unused `metadata` map
    
    * Add back metadata to Spicepod spec
    
    * Add initial runtime component Catalog struct
    
    * Use catalog_id instead of provider_path
    
    * Add comment
    
    * Add catalog to app
    
    * Use NewType pattern for log_failures: bool
    
    * small fixes
    
    * Add derive Clone,Copy to LogErrors
    
    * Fix merge conflict

--- crates/runtime/src/component/mod.rs
+++ crates/runtime/src/component/mod.rs
@@ -1,2 +1,3 @@
+pub mod catalog;
 pub mod dataset;
 pub mod view;
--- crates/runtime/src/lib.rs
+++ crates/runtime/src/lib.rs
@@ -232,6 +232,9 @@ pub type Result<T, E = Error> = std::result::Result<T, E>;
 
 pub type EmbeddingModelStore = HashMap<String, RwLock<Box<dyn Embed>>>;
 
+#[derive(Clone, Copy)]
+pub struct LogErrors(pub bool);
+
 #[derive(Clone)]
 pub struct Runtime {
     instance_name: String,
@@ -338,13 +341,13 @@ impl Runtime {
     }
 
     /// Returns a list of valid datasets from the given App, skipping any that fail to parse and logging an error for them.
-    fn get_valid_datasets(app: &App, log_failures: bool) -> Vec<Dataset> {
+    fn get_valid_datasets(app: &App, log_errors: LogErrors) -> Vec<Dataset> {
         Self::datasets_iter(app)
             .zip(&app.datasets)
             .filter_map(|(ds, spicepod_ds)| match ds {
                 Ok(ds) => Some(ds),
                 Err(e) => {
-                    if log_failures {
+                    if log_errors.0 {
                         status::update_dataset(
                             &TableReference::parse_str(&spicepod_ds.name),
                             status::ComponentStatus::Error,
@@ -359,8 +362,8 @@ impl Runtime {
     }
 
     /// Returns a list of valid views from the given App, skipping any that fail to parse and logging an error for them.
-    fn get_valid_views(app: &App, log_failures: bool) -> Vec<View> {
-        let datasets = Self::get_valid_datasets(app, log_failures)
+    fn get_valid_views(app: &App, log_errors: LogErrors) -> Vec<View> {
+        let datasets = Self::get_valid_datasets(app, log_errors)
             .iter()
             .map(|ds| ds.name.clone())
             .collect::<HashSet<_>>();
@@ -374,7 +377,7 @@ impl Runtime {
                 Ok(view) => {
                     // only load this view if the name isn't used by an existing dataset
                     if datasets.contains(&view.name) {
-                        if log_failures {
+                        if log_errors.0 {
                             metrics::counter!("views_load_error").increment(1);
                             tracing::error!(
                                 view = &spicepod_view.name,
@@ -387,7 +390,7 @@ impl Runtime {
                     }
                 }
                 Err(e) => {
-                    if log_failures {
+                    if log_errors.0 {
                         metrics::counter!("views_load_error").increment(1);
                         tracing::error!(view = &spicepod_view.name, "{e}");
                     }
@@ -403,7 +406,7 @@ impl Runtime {
             return;
         };
 
-        let valid_datasets = Self::get_valid_datasets(app, true);
+        let valid_datasets = Self::get_valid_datasets(app, LogErrors(true));
         let mut futures = vec![];
         for ds in &valid_datasets {
             status::update_dataset(&ds.name, status::ComponentStatus::Initializing);
@@ -425,7 +428,7 @@ impl Runtime {
     }
 
     fn load_views(&self, app: &App, valid_datasets: &[Dataset]) {
-        let views: Vec<View> = Self::get_valid_views(app, true);
+        let views: Vec<View> = Self::get_valid_views(app, LogErrors(true));
 
         for view in &views {
             if let Err(e) = self.load_view(view, valid_datasets) {
@@ -621,7 +624,7 @@ impl Runtime {
         }
     }
 
-    pub async fn remove_dataset(&self, ds: &Dataset) {
+    async fn remove_dataset(&self, ds: &Dataset) {
         if self.df.table_exists(ds.name.clone()) {
             if let Some(datasets_health_monitor) = &self.datasets_health_monitor {
                 datasets_health_monitor
@@ -649,7 +652,7 @@ impl Runtime {
         metrics::gauge!("datasets_count", "engine" => engine).decrement(1.0);
     }
 
-    pub async fn update_dataset(&self, ds: &Dataset) {
+    async fn update_dataset(&self, ds: &Dataset) {
         status::update_dataset(&ds.name, status::ComponentStatus::Refreshing);
         match self.load_dataset_connector(ds).await {
             Ok(connector) => {
@@ -1150,8 +1153,8 @@ impl Runtime {
                 tracing::debug!("Previous pods information: {:?}", current_app);
 
                 // check for new and updated datasets
-                let valid_datasets = Self::get_valid_datasets(&new_app, true);
-                let existing_datasets = Self::get_valid_datasets(current_app, false);
+                let valid_datasets = Self::get_valid_datasets(&new_app, LogErrors(true));
+                let existing_datasets = Self::get_valid_datasets(current_app, LogErrors(false));
 
                 for ds in &valid_datasets {
                     if let Some(current_ds) = existing_datasets.iter().find(|d| d.name == ds.name) {
