32f5837e67daa507ed12d5fda52fe1b334fbe8bb
sgrebnov <sergei.grebnov@gmail.com>
2024-07-23 14:17:42 -0700

    Add benchmark tests for s3 data connector

--- crates/runtime/benches/bench.rs
+++ crates/runtime/benches/bench.rs
@@ -1,3 +1,19 @@
+/*
+Copyright 2024 The Spice.ai OSS Authors
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+     https://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
 //! This is a benchmark test suite for the Spice runtime.
 //!
 //! It performs the following actions:
@@ -25,6 +41,7 @@ mod setup;
 
 #[cfg(feature = "spark")]
 mod bench_spark;
+mod bench_s3;
 mod bench_spicecloud;
 
 #[tokio::main]
@@ -39,6 +56,7 @@ async fn main() -> Result<(), String> {
         "spice.ai",
         #[cfg(feature = "spark")]
         "spark",
+        "s3",
     ];
 
     let mut display_records = vec![];
@@ -54,6 +72,9 @@ async fn main() -> Result<(), String> {
             #[cfg(feature = "spark")]
             "spark" => {
                 bench_spark::run(&mut rt, &mut benchmark_results).await?;
+            }
+            "s3" => {
+                bench_s3::run(&mut rt, &mut benchmark_results).await?;
             }
             _ => {}
         }
