88a980921036e38b8a0d1abbd19bc3e6d53611ee
jeadie <jack@spice.ai>
2024-08-15 21:34:07 +1000

    implement TableProvider and ExecutionPlan

--- crates/data_components/src/sharepoint/client.rs
+++ crates/data_components/src/sharepoint/client.rs
@@ -1,3 +1,4 @@
+use std::fmt;
 /*
 Copyright 2024 The Spice.ai OSS Authors
 
@@ -16,28 +17,72 @@ limitations under the License.
 #[allow(unused_variables, dead_code)]
 use std::{any::Any, sync::Arc};
 
-use arrow::datatypes::SchemaRef;
+use arrow::{
+    array::RecordBatch,
+    datatypes::{Schema, SchemaRef},
+};
+use async_stream::stream;
 use async_trait::async_trait;
 use datafusion::{
+    common::project_schema,
     datasource::{TableProvider, TableType},
     error::{DataFusionError, Result as DataFusionResult},
-    execution::context::SessionState,
+    execution::{context::SessionState, SendableRecordBatchStream, TaskContext},
     logical_expr::Expr,
-    physical_plan::ExecutionPlan,
+    physical_expr::EquivalenceProperties,
+    physical_plan::{
+        stream::RecordBatchStreamAdapter, DisplayAs, DisplayFormatType, ExecutionMode,
+        ExecutionPlan, Partitioning, PlanProperties,
+    },
 };
-use graph_rs_sdk::GraphClient;
+use futures::{Stream, StreamExt};
+
+use graph_rs_sdk::{
+    default_drive::DefaultDriveApiClient, drives::DrivesIdApiClient, GraphClient, GraphFailure,
+};
+use snafu::ResultExt;
+
+use crate::sharepoint::drive_items::drive_items_to_record_batch;
+
+use super::drive_items::{drive_item_table_schema, DriveItemResponse};
+
+/// Represents all the ways a Sharepoint [Drive](https://learn.microsoft.com/en-us/graph/api/resources/drive?view=graph-rest-1.0) can be identified.
+#[derive(Default, Debug, Clone, PartialEq)]
+pub enum DrivePtr {
+    DriveId(String),
+    UserId(String),
+    GroupId(String),
+
+    #[default]
+    Me,
+}
+
+enum DriveApi {
+    Id(DrivesIdApiClient),
+    Default(DefaultDriveApiClient),
+}
+
+/// Represents all the ways a Sharepoint [DriveItem](https://learn.microsoft.com/en-us/graph/api/resources/driveitem?view=graph-rest-1.0) can be identified.
+#[derive(Default, Debug, Clone, PartialEq)]
+pub enum DriveItemPtr {
+    ItemId(String),
+    ItemPath(String),
+
+    #[default]
+    Root,
+}
 
 pub struct SharepointClient {
-    _client: Arc<GraphClient>,
+    client: Arc<GraphClient>,
 }
 
 impl SharepointClient {
     pub fn new_from_client(client: Arc<GraphClient>) -> Self {
-        Self { _client: client }
+        Self { client: client }
     }
 
     pub fn table_schema() -> arrow::datatypes::Schema {
-        arrow::datatypes::Schema::empty()
+        drive_item_table_schema()
     }
 }
 
@@ -58,12 +103,173 @@ impl TableProvider for SharepointClient {
     async fn scan(
         &self,
         _state: &SessionState,
-        _projection: Option<&Vec<usize>>,
+        projection: Option<&Vec<usize>>,
         _filters: &[Expr],
         _limit: Option<usize>,
     ) -> DataFusionResult<Arc<dyn ExecutionPlan>> {
-        Err(DataFusionError::NotImplemented(
-            "scan not implemented".to_string(),
-        ))
+        Ok(Arc::new(SharepointListExec::new(
+            Arc::clone(&self.client),
+            &DrivePtr::Me,
+            &DriveItemPtr::Root,
+            projection,
+        )?))
+    }
+}
+
+struct SharepointListExec {
+    client: Arc<GraphClient>,
+    drive: DrivePtr,
+    drive_item: DriveItemPtr,
+
+    schema: SchemaRef,
+    properties: PlanProperties,
+}
+
+impl SharepointListExec {
+    pub fn new(
+        client: Arc<GraphClient>,
+        drive: &DrivePtr,
+        drive_item: &DriveItemPtr,
+        projections: Option<&Vec<usize>>,
+    ) -> DataFusionResult<Self> {
+        let schema = project_schema(&Arc::new(SharepointClient::table_schema()), projections)?;
+        let properties = PlanProperties::new(
+            EquivalenceProperties::new(Arc::clone(&schema)),
+            Partitioning::UnknownPartitioning(1),
+            ExecutionMode::Bounded,
+        );
+
+        Ok(Self {
+            client,
+            drive: drive.clone(),
+            drive_item: drive_item.clone(),
+            schema,
+            properties,
+        })
+    }
+
+    fn drive_client(graph: Arc<GraphClient>, drive: &DrivePtr) -> DriveApi {
+        match drive {
+            DrivePtr::DriveId(drive_id) => DriveApi::Id(graph.drive(drive_id)),
+            DrivePtr::UserId(user_id) => DriveApi::Default(graph.user(user_id).drive()),
+            DrivePtr::GroupId(_group_id) => unimplemented!("group id not supported"), // self.client.group(group_id).get_drive().url(),
+            DrivePtr::Me => DriveApi::Default(graph.me().drive()),
+        }
+    }
+
+    /// TODO: "You can use the $expand query string parameter to include the children of an item in the same call as retrieving the metadata of an item if the item has a children relationship."
+    /// `<https://learn.microsoft.com/en-us/graph/api/driveitem-get?view=graph-rest-1.0&tabs=http#optional-query-parameters>`
+    /// TODO: might need to explicitly
+    async fn list_from_path(
+        graph: Arc<GraphClient>,
+        drive: &DrivePtr,
+        drive_item: &DriveItemPtr,
+    ) -> Result<DriveItemResponse, GraphFailure> {
+        // `<https://learn.microsoft.com/en-us/graph/api/driveitem-get?view=graph-rest-1.0&tabs=http#http-request>`
+        let req = match Self::drive_client(graph, drive) {
+            DriveApi::Id(client) => match drive_item {
+                DriveItemPtr::ItemId(id) => client.item(id).list_children(),
+                DriveItemPtr::ItemPath(path) => client.item_by_path(path).list_children(),
+                DriveItemPtr::Root => client.item_by_path("").list_children(),
+            },
+            DriveApi::Default(client) => match drive_item {
+                DriveItemPtr::ItemId(id) => client.item(id).list_children(),
+                DriveItemPtr::ItemPath(path) => client.item_by_path(path).list_children(),
+                DriveItemPtr::Root => client.item_by_path("").list_children(),
+            },
+        };
+        req.send()
+            .await?
+            .json::<DriveItemResponse>()
+            .await
+            .map_err(|e| GraphFailure::ReqwestError(e))
+    }
+}
+
+impl ExecutionPlan for SharepointListExec {
+    fn name(&self) -> &'static str {
+        "SharepointListExec"
+    }
+
+    fn as_any(&self) -> &dyn Any {
+        self
+    }
+
+    fn schema(&self) -> SchemaRef {
+        Arc::clone(&self.schema)
+    }
+
+    fn properties(&self) -> &PlanProperties {
+        &self.properties
+    }
+
+    fn children(&self) -> Vec<&Arc<dyn ExecutionPlan>> {
+        vec![]
+    }
+
+    fn with_new_children(
+        self: Arc<Self>,
+        _children: Vec<Arc<dyn ExecutionPlan>>,
+    ) -> DataFusionResult<Arc<dyn ExecutionPlan>> {
+        Ok(self)
+    }
+
+    fn execute(
+        &self,
+        partition: usize,
+        context: Arc<TaskContext>,
+    ) -> DataFusionResult<SendableRecordBatchStream> {
+        let stream_adapter = RecordBatchStreamAdapter::new(
+            self.schema(),
+            process_list_drive_items(
+                self.client.clone(),
+                self.drive.clone(),
+                self.drive_item.clone(),
+            ),
+        );
+
+        Ok(Box::pin(stream_adapter))
+    }
+}
+
+impl std::fmt::Debug for SharepointListExec {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        write!(
+            f,
+            "SharepointListExec drive={:?} drive_item={:?}",
+            self.drive, self.drive_item
+        )
+    }
+}
+
+impl DisplayAs for SharepointListExec {
+    fn fmt_as(&self, _t: DisplayFormatType, f: &mut fmt::Formatter) -> std::fmt::Result {
+        write!(
+            f,
+            "SharepointListExec drive={:?} drive_item={:?}",
+            self.drive, self.drive_item
+        )
+    }
+}
+
+fn process_list_drive_items(
+    graph: Arc<GraphClient>,
+    drive: DrivePtr,
+    drive_item: DriveItemPtr,
+) -> impl Stream<Item = DataFusionResult<RecordBatch>> {
+    stream! {
+        match SharepointListExec::list_from_path(graph, &drive, &drive_item).await.boxed().map_err(|e| DataFusionError::External(e)) {
+            Ok(resp) => match drive_items_to_record_batch(resp.value) {
+                Ok(batch) => yield Ok(batch),
+                Err(e) => yield Err(DataFusionError::ArrowError(e, None)),
+            },
+            Err(e) => yield Err(e),
+        }
+
+        // TODO: handle pagination
+        // while let Some(next_link) = resp.next_link {
+
+        // }
+
     }
 }
---
+++ crates/data_components/src/sharepoint/drive_items.rs
@@ -0,0 +1,207 @@
+use chrono::{DateTime, Utc};
+/*
+Copyright 2024 The Spice.ai OSS Authors
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+     https://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+use arrow::array::{ArrayRef, Int64Array, StringArray, TimestampSecondArray, UInt32Array};
+use arrow::datatypes::{DataType, Field, Schema, TimeUnit};
+use arrow::error::Result as ArrowResult;
+use arrow::record_batch::RecordBatch;
+use std::sync::Arc;
+
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+struct User {
+    id: String,
+    display_name: String,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+struct CreatedBy {
+    user: User,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+struct LastModifiedBy {
+    user: User,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+struct Folder {
+    child_count: u32,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub(crate) struct DriveItemResponse {
+    pub value: Vec<DriveItem>,
+
+    // @odata.nextLink e.g. `"https://graph.microsoft.com/v1.0/users?$top=5&$skiptoken=RFNwdAIAAQAAAD8...AAAAAAAA"`
+    #[serde(rename = "@odata.nextLink")]
+    pub next_link: Option<String>,
+}
+
+/// Represents a Sharepoint DriveItem. JSON representation from:
+///  - get: `<https://learn.microsoft.com/en-us/graph/api/driveitem-get?view=graph-rest-1.0&tabs=http#response-1>`
+///  - list: `<https://learn.microsoft.com/en-us/graph/api/driveitem-list-children?view=graph-rest-1.0&tabs=http#response>`
+#[derive(Debug, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub(crate) struct DriveItem {
+    created_by: CreatedBy,
+    created_date_time: String,
+    c_tag: String,
+    e_tag: String,
+    folder: Folder,
+    id: String,
+    last_modified_by: LastModifiedBy,
+    last_modified_date_time: String,
+    name: String,
+    // root: Option<Root>, struct Root {}
+    size: u64,
+    web_url: String,
+}
+
+/// Flattened Arrow schema for [`DriveItem`].
+pub fn drive_item_table_schema() -> arrow::datatypes::Schema {
+    arrow::datatypes::Schema::new(vec![
+        arrow::datatypes::Field::new("created_by_id", arrow::datatypes::DataType::Utf8, false),
+        arrow::datatypes::Field::new("created_by_name", arrow::datatypes::DataType::Utf8, true),
+        // "2016-03-21T20:01:37Z",
+        arrow::datatypes::Field::new(
+            "created_date_time",
+            arrow::datatypes::DataType::Timestamp(arrow::datatypes::TimeUnit::Second, None),
+            false,
+        ),
+        arrow::datatypes::Field::new("c_tag", arrow::datatypes::DataType::Utf8, false),
+        arrow::datatypes::Field::new("e_tag", arrow::datatypes::DataType::Utf8, false),
+        arrow::datatypes::Field::new(
+            "folder_child_count",
+            arrow::datatypes::DataType::UInt32,
+            false,
+        ),
+        arrow::datatypes::Field::new("id", arrow::datatypes::DataType::Utf8, false),
+        arrow::datatypes::Field::new(
+            "last_modified_by_id",
+            arrow::datatypes::DataType::Utf8,
+            false,
+        ),
+        arrow::datatypes::Field::new(
+            "last_modified_by_name",
+            arrow::datatypes::DataType::Utf8,
+            true,
+        ),
+        arrow::datatypes::Field::new(
+            "last_modified_date_time",
+            arrow::datatypes::DataType::Timestamp(arrow::datatypes::TimeUnit::Second, None),
+            false,
+        ),
+        arrow::datatypes::Field::new("name", arrow::datatypes::DataType::Utf8, false),
+        arrow::datatypes::Field::new("size", arrow::datatypes::DataType::Int64, false),
+        arrow::datatypes::Field::new("web_url", arrow::datatypes::DataType::Utf8, false),
+    ])
+}
+
+pub(crate) fn drive_items_to_record_batch(drive_items: Vec<DriveItem>) -> ArrowResult<RecordBatch> {
+    let schema = Arc::new(drive_item_table_schema());
+
+    /// Microsoft graph returns timestamps in ISO 8601 format
+    fn parse_timestamp(ts: &str) -> i64 {
+        DateTime::parse_from_rfc3339(ts)
+            .unwrap()
+            .with_timezone(&Utc)
+            .timestamp()
+    }
+
+    // Aggregate column wise
+    let created_by_id: Vec<&str> = drive_items
+        .iter()
+        .map(|item| item.created_by.user.id.as_str())
+        .collect();
+    let created_by_name: Vec<Option<&str>> = drive_items
+        .iter()
+        .map(|item| Some(item.created_by.user.display_name.as_str()))
+        .collect();
+    let created_date_time: Vec<i64> = drive_items
+        .iter()
+        .map(|item| parse_timestamp(&item.created_date_time))
+        .collect();
+    let c_tag: Vec<&str> = drive_items.iter().map(|item| item.c_tag.as_str()).collect();
+    let e_tag: Vec<&str> = drive_items.iter().map(|item| item.e_tag.as_str()).collect();
+    let folder_child_count: Vec<u32> = drive_items
+        .iter()
+        .map(|item| item.folder.child_count)
+        .collect();
+    let id: Vec<&str> = drive_items.iter().map(|item| item.id.as_str()).collect();
+    let last_modified_by_id: Vec<&str> = drive_items
+        .iter()
+        .map(|item| item.last_modified_by.user.id.as_str())
+        .collect();
+    let last_modified_by_name: Vec<Option<&str>> = drive_items
+        .iter()
+        .map(|item| Some(item.last_modified_by.user.display_name.as_str()))
+        .collect();
+    let last_modified_date_time: Vec<i64> = drive_items
+        .iter()
+        .map(|item| parse_timestamp(&item.last_modified_date_time))
+        .collect();
+    let name: Vec<&str> = drive_items.iter().map(|item| item.name.as_str()).collect();
+    let size: Vec<i64> = drive_items.iter().map(|item| item.size as i64).collect();
+    let web_url: Vec<&str> = drive_items
+        .iter()
+        .map(|item| item.web_url.as_str())
+        .collect();
+
+    // Create the Arrow arrays
+    let created_by_id_array = Arc::new(StringArray::from(created_by_id)) as ArrayRef;
+    let created_by_name_array = Arc::new(StringArray::from(created_by_name)) as ArrayRef;
+    let created_date_time_array =
+        Arc::new(TimestampSecondArray::from(created_date_time)) as ArrayRef;
+    let c_tag_array = Arc::new(StringArray::from(c_tag)) as ArrayRef;
+    let e_tag_array = Arc::new(StringArray::from(e_tag)) as ArrayRef;
+    let folder_child_count_array = Arc::new(UInt32Array::from(folder_child_count)) as ArrayRef;
+    let id_array = Arc::new(StringArray::from(id)) as ArrayRef;
+    let last_modified_by_id_array = Arc::new(StringArray::from(last_modified_by_id)) as ArrayRef;
+    let last_modified_by_name_array =
+        Arc::new(StringArray::from(last_modified_by_name)) as ArrayRef;
+    let last_modified_date_time_array =
+        Arc::new(TimestampSecondArray::from(last_modified_date_time)) as ArrayRef;
+    let name_array = Arc::new(StringArray::from(name)) as ArrayRef;
+    let size_array = Arc::new(Int64Array::from(size)) as ArrayRef;
+    let web_url_array = Arc::new(StringArray::from(web_url)) as ArrayRef;
+
+    RecordBatch::try_new(
+        schema,
+        vec![
+            created_by_id_array,
+            created_by_name_array,
+            created_date_time_array,
+            c_tag_array,
+            e_tag_array,
+            folder_child_count_array,
+            id_array,
+            last_modified_by_id_array,
+            last_modified_by_name_array,
+            last_modified_date_time_array,
+            name_array,
+            size_array,
+            web_url_array,
+        ],
+    )
+}
