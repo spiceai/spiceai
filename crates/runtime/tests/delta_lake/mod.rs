df7de3049a0768fa3dda008c4fe9a7fdaba1f859
yfu <fevin86@gmail.com>
2024-08-23 17:24:33 +1000

    fix misaligned columns from delta lake (#2356)
    * re-calculate the projection to pass into parquet scan
    
    * fix misaligned projected column in delta lake connector
    
    * re-enable test
    
    * fix
    
    * fix test body

--- crates/runtime/tests/delta_lake/mod.rs
+++ crates/runtime/tests/delta_lake/mod.rs
@@ -23,8 +23,6 @@ impl Drop for FileCleanup {
     }
 }
 
-// Once https://github.com/spiceai/spiceai/issues/2355 is resolved, this test should be re-enabled
-#[ignore]
 #[tokio::test]
 async fn query_delta_lake_with_partitions() -> Result<(), String> {
     let tmp_dir = std::env::temp_dir();
@@ -77,35 +75,35 @@ async fn query_delta_lake_with_partitions() -> Result<(), String> {
         .map_err(|e| format!("query `{query}` to results: {e}"))?;
 
     let expected_results = [
-        "+-------------+----------------+--------------------------------------------------------------------------------------------------------------------+-------------+",
-        "| n_nationkey | n_name         | n_comment                                                                                                          | n_regionkey |",
-        "+-------------+----------------+--------------------------------------------------------------------------------------------------------------------+-------------+",
-        "| 0           | ALGERIA        | furiously regular requests. platelets affix furious                                                                | 0           |",
-        "| 1           | ARGENTINA      | instructions wake quickly. final deposits haggle. final, silent theodolites                                        | 1           |",
-        "| 2           | BRAZIL         | asymptotes use fluffily quickly bold instructions. slyly bold dependencies sleep carefully pending accounts        | 1           |",
-        "| 3           | CANADA         | ss deposits wake across the pending foxes. packages after the carefully bold requests integrate caref              | 1           |",
-        "| 4           | EGYPT          | usly ironic, pending foxes. even, special instructions nag. sly, final foxes detect slyly fluffily                 | 4           |",
-        "| 5           | ETHIOPIA       | regular requests sleep carefull                                                                                    | 0           |",
-        "| 6           | FRANCE         | oggedly. regular packages solve across                                                                             | 3           |",
-        "| 7           | GERMANY        | ong the regular requests: blithely silent pinto beans hagg                                                         | 3           |",
-        "| 8           | INDIA          | uriously unusual deposits about the slyly final pinto beans could                                                  | 2           |",
-        "| 9           | INDONESIA      | d deposits sleep quickly according to the dogged, regular dolphins. special excuses haggle furiously special reque | 2           |",
-        "| 10          | IRAN           | furiously idle platelets nag. express asymptotes s                                                                 | 4           |",
-        "| 11          | IRAQ           | pendencies; slyly express foxes integrate carefully across the reg                                                 | 4           |",
-        "| 12          | JAPAN          |  quickly final packages. furiously i                                                                               | 2           |",
-        "| 13          | JORDAN         | the slyly regular ideas. silent Tiresias affix slyly fu                                                            | 4           |",
-        "| 14          | KENYA          | lyly special foxes. slyly regular deposits sleep carefully. carefully permanent accounts slee                      | 0           |",
-        "| 15          | MOROCCO        | ct blithely: blithely express accounts nag carefully. silent packages haggle carefully abo                         | 0           |",
-        "| 16          | MOZAMBIQUE     |  beans after the carefully regular accounts r                                                                      | 0           |",
-        "| 17          | PERU           | ly final foxes. blithely ironic accounts haggle. regular foxes about the regular deposits are furiously ir         | 1           |",
-        "| 18          | CHINA          | ckly special packages cajole slyly. unusual, unusual theodolites mold furiously. slyly sile                        | 2           |",
-        "| 19          | ROMANIA        | sly blithe requests. thinly bold deposits above the blithely regular accounts nag special, final requests. care    | 3           |",
-        "| 20          | SAUDI ARABIA   | se slyly across the blithely regular deposits. deposits use carefully regular                                      | 4           |",
-        "| 21          | VIETNAM        | lly across the quickly even pinto beans. caref                                                                     | 2           |",
-        "| 22          | RUSSIA         | uctions. furiously unusual instructions sleep furiously ironic packages. slyly                                     | 3           |",
-        "| 23          | UNITED KINGDOM | carefully pending courts sleep above the ironic, regular theo                                                      | 3           |",
-        "| 24          | UNITED STATES  | ly ironic requests along the slyly bold ideas hang after the blithely special notornis; blithely even accounts     | 1           |",
-        "+-------------+----------------+--------------------------------------------------------------------------------------------------------------------+-------------+",
+    "+-------------+----------------+-------------+--------------------------------------------------------------------------------------------------------------------+",
+    "| n_nationkey | n_name         | n_regionkey | n_comment                                                                                                          |",
+    "+-------------+----------------+-------------+--------------------------------------------------------------------------------------------------------------------+",
+    "| 0           | ALGERIA        | 0           | furiously regular requests. platelets affix furious                                                                |",
+    "| 1           | ARGENTINA      | 1           | instructions wake quickly. final deposits haggle. final, silent theodolites                                        |",
+    "| 2           | BRAZIL         | 1           | asymptotes use fluffily quickly bold instructions. slyly bold dependencies sleep carefully pending accounts        |",
+    "| 3           | CANADA         | 1           | ss deposits wake across the pending foxes. packages after the carefully bold requests integrate caref              |",
+    "| 4           | EGYPT          | 4           | usly ironic, pending foxes. even, special instructions nag. sly, final foxes detect slyly fluffily                 |",
+    "| 5           | ETHIOPIA       | 0           | regular requests sleep carefull                                                                                    |",
+    "| 6           | FRANCE         | 3           | oggedly. regular packages solve across                                                                             |",
+    "| 7           | GERMANY        | 3           | ong the regular requests: blithely silent pinto beans hagg                                                         |",
+    "| 8           | INDIA          | 2           | uriously unusual deposits about the slyly final pinto beans could                                                  |",
+    "| 9           | INDONESIA      | 2           | d deposits sleep quickly according to the dogged, regular dolphins. special excuses haggle furiously special reque |",
+    "| 10          | IRAN           | 4           | furiously idle platelets nag. express asymptotes s                                                                 |",
+    "| 11          | IRAQ           | 4           | pendencies; slyly express foxes integrate carefully across the reg                                                 |",
+    "| 12          | JAPAN          | 2           |  quickly final packages. furiously i                                                                               |",
+    "| 13          | JORDAN         | 4           | the slyly regular ideas. silent Tiresias affix slyly fu                                                            |",
+    "| 14          | KENYA          | 0           | lyly special foxes. slyly regular deposits sleep carefully. carefully permanent accounts slee                      |",
+    "| 15          | MOROCCO        | 0           | ct blithely: blithely express accounts nag carefully. silent packages haggle carefully abo                         |",
+    "| 16          | MOZAMBIQUE     | 0           |  beans after the carefully regular accounts r                                                                      |",
+    "| 17          | PERU           | 1           | ly final foxes. blithely ironic accounts haggle. regular foxes about the regular deposits are furiously ir         |",
+    "| 18          | CHINA          | 2           | ckly special packages cajole slyly. unusual, unusual theodolites mold furiously. slyly sile                        |",
+    "| 19          | ROMANIA        | 3           | sly blithe requests. thinly bold deposits above the blithely regular accounts nag special, final requests. care    |",
+    "| 20          | SAUDI ARABIA   | 4           | se slyly across the blithely regular deposits. deposits use carefully regular                                      |",
+    "| 21          | VIETNAM        | 2           | lly across the quickly even pinto beans. caref                                                                     |",
+    "| 22          | RUSSIA         | 3           | uctions. furiously unusual instructions sleep furiously ironic packages. slyly                                     |",
+    "| 23          | UNITED KINGDOM | 3           | carefully pending courts sleep above the ironic, regular theo                                                      |",
+    "| 24          | UNITED STATES  | 1           | ly ironic requests along the slyly bold ideas hang after the blithely special notornis; blithely even accounts     |",
+    "+-------------+----------------+-------------+--------------------------------------------------------------------------------------------------------------------+",
     ];
     assert_batches_eq!(expected_results, &data);
 
