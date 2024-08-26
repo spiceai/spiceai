f936d6e06ef39050bb3b6252d38143b6ff197a1f
sgrebnov <sergei.grebnov@gmail.com>
2024-08-26 14:41:52 -0700

    Disable MySQL quick start and add trace message when runtime installation is required

--- bin/spice/pkg/runtime/runtime.go
+++ bin/spice/pkg/runtime/runtime.go
@@ -41,6 +41,7 @@ func EnsureInstalled(flavor string) (bool, error) {
 	shouldInstall := false
 	var upgradeVersion string
 	if installRequired := rtcontext.IsRuntimeInstallRequired(); installRequired {
+		fmt.Println("Runtime installation is required")
 		shouldInstall = true
 	} else {
 		upgradeVersion, err = rtcontext.IsRuntimeUpgradeAvailable()
