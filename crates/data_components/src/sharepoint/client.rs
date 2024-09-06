6d6b2fd85c4cc6ef03214bf08eecbc4541141130
Luke Kim <80174+lukekim@users.noreply.github.com>
2024-09-06 09:31:43 -0700

    Update crates/data_components/src/sharepoint/client.rs
    Co-authored-by: Phillip LeBlanc <phillip@leblanc.tech>

--- crates/data_components/src/sharepoint/client.rs
+++ crates/data_components/src/sharepoint/client.rs
@@ -14,7 +14,6 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 */
-#[allow(unused_variables, dead_code)]
 use std::{any::Any, sync::Arc};
 
 use arrow::{array::RecordBatch, datatypes::SchemaRef, error::ArrowError};
