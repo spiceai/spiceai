ef092e2dc4229e944aeaab57fe2b7af88ba44673
Phillip LeBlanc <phillip@leblanc.tech>
2024-09-12 21:32:39 +0900

    Add the metadata tests for duckdb and sqlite

--- crates/runtime/tests/acceleration/metadata_duckdb.rs
+++ crates/runtime/tests/acceleration/metadata_duckdb.rs
@@ -13,3 +13,84 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 */
+
+use app::AppBuilder;
+use arrow::array::RecordBatch;
+use datafusion_table_providers::sql::db_connection_pool::duckdbpool::DuckDbConnectionPool;
+use datafusion_table_providers::sql::db_connection_pool::DbConnectionPool;
+use duckdb::AccessMode;
+use futures::TryStreamExt;
+use runtime::{status, Runtime};
+use spicepod::component::dataset::acceleration::{Acceleration, Mode, RefreshMode};
+use std::sync::Arc;
+
+use crate::{get_test_datafusion, init_tracing, runtime_ready_check, s3::get_s3_dataset};
+
+#[tokio::test]
+async fn test_acceleration_duckdb_metadata() -> Result<(), anyhow::Error> {
+    let _tracing = init_tracing(Some("integration=debug,info"));
+
+    let status = status::RuntimeStatus::new();
+    let df = get_test_datafusion(Arc::clone(&status));
+
+    let mut dataset = get_s3_dataset();
+    dataset.acceleration = Some(Acceleration {
+        enabled: true,
+        engine: Some("duckdb".to_string()),
+        mode: Mode::File,
+        refresh_mode: Some(RefreshMode::Full),
+        refresh_sql: Some("SELECT * FROM taxi_trips LIMIT 10".to_string()),
+        ..Acceleration::default()
+    });
+
+    let app = AppBuilder::new("test_acceleration_duckdb_metadata")
+        .with_dataset(dataset)
+        .build();
+
+    let rt = Arc::new(
+        Runtime::builder()
+            .with_app(app)
+            .with_datafusion(df)
+            .with_runtime_status(status)
+            .build()
+            .await,
+    );
+
+    tokio::select! {
+        () = tokio::time::sleep(std::time::Duration::from_secs(10)) => {
+            return Err(anyhow::Error::msg("Timed out waiting for datasets to load"));
+        }
+        () = rt.load_components() => {}
+    }
+
+    runtime_ready_check(&rt).await;
+
+    drop(rt);
+
+    let conn = DuckDbConnectionPool::new_file(".spice/data/taxi_trips.db", &AccessMode::ReadWrite)
+        .expect("valid path");
+    let result: Vec<RecordBatch> = conn
+        .connect()
+        .await
+        .expect("valid connection")
+        .as_sync()
+        .expect("sync connection")
+        .query_arrow(
+            "SELECT dataset, key, metadata FROM spice_sys_metadata",
+            &[],
+            None,
+        )
+        .expect("query executes")
+        .try_collect::<Vec<RecordBatch>>()
+        .await
+        .expect("collects results");
+
+    let pretty = arrow::util::pretty::pretty_format_batches(&result)
+        .map_err(|e| anyhow::Error::msg(e.to_string()))?;
+    insta::assert_snapshot!(pretty);
+
+    // Remove the file
+    std::fs::remove_file(".spice/data/taxi_trips.db").expect("remove file");
+
+    Ok(())
+}
--- crates/runtime/tests/acceleration/metadata_sqlite.rs
+++ crates/runtime/tests/acceleration/metadata_sqlite.rs
@@ -13,3 +13,90 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 */
+
+use app::AppBuilder;
+use arrow::array::RecordBatch;
+use datafusion_table_providers::sql::db_connection_pool::sqlitepool::SqliteConnectionPool;
+use datafusion_table_providers::sql::db_connection_pool::DbConnectionPool;
+use datafusion_table_providers::sql::db_connection_pool::JoinPushDown;
+use futures::TryStreamExt;
+use runtime::{status, Runtime};
+use spicepod::component::dataset::acceleration::Mode;
+use spicepod::component::dataset::acceleration::{Acceleration, RefreshMode};
+use std::sync::Arc;
+
+use crate::{get_test_datafusion, init_tracing, runtime_ready_check, s3::get_s3_dataset};
+
+#[tokio::test]
+async fn test_acceleration_sqlite_metadata() -> Result<(), anyhow::Error> {
+    let _tracing = init_tracing(Some("integration=debug,info"));
+
+    let status = status::RuntimeStatus::new();
+    let df = get_test_datafusion(Arc::clone(&status));
+
+    let mut dataset = get_s3_dataset();
+    dataset.acceleration = Some(Acceleration {
+        enabled: true,
+        engine: Some("sqlite".to_string()),
+        mode: Mode::File,
+        refresh_mode: Some(RefreshMode::Full),
+        refresh_sql: Some("SELECT * FROM taxi_trips LIMIT 10".to_string()),
+        ..Acceleration::default()
+    });
+
+    let app = AppBuilder::new("test_acceleration_sqlite_metadata")
+        .with_dataset(dataset)
+        .build();
+
+    let rt = Arc::new(
+        Runtime::builder()
+            .with_app(app)
+            .with_datafusion(df)
+            .with_runtime_status(status)
+            .build()
+            .await,
+    );
+
+    tokio::select! {
+        () = tokio::time::sleep(std::time::Duration::from_secs(10)) => {
+            return Err(anyhow::Error::msg("Timed out waiting for datasets to load"));
+        }
+        () = rt.load_components() => {}
+    }
+
+    runtime_ready_check(&rt).await;
+
+    drop(rt);
+
+    let results = SqliteConnectionPool::new(
+        ".spice/data/taxi_trips_sqlite.db",
+        datafusion_table_providers::sql::db_connection_pool::Mode::File,
+        JoinPushDown::Disallow,
+        vec![],
+    )
+    .await
+    .expect("connection pool")
+    .connect()
+    .await
+    .expect("connection")
+    .as_async()
+    .expect("async connection")
+    .query_arrow(
+        "SELECT dataset, key, metadata FROM spice_sys_metadata",
+        &[],
+        None,
+    )
+    .await
+    .expect("query")
+    .try_collect::<Vec<RecordBatch>>()
+    .await
+    .expect("valid results");
+
+    let pretty = arrow::util::pretty::pretty_format_batches(&results).expect("pretty");
+    insta::assert_snapshot!(pretty);
+
+    // Remove the file
+    std::fs::remove_file(".spice/data/taxi_trips_sqlite.db").expect("remove file");
+
+    Ok(())
+}
