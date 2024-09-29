e36d104e3fc460c210696fc21d452f74e93abe3a
jeadie <jack@spice.ai>
2024-09-30 09:45:29 +1000

    lint

--- crates/runtime/src/embeddings/execution_plan.rs
+++ crates/runtime/src/embeddings/execution_plan.rs
@@ -215,11 +215,11 @@ fn construct_record_batch(
     RecordBatch::try_new(Arc::clone(projected_schema), cols)
 }
 
-/// Get the additional, embedding, columns to add to the [`RecordBatch`]. The columns are 
+/// Get the additional, embedding, columns to add to the [`RecordBatch`]. The columns are
 ///     1. Embedding vectors for each column in `embedded_columns`.
 ///     2. If a [`Chunker`] is provided for a given column, an additional column of offsets. For
 ///         each string, these offsets map the substrings used for each embeddding vector.
-/// 
+///
 /// The additional columns returned here should match those specified in [`super::table::EmbeddingTable::embedding_fields`]
 pub(crate) async fn get_embedding_columns(
     rb: &RecordBatch,
