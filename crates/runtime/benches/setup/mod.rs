478f294f82fb8eb6e4b7e7b9739e70fd24285371
Scott Lyons <scott@spice.ai>
2024-10-09 12:31:29 -0700

    Bump for build

--- crates/runtime/benches/setup/mod.rs
+++ crates/runtime/benches/setup/mod.rs
@@ -132,6 +132,7 @@ fn build_app(
 
     app_builder = match connector {
         "spice.ai" => crate::bench_spicecloud::build_app(app_builder),
+        // Run both S3, ABFS and any other object store benchmarks
         "s3" | "abfs" => crate::bench_object_store::build_app(connector, app_builder, bench_name),
         #[cfg(feature = "spark")]
         "spark" => crate::bench_spark::build_app(app_builder),
