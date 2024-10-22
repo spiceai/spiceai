153735bfee50c3f2d97b65ce98cae8bf1610331a
Scott Lyons <scott@spice.ai>
2024-10-22 10:22:21 -0700

    Changing to simple string splitting and a simple unit test

--- crates/runtime/src/user_agent_util.rs
+++ crates/runtime/src/user_agent_util.rs
@@ -1,40 +1,43 @@
-use regex::Regex;
-use std::sync::LazyLock;
+/*
+Copyright 2024 The Spice.ai OSS Authors
 
-static UA_REGEX: LazyLock<Result<Regex, regex::Error>> =
-    LazyLock::new(|| Regex::new(r"^(\S+) (\S+) \((.*?)\)$"));
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+     https://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
 
 pub fn extract_user_agent(ua_header: Option<&str>) -> (String, String, String) {
     let ua_header = ua_header.unwrap_or("");
-    if let Ok(regex) = &*UA_REGEX {
-        if let Some(ua_parts) = regex.captures(ua_header) {
-            let user_agent = ua_parts
-                .get(1)
-                .map_or_else(|| "unknown", |p| p.as_str())
-                .to_string();
-            let agent_version = ua_parts
-                .get(2)
-                .map_or_else(|| "unknown", |p| p.as_str())
-                .to_string();
-            let agent_os = ua_parts
-                .get(3)
-                .map_or_else(|| "unknown", |p| p.as_str())
-                .to_string();
-            (user_agent, agent_version, agent_os)
-        } else {
-            tracing::trace!("Could not parse user agent: {ua_header}");
-            (
-                "unknown".to_string(),
-                "unknown".to_string(),
-                "unknown".to_string(),
-            )
-        }
+    let parts:Vec<&str> = ua_header.splitn(3, ' ').collect();
+    if parts.len() == 3 {
+        tracing::trace!("Client user agent: {ua_header}");
+        (parts[0].to_string(), parts[1].to_string(), parts[2].to_string())
     } else {
-        tracing::error!("Could not compile UA regex");
-        (
-            "unknown".to_string(),
-            "unknown".to_string(),
-            "unknown".to_string(),
-        )
+        tracing::trace!("Could not parse user agent: {ua_header}");
+        ("unknown".to_string(), "unknown".to_string(), "unknown".to_string())
+    }
+}
+
+#[cfg(test)]
+mod test
+{
+    #[test]
+    fn test_ua_extraction() {
+        let (agent, version, os_str) = super::extract_user_agent(Some("gospice 0.1.0 (Pop!_OS 22.04 LTS)"));
+        assert_eq!(agent, "gospice");
+        assert_eq!(version, "0.1.0");
+        assert_eq!(os_str, "(Pop!_OS 22.04 LTS)");
+
+        let (agent, version, _os_str) = super::extract_user_agent(Some("gospice 0.1.0"));
+        assert_eq!(agent, "unknown");
+        assert_eq!(version, "unknown");
     }
 }
