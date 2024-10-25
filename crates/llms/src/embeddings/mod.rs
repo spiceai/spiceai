2bd9c88128f2383168e65a89aa4b57fb6b28102b
jeadie <jack@spice.ai>
2024-10-25 11:51:58 -0700

    missing_errors_doc

--- crates/llms/src/embeddings/mod.rs
+++ crates/llms/src/embeddings/mod.rs
@@ -10,6 +10,8 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 */
+#![allow(clippy::missing_errors_doc)]
+
 use async_openai::{
     error::{ApiError, OpenAIError},
     types::{
