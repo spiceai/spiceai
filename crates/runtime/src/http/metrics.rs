7ff65e426d4b2f4deb401adf62e31803b0aad366
sgrebnov <sergei.grebnov@gmail.com>
2024-10-27 13:26:29 -0700

    Update http_requests_duration_ms to be http_requests_duration

--- crates/runtime/src/http/metrics.rs
+++ crates/runtime/src/http/metrics.rs
@@ -28,7 +28,7 @@ pub(crate) static REQUESTS_TOTAL: LazyLock<Counter<u64>> =
 
 pub(crate) static REQUESTS_DURATION_MS: LazyLock<Histogram<f64>> = LazyLock::new(|| {
     METER
-        .f64_histogram("http_requests_duration_ms")
+        .f64_histogram("http_requests_duration")
         .with_unit("ms")
         .init()
 });
