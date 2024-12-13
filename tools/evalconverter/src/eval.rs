25d5e70bec756f6e2bd39d0f125e6b61fdef1209
jeadie <jack@spice.ai>
2024-12-13 11:59:03 +1000

    initial work on 'evalconverter'

---
+++ tools/evalconverter/src/eval.rs
@@ -0,0 +1,130 @@
+use anyhow::{anyhow, Context, Result};
+use path_clean::PathClean;
+use serde::{Deserialize, Serialize};
+use serde_yaml::Value;
+use std::collections::HashMap;
+use std::path::{Path, PathBuf};
+
+/// YAML representation of an eval specification file.
+#[derive(Debug, Serialize, Deserialize, Clone)]
+pub struct EvalSpecification {
+    #[serde(flatten)]
+    pub entries: HashMap<String, EvalEntry>,
+}
+
+/// Valid Eval from a specification YAML file.
+pub struct Eval {
+    pub id: String,
+    pub description: Option<String>,
+    pub metrics: Option<Vec<String>>,
+
+    pub class: String,
+    pub args: HashMap<String, Value>,
+}
+pub type Class = String;
+
+#[derive(Debug, Serialize, Deserialize, Clone)]
+#[serde(untagged)]
+pub enum EvalEntry {
+    Metadata(EvalMetadata),
+    Definition(EvalDefinition),
+}
+
+#[derive(Debug, Serialize, Deserialize, Clone)]
+pub struct EvalDefinition {
+    pub class: String,
+    pub args: HashMap<String, Value>,
+}
+
+#[derive(Debug, Serialize, Deserialize, Clone)]
+pub struct EvalMetadata {
+    id: String,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    description: Option<String>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    metrics: Option<Vec<String>>,
+}
+
+impl EvalSpecification {
+    pub fn validate_from_file(file: &Path, data_dir: &Path) -> Result<Eval> {
+        let file_contents = std::fs::read_to_string(file)
+            .context(anyhow!("Failed to read YAML file '{}'", file.display()))?;
+
+        let spec: EvalSpecification = serde_yaml::from_str(&file_contents)
+            .context(anyhow!("Failed to parse YAML file '{}'", file.display()))?;
+
+        spec.validate(data_dir)
+    }
+
+    /// Validate the specification from a YAML file, and converts it into an internal representation.
+    pub fn validate(&self, data_dir: &Path) -> Result<Eval> {
+        self.resolve_file_paths(data_dir)?;
+
+        let Some((
+            name,
+            EvalMetadata {
+                id,
+                description,
+                metrics,
+            },
+        )) = self.get_metadata()
+        else {
+            return Err(anyhow!("Metadata entry not found"));
+        };
+
+        let defs = self.get_definition_entries();
+        let Some((_, def)) = defs.iter().find(|(n, _)| *n == id.as_str()) else {
+            return Err(anyhow!("For {name}, expected '{id}' entry"));
+        };
+
+        Ok(Eval {
+            id: id.clone(),
+            description: description.clone(),
+            metrics: metrics.clone(),
+            class: def.class.clone(),
+            args: def.args.clone(),
+        })
+    }
+
+    pub fn resolve_file_paths(&self, reference_dir: &Path) -> Result<()> {
+        for (_, entry) in self.get_definition_entries() {
+            for (key, value) in &entry.args {
+                let Value::String(s) = value else {
+                    continue;
+                };
+                if is_potential_file_key(key.as_str()) && !reference_dir.join(s).exists() {
+                    return Err(anyhow!(
+                        "Value in `{key}: {}` should be a file, but does not exist.",
+                        reference_dir.join(s).display()
+                    ));
+                }
+            }
+        }
+        Ok(())
+    }
+
+    pub fn get_metadata(&self) -> Option<(&str, &EvalMetadata)> {
+        self.entries.iter().find_map(|(name, entry)| match entry {
+            EvalEntry::Metadata(metadata) => Some((name.as_str(), metadata)),
+            _ => None,
+        })
+    }
+
+    /// Get all definition entries
+    pub fn get_definition_entries(&self) -> Vec<(&str, &EvalDefinition)> {
+        self.entries
+            .iter()
+            .filter_map(|(name, entry)| match entry {
+                EvalEntry::Definition(definition) => Some((name.as_str(), definition)),
+                _ => None,
+            })
+            .collect()
+    }
+}
+
+fn is_potential_file_key(value: &str) -> bool {
+    match value {
+        "samples_jsonl" => true,
+        _ => false,
+    }
+}
---
+++ tools/evalconverter/src/main.rs
@@ -0,0 +1,80 @@
+mod eval;
+
+use anyhow::{Context, Result};
+use clap::Parser;
+use std::path::{Path, PathBuf};
+
+use eval::EvalSpecification;
+
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Cli {
+    /// Path to YAML file or directory containing YAML files
+    #[arg(short, long)]
+    input: PathBuf,
+
+    /// Override base path for resolving relative paths
+    #[arg(short, long)]
+    base_path: Option<PathBuf>,
+
+    /// Enable verbose logging
+    #[arg(short, long, action)]
+    verbose: bool,
+}
+
+fn main() -> Result<()> {
+    let cli = Cli::parse();
+
+    env_logger::init_from_env(
+        env_logger::Env::default()
+            .filter_or("LOG_LEVEL", if cli.verbose { "debug" } else { "info" }),
+    );
+
+    // Order of precendence for determining where we expect data (referenced in input YAMLs) to exist.
+    // 1. Explicitly provided base path in --base-path
+    // 2. Directory of input YAML files (if input is directory)
+    // 3. Parent directory of input YAML file (if input is file)
+    let data_dir = cli.base_path.unwrap_or(if cli.input.is_dir() {
+        cli.input.clone()
+    } else {
+        cli.input.parent().unwrap().to_path_buf()
+    });
+
+    // Determine input handling strategy
+    let files = if cli.input.is_dir() {
+        yaml_files_from(&cli.input)?
+    } else if cli.input.is_file() {
+        vec![cli.input]
+    } else {
+        anyhow::bail!("Input path must be a directory or a YAML file");
+    };
+
+    let output: Vec<_> = files
+        .iter()
+        .map(|f| EvalSpecification::validate_from_file(f, data_dir.as_path()))
+        .collect::<anyhow::Result<Vec<_>>>()?;
+
+    for o in output {
+        println!("Eval {:?} is valid.", o.id);
+    }
+
+    Ok(())
+}
+
+fn yaml_files_from(dir: &Path) -> Result<Vec<PathBuf>> {
+    Ok(std::fs::read_dir(dir)?
+        .filter_map(|entry| {
+            let entry = entry.ok()?;
+            let path = entry.path();
+
+            if path
+                .extension()
+                .map_or(false, |ext| ext == "yaml" || ext == "yml")
+            {
+                Some(path)
+            } else {
+                None
+            }
+        })
+        .collect::<Vec<PathBuf>>())
+}
