a47dd8dfe049ca5bbfc5ad7e8d81f9ea0fc9a7fe
peasee <98815791+peasee@users.noreply.github.com>
2025-01-14 12:18:08 +1000

    fix: Make progress bar count longer for load test

--- crates/test-framework/src/spicetest/mod.rs
+++ crates/test-framework/src/spicetest/mod.rs
@@ -123,7 +123,8 @@ impl SpiceTest<NotStarted> {
             EndCondition::Duration(duration) => {
                 // refresh the progress bar every 10 seconds, or every 1/1000th of the duration
                 // for an 8 hour test, this would be every ~28 seconds
-                let pb = ProgressBar::new(duration.as_secs());
+                let pb =
+                    ProgressBar::new(duration.as_secs() / self.state.query_set.len() as u64 * 2); // this isn't 100% representative of the progress bar count, but it's close enough (500ms per query)
                 pb.enable_steady_tick(Duration::from_secs((duration.as_secs() / 1000).max(10)));
 
                 pb
