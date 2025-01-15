9a31e7987113914126b2a1665d24e56ce96398e1
Phillip LeBlanc <phillip@leblanc.tech>
2025-01-15 16:20:20 +0900

    Add Iceberg SF1 TPCH queries

---
+++ crates/test-framework/src/queries/tpch/iceberg_sf1/q2.sql
@@ -0,0 +1,43 @@
+select
+    s_acctbal,
+    s_name,
+    n_name,
+    p_partkey,
+    p_mfgr,
+    s_address,
+    s_phone,
+    s_comment
+from
+    ice.tpch_sf1.part,
+    ice.tpch_sf1.supplier,
+    ice.tpch_sf1.partsupp,
+    ice.tpch_sf1.nation,
+    ice.tpch_sf1.region
+where
+        p_partkey = ps_partkey
+  and s_suppkey = ps_suppkey
+  and p_size = 15
+  and p_type like '%BRASS'
+  and s_nationkey = n_nationkey
+  and n_regionkey = r_regionkey
+  and r_name = 'EUROPE'
+  and ps_supplycost = (
+    select
+        min(ps_supplycost)
+    from
+        ice.tpch_sf1.partsupp,
+        ice.tpch_sf1.supplier,
+        ice.tpch_sf1.nation,
+        ice.tpch_sf1.region
+    where
+            p_partkey = ps_partkey
+      and s_suppkey = ps_suppkey
+      and s_nationkey = n_nationkey
+      and n_regionkey = r_regionkey
+      and r_name = 'EUROPE'
+)
+order by
+    s_acctbal desc,
+    n_name,
+    s_name,
+    p_partkey;
