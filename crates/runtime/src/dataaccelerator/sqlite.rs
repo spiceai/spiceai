037a498c038b29f3a685e434ce0a9689ecc75c45
peasee <98815791+peasee@users.noreply.github.com>
2025-01-15 17:05:26 +1000

    test: Fix SQLite test from update

--- crates/runtime/src/dataaccelerator/sqlite.rs
+++ crates/runtime/src/dataaccelerator/sqlite.rs
@@ -420,7 +420,7 @@ mod tests {
             .as_any()
             .downcast_ref::<UInt64Array>()
             .expect("result should be UInt64Array");
-        let expected = UInt64Array::from(vec![2]);
+        let expected = UInt64Array::from(vec![1]);
         assert_eq!(actual, &expected);
 
         let filter = col("time_int").lt(lit(1354360273));
