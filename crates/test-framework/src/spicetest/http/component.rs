62f6d937979eba184bf44a5d048ae0b655ec737d
jeadie <jack@spice.ai>
2025-01-17 10:14:05 +1000

    clppy

--- crates/test-framework/src/spicetest/http/component.rs
+++ crates/test-framework/src/spicetest/http/component.rs
@@ -58,7 +58,8 @@ impl HttpComponent {
         }
     }
 
-    #[must_use] pub fn component_name(&self) -> String {
+    #[must_use]
+    pub fn component_name(&self) -> String {
         match self {
             HttpComponent::Generic {
                 component_name: model,
@@ -69,7 +70,8 @@ impl HttpComponent {
         }
     }
 
-    #[must_use] pub fn with_api_base(self, api_base: String) -> Self {
+    #[must_use]
+    pub fn with_api_base(self, api_base: String) -> Self {
         match self {
             HttpComponent::Model { model, .. } => HttpComponent::Model { model, api_base },
             HttpComponent::Embedding { embedding, .. } => HttpComponent::Embedding {
@@ -83,7 +85,8 @@ impl HttpComponent {
         }
     }
 
-    #[must_use] pub fn with_component_name(self, component_name: String) -> Self {
+    #[must_use]
+    pub fn with_component_name(self, component_name: String) -> Self {
         match self {
             HttpComponent::Model { api_base, .. } => HttpComponent::Model {
                 model: component_name,
