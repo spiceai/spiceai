6cc4eb08f6e6da754f0caef7984fd3d6e022a198
jeadie <jack@spice.ai>
2025-01-20 16:25:47 +1000

    clppy

--- crates/test-framework/src/spicetest/http/consistency.rs
+++ crates/test-framework/src/spicetest/http/consistency.rs
@@ -57,14 +57,7 @@ impl ConsistencyConfig {
         disable_progress_bars: bool,
     ) -> Self {
         Self {
-            http: HttpConfig {
-                duration,
-                concurrency,
-                payloads,
-                warmup,
-                component,
-                disable_progress_bars,
-            },
+            http: HttpConfig { duration, concurrency, payloads, component, warmup, disable_progress_bars },
             buckets,
         }
     }
