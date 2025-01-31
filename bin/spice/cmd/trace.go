df42330e5991b613c105945c95eccd0fe18b885b
jeadie <jack@spice.ai>
2025-01-31 11:49:16 +1000

    better errors

--- bin/spice/cmd/trace.go
+++ bin/spice/cmd/trace.go
@@ -18,6 +18,7 @@ package cmd
 
 import (
 	"fmt"
+	"log/slog"
 
 	"github.com/spf13/cobra"
 	"github.com/spiceai/spiceai/bin/spice/pkg/context"
@@ -66,7 +67,12 @@ $ spice trace chat --last
 
 		traces, err := taskhistory.SqlRequestToTraces(rtcontext, fmt.Sprintf("SELECT * FROM runtime.task_history WHERE %s ORDER BY start_time asc", filter))
 		if err != nil {
-			cmd.Println(err)
+			slog.Error("SQL query to 'task_history' failed", "error", err)
+			cmd.PrintErrln("Error: failed to retrieve events from runtime.")
+			return
+		}
+		if len(traces) == 0 {
+			cmd.PrintErrln("Error: No events found")
 			return
 		}
 		taskhistory.PrintTreeFromTraces(cmd.OutOrStdout(), traces, Display)
