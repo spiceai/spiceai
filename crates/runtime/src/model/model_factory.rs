59593ee8391793a111203527cf7968968ac26cd8
Advay Patil <advaypatil27@gmail.com>
2025-03-27 15:10:25 -0700

    Attempt to fix build issues

--- crates/runtime/src/model/model_factory.rs
+++ crates/runtime/src/model/model_factory.rs
@@ -1,13 +1,15 @@
+use std::sync::Arc;
 use std::{collections::HashMap, sync::RwLock};
 
 use async_openai::types::Embedding;
-use llms::{chat::Chat, embeddings::Embed};
-use model_components::modelsource::ModelSource;
-use secrecy::{SecretBox, SecretString};
-use spicepod::component::embeddings::EmbeddingPrefix;
-
 use llms::chat::{Chat, Error as LlmError};
 use llms::embeddings::{Embed, Error as EmbedError};
+use secrecy::{SecretBox, SecretString};
+use spicepod::component::embeddings::EmbeddingPrefix;
+use spicepod::component::model::ModelSource;
+
+use crate::secrets::Secrets;
+
 trait ChatModel {
     fn create_chat(
         &self,
@@ -28,8 +30,8 @@ trait EmbedModel {
 }
 
 pub struct ModelFactory {
-    chat_models: HashMap<ModelSource, Box<dyn ChatModel>>,
-    embed_models: HashMap<EmbeddingPrefix, Box<dyn EmbedModel>>,
+    chat_models: HashMap<String, Box<dyn ChatModel>>,
+    embed_models: HashMap<String, Box<dyn EmbedModel>>,
 }
 
 impl ModelFactory {
@@ -41,19 +43,19 @@ impl ModelFactory {
     }
 
     pub fn register_chat_model(&mut self, source: ModelSource, model: Box<dyn ChatModel>) {
-        self.chat_models.insert(source, model);
+        self.chat_models.insert(source.to_string(), model);
     }
 
     pub fn register_embed_model(&mut self, source: EmbeddingPrefix, model: Box<dyn EmbedModel>) {
-        self.embed_models.insert(source, model);
+        self.embed_models.insert(source.to_string(), model);
     }
 
     pub fn get_chat_model(&self, source: &ModelSource) -> Result<&Box<dyn ChatModel>, LlmError> {
-        let model = self.chat_models.get(source);
+        let model = self.chat_models.get(&source.to_string());
         match model {
             Some(m) => Ok(m),
             None => Err(LlmError::UnknownModelSource {
-                from: source.clone(),
+                from: source.to_string(),
             }),
         }
     }
@@ -62,11 +64,11 @@ impl ModelFactory {
         &self,
         source: &EmbeddingPrefix,
     ) -> Result<&Box<dyn EmbedModel>, EmbedError> {
-        let model = self.embed_models.get(source);
+        let model = self.embed_models.get(&source.to_string());
         match model {
             Some(m) => Ok(m),
             None => Err(EmbedError::UnknownModelSource {
-                from: source.clone(),
+                from: source.to_string(),
             }),
         }
     }
