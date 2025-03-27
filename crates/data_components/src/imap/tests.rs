151c2e9600eb0516d6003245893578faa3a92bce
Ben Russell <br@x-plugins.com>
2025-03-28 01:41:41 +1100

    prov/imap: tests.rs: better debug msg if eml_utf8 attachments records fail to parse.
    Co-authored-by: Phillip LeBlanc <phillip@leblanc.tech>

--- crates/data_components/src/imap/tests.rs
+++ crates/data_components/src/imap/tests.rs
@@ -24,7 +24,7 @@ fn eml_utf8() {
         let _content_records = ContentSectionRecords::try_from( &message ).expect("Failed to parse message");
 
 
-        let attachment_records = AttachmentRecords::try_from( &message ).unwrap();
+        let attachment_records = AttachmentRecords::try_from( &message ).expect("Failed to parse attachments");
 
         let extracted_set = attachment_records.attachments;
 
