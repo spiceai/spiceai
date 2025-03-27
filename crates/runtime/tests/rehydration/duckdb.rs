a6c86d8c423082ca4111819d1c8f08b7bbc242de
Sevenannn <qianqliu@uw.edu>
2025-03-27 22:21:15 +0800

    fix

--- crates/runtime/tests/rehydration/duckdb.rs
+++ crates/runtime/tests/rehydration/duckdb.rs
@@ -25,7 +25,7 @@ pub(crate) async fn query_local_db(
     db_file_path: &str,
     query: &str,
 ) -> Result<SendableRecordBatchStream> {
-    let pool = DuckDbConnectionPool::new_file(db_file_path, &AccessMode::ReadOnly, None)
+    let pool = DuckDbConnectionPool::new_file(db_file_path, &AccessMode::ReadOnly)
         .map_err(|e| Error::msg(format!("Failed to create DuckDB connection pool: {e}")))?;
 
     let conn_dyn = pool
