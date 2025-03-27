38d993ef5ddcd4926e9cb53852cbf19d24552014
Ben Russell <br@x-plugins.com>
2025-03-28 01:43:11 +1100

    prov/imap: tests.rs: better debug msg if eml_three_attachments attachments records fail to parse.
    Co-authored-by: Phillip LeBlanc <phillip@leblanc.tech>

--- crates/data_components/src/imap/tests.rs
+++ crates/data_components/src/imap/tests.rs
@@ -112,7 +112,7 @@ fn eml_three_attachments(){
         let _content_records = ContentSectionRecords::try_from( &message ).expect("Failed to parse message");
 
 
-        let attachment_records = AttachmentRecords::try_from(&message).unwrap();
+        let attachment_records = AttachmentRecords::try_from(&message).expect("Failed to parse attachments");
 
         let extracted_set = attachment_records.attachments;
         for rec in &extracted_set {
