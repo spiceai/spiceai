75a427e653a4b8c53239550556624f4f838c413d
Phillip LeBlanc <phillip@spice.ai>
2025-04-07 09:44:47 +0900

    Apply suggestions from code review
    Co-authored-by: peasee <98815791+peasee@users.noreply.github.com>

--- crates/runtime/src/metrics/components/mod.rs
+++ crates/runtime/src/metrics/components/mod.rs
@@ -24,10 +24,10 @@ use super::{global, LazyLock, Meter};
 
 #[derive(Debug, Snafu)]
 pub enum Error {
-    #[snafu(display("Internal error. Please report an issue on https://github.com/spiceai/spiceai.\nMetric callback not implemented for metric {} with type {:?}", metric.name, metric.metric_type))]
+    #[snafu(display("Internal error. Report an issue at https://github.com/spiceai/spiceai/issues.\nMetric callback not implemented for metric {} with type {:?}", metric.name, metric.metric_type))]
     MetricCallbackNotImplemented { metric: MetricSpec },
 
-    #[snafu(display("Internal error. Please report an issue on https://github.com/spiceai/spiceai.\nMetric {} callback has wrong type. Expected {}, got {}", metric.name, expected_type, actual_type))]
+    #[snafu(display("Internal error. Report an issue at https://github.com/spiceai/spiceai/issues.\nMetric {} callback has wrong type. Expected {}, got {}", metric.name, expected_type, actual_type))]
     MetricCallbackWrongType {
         metric: MetricSpec,
         expected_type: &'static str,
