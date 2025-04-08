565cb188ac2737ab5b37492cde70360a7ecf6cc6
sgrebnov <sergei.grebnov@gmail.com>
2025-04-08 11:27:42 -0700

    Update s3_pdfs test

--- crates/runtime/tests/s3/mod.rs
+++ crates/runtime/tests/s3/mod.rs
@@ -153,13 +153,14 @@ async fn s3_pdfs() -> Result<(), anyhow::Error> {
                 .with_app(app)
                 .build()
                 .await;
+            let cloned_rt = Arc::new(rt.clone());
 
             // Set a timeout for the test
             tokio::select! {
                 () = tokio::time::sleep(std::time::Duration::from_secs(60)) => {
                     return Err(anyhow::anyhow!("Timed out waiting for datasets to load"));
                 }
-                () = rt.load_components() => {}
+                () = cloned_rt.load_components() => {}
             }
 
             let mut query_result = rt
