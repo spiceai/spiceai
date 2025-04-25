13a0386eba6a1d81f4f2c85e68ceacbc45552def
Kevin <4733573+kczimm@users.noreply.github.com.>
2025-04-25 12:23:38 -0500

    add basic test

--- crates/runtime/tests/flight/prepared_statements.rs
+++ crates/runtime/tests/flight/prepared_statements.rs
@@ -1,8 +1,8 @@
-use crate::{
-    flight::{create_flight_client, start_spice_test_app},
-    init_tracing,
-    utils::test_request_context,
-};
+use arrow::array::{Int64Array, RecordBatch};
+use datafusion::scalar::ScalarValue;
+use futures::TryStreamExt as _;
+
+use crate::{flight::start_spice_test_app, init_tracing, utils::test_request_context};
 
 #[tokio::test]
 async fn test_basic_binding() -> Result<(), anyhow::Error> {
@@ -10,9 +10,34 @@ async fn test_basic_binding() -> Result<(), anyhow::Error> {
 
     test_request_context()
         .scope(async {
-            let (channel, df) = start_spice_test_app(None, None).await?;
+            let (_, df) = start_spice_test_app(None, None).await?;
 
-            let mut client = create_flight_client(channel, None)?;
+            let result = df
+                .query_builder("SELECT $1 + 1 AS the_answer")
+                .parameters(vec![ScalarValue::Int64(Some(41))].into())
+                .build()
+                .run()
+                .await?;
+
+            let mut results: Vec<RecordBatch> =
+                result.data.try_collect::<Vec<RecordBatch>>().await?;
+
+            assert_eq!(results.len(), 1);
+
+            let record = results.pop().expect("1 record batch only");
+            let (i, _) = record
+                .schema()
+                .column_with_name("the_answer")
+                .expect("the_answer column");
+
+            let column = record
+                .column(i)
+                .as_any()
+                .downcast_ref::<Int64Array>()
+                .expect("the_answer is Int64Array");
+
+            assert_eq!(column.len(), 1);
+            assert_eq!(column.value(0), 42);
 
             Ok(())
         })
