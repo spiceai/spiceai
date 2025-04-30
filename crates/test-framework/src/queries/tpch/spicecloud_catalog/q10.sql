2d48a96c2fe28616b1873bbcd37ccae66d943a1d
Phillip LeBlanc <phillip@leblanc.tech>
2025-04-30 16:16:33 +0900

    Fix override queries

--- crates/test-framework/src/queries/tpch/spicecloud_catalog/q10.sql
+++ crates/test-framework/src/queries/tpch/spicecloud_catalog/q10.sql
@@ -13,12 +13,12 @@ from
     tpch.tpch.lineitem,
     tpch.tpch.nation
 where
-        c_custkey = o_custkey
-  and l_orderkey = o_orderkey
-  and o_orderdate >= date '1993-10-01'
-  and o_orderdate < date '1994-01-01'
-  and l_returnflag = 'R'
-  and c_nationkey = n_nationkey
+    c_custkey = o_custkey
+    and l_orderkey = o_orderkey
+    and o_orderdate >= date '1993-10-01'
+    and o_orderdate < date '1994-01-01'
+    and l_returnflag = 'R'
+    and c_nationkey = n_nationkey
 group by
     c_custkey,
     c_name,
@@ -28,4 +28,6 @@ group by
     c_address,
     c_comment
 order by
-    revenue desc;
+    revenue desc
+limit
+    20;
--- crates/test-framework/src/queries/tpch/spicecloud_catalog/q2.sql
+++ crates/test-framework/src/queries/tpch/spicecloud_catalog/q2.sql
@@ -14,30 +14,32 @@ from
     tpch.tpch.nation,
     tpch.tpch.region
 where
-        p_partkey = ps_partkey
-  and s_suppkey = ps_suppkey
-  and p_size = 15
-  and p_type like '%BRASS'
-  and s_nationkey = n_nationkey
-  and n_regionkey = r_regionkey
-  and r_name = 'EUROPE'
-  and ps_supplycost = (
-    select
-        min(ps_supplycost)
-    from
-        tpch.tpch.partsupp,
-        tpch.tpch.supplier,
-        tpch.tpch.nation,
-        tpch.tpch.region
-    where
+    p_partkey = ps_partkey
+    and s_suppkey = ps_suppkey
+    and p_size = 15
+    and p_type like '%BRASS'
+    and s_nationkey = n_nationkey
+    and n_regionkey = r_regionkey
+    and r_name = 'EUROPE'
+    and ps_supplycost = (
+        select
+            min(ps_supplycost)
+        from
+            tpch.tpch.partsupp,
+            tpch.tpch.supplier,
+            tpch.tpch.nation,
+            tpch.tpch.region
+        where
             p_partkey = ps_partkey
-      and s_suppkey = ps_suppkey
-      and s_nationkey = n_nationkey
-      and n_regionkey = r_regionkey
-      and r_name = 'EUROPE'
-)
+            and s_suppkey = ps_suppkey
+            and s_nationkey = n_nationkey
+            and n_regionkey = r_regionkey
+            and r_name = 'EUROPE'
+    )
 order by
     s_acctbal desc,
     n_name,
     s_name,
-    p_partkey;
+    p_partkey
+limit
+    100;
