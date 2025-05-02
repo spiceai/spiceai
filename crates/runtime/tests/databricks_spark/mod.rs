badc2c52deb1e0baec4b4fafd13b249c578b51bf
Evgenii Khramkov <evgenii@spice.ai>
2025-05-02 09:12:50 +0900

    fix env var names

--- crates/runtime/tests/databricks_spark/mod.rs
+++ crates/runtime/tests/databricks_spark/mod.rs
@@ -35,24 +35,24 @@ fn make_catalog(path: &str, name: &str) -> Catalog {
 #[allow(clippy::expect_used)]
 fn get_params() -> Params {
     // Verify that the environment variables are set
-    let _ = std::env::var("TEST_DATABRICKSCKS_HOST").expect("TEST_DATABRICKSCKS_HOST is not set");
-    let _ = std::env::var("TEST_DATABRICKSCKS_TOKEN").expect("TEST_DATABRICKSCKS_TOKEN is not set");
-    let _ = std::env::var("TEST_DATABRICKSCKS_CLUSTER_ID")
-        .expect("TEST_DATABRICKSCKS_CLUSTER_ID is not set");
+    let _ = std::env::var("TEST_DATABRICKS_HOST").expect("TEST_DATABRICKS_HOST is not set");
+    let _ = std::env::var("TEST_DATABRICKS_TOKEN").expect("TEST_DATABRICKS_TOKEN is not set");
+    let _ =
+        std::env::var("TEST_DATABRICKS_CLUSTER_ID").expect("TEST_DATABRICKS_CLUSTER_ID is not set");
 
     Params::from_string_map(
         vec![
             (
                 "databricks_endpoint".to_string(),
-                "${ env:TEST_DATABRICKSCKS_HOST }".to_string(),
+                "${ env:TEST_DATABRICKS_HOST }".to_string(),
             ),
             (
                 "databricks_token".to_string(),
-                "${ env:TEST_DATABRICKSCKS_TOKEN }".to_string(),
+                "${ env:TEST_DATABRICKS_TOKEN }".to_string(),
             ),
             (
                 "databricks_cluster_id".to_string(),
-                "${ env:TEST_DATABRICKSCKS_CLUSTER_ID }".to_string(),
+                "${ env:TEST_DATABRICKS_CLUSTER_ID }".to_string(),
             ),
             ("mode".to_string(), "spark_connect".to_string()),
         ]
