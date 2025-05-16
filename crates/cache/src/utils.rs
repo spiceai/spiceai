5cc40e97522bfd445002e0bda727b8873405f1a7
peasee <98815791+peasee@users.noreply.github.com>
2025-05-16 16:40:00 +0900

    chore: Clippy

--- crates/cache/src/utils.rs
+++ crates/cache/src/utils.rs
@@ -14,10 +14,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-use std::{
-    collections::{HashSet, VecDeque},
-    sync::Arc,
-};
+use std::{collections::HashSet, sync::Arc};
 
 use arrow::array::RecordBatch;
 use datafusion::{
