3bc425e770159f86bf18cc6d8d0ddb74893b5b44
jeadie <jack@spice.ai>
2025-06-06 22:16:02 +1000

    default field

--- crates/search/src/generation/text_search.rs
+++ crates/search/src/generation/text_search.rs
@@ -146,7 +146,13 @@ impl FullTextSearch {
 
     fn search_query_literal(&self, literal: &str, limit: usize) -> Result<Vec<Value>> {
         // Explicitly create AST to avoid user queries being considered a query language (e.g. `"title:sea^20 body:whale^70"`).
-        let q = QueryParser::for_index(&self.idx, vec![])
+        let default_field = self
+            .idx
+            .schema()
+            .find_field(self.field.as_str())
+            .map(|(f, _)| vec![f])
+            .unwrap_or_default();
+        let q = QueryParser::for_index(&self.idx, default_field)
             .parse_query(literal)
             // let q = QueryParser::for_index(&self.idx, vec![])
             //     .build_query_from_user_input_ast(UserInputAst::Leaf(Box::new(UserInputLeaf::Literal(
