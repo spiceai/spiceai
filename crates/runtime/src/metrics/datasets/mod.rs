004c4faa1cf34331c8098758ded34f93d8231a82
Phillip LeBlanc <phillip@leblanc.tech>
2025-06-12 08:29:00 -0700

    Fix load status metric description

--- crates/runtime/src/metrics/datasets/mod.rs
+++ crates/runtime/src/metrics/datasets/mod.rs
@@ -44,7 +44,7 @@ pub(crate) static STATUS: LazyLock<Gauge<u64>> = LazyLock::new(|| {
     DATASETS_METER
         .u64_gauge("dataset_load_state")
         .with_description(
-            "Status of the dataset. 1=Initializing, 2=Ready, 3=Disabled, 4=Error, 5=Refreshing.",
+            "Status of the dataset. 0=Initializing, 1=Ready, 2=Disabled, 3=Error, 4=Refreshing, 5=ShuttingDown.",
         )
         .build()
 });
