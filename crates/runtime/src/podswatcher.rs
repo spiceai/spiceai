c9a08d19ebe9eee39f09e13b99352cb026f9f7f9
Phillip LeBlanc <phillip@leblanc.tech>
2025-06-14 12:12:50 -0700

    fix

--- crates/runtime/src/podswatcher.rs
+++ crates/runtime/src/podswatcher.rs
@@ -65,7 +65,7 @@ impl PodsWatcher {
                             }
                         }
 
-                        tx.blocking_send(root_path.clone());
+                        let _ = tx.blocking_send(root_path.clone());
                     }
                     Err(e) => tracing::error!("Pods content watcher error: {e}"),
                 }
