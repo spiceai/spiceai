4f0e8c6a5b4b3bad19ba103c4f87136e2b5b93bb
Jack Eadie <jack@spice.ai>
2025-06-17 16:13:35 +1000

    Use dev HTTP endpoint for spice.ai catalog provider

--- crates/runtime/src/catalogconnector/spice_cloud.rs
+++ crates/runtime/src/catalogconnector/spice_cloud.rs
@@ -117,7 +117,13 @@ impl SpiceCloudPlatformCatalog {
             .params
             .get("http_endpoint")
             .expose()
-            .unwrap_or_else(|_| "https://data.spiceai.io");
+            .unwrap_or_else(|_| {
+                if cfg!(feature = "dev") {
+                    "https://dev.spiceai.io".into()
+                } else {
+                    "https://spiceai.io".into()
+                }
+            });
 
         let mut props = HashMap::new();
         if let ExposedParamLookup::Present(api_key) = self.params.get("api_key").expose() {
