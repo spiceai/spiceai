4954d763917f79fd811e075cbd413b820090aab7
Phillip LeBlanc <phillip@leblanc.tech>
2025-06-29 18:26:19 +0900

    remove unused

--- crates/runtime/src/search/full_text/index.rs
+++ crates/runtime/src/search/full_text/index.rs
@@ -117,11 +117,6 @@ impl FullTextDatabaseIndex {
     }
 
     #[must_use]
-    pub fn as_arc_any(self: Arc<Self>) -> Arc<dyn Any + Send + Sync> {
-        self
-    }
-
-    #[must_use]
     pub fn underlying_table(&self) -> Arc<dyn TableProvider> {
         Arc::clone(&self.base_table)
     }
