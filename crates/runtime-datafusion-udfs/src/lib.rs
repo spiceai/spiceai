f30f1634c5dded0c9c8ca98b6e212eec743a692f
Kevin <4733573+kczimm@users.noreply.github.com.>
2025-07-09 20:41:55 -0500

    remove register_udfs function

--- crates/runtime-datafusion-udfs/src/lib.rs
+++ crates/runtime-datafusion-udfs/src/lib.rs
@@ -14,19 +14,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-use std::sync::Arc;
-
-use datafusion::{functions::math::random::RandomFunc, prelude::SessionContext};
-
 pub mod alias;
 pub mod bucket;
 pub mod cosine_distance;
 pub mod truncate;
-
-// UDFs that need a reference to [`crate::datafusion::DataFusion`] must be defined in [`crate::builder::RuntimeBuilder::build`].
-pub fn register_udfs(ctx: &SessionContext) {
-    ctx.register_udf(cosine_distance::CosineDistance::new().into());
-    ctx.register_udf(alias::ScalarUDFAlias::new(Arc::new(RandomFunc::default()), "rand").into());
-    ctx.register_udf(bucket::Bucket::new().into());
-    ctx.register_udf(truncate::Truncate::new().into());
-}
