17e7ddb65248dc4826fabe9d6f7f925a6863575e
jeadie <jack@spice.ai>
2025-07-11 13:55:47 +1000

    fix Instant import

--- crates/runtime/src/datafusion/query/builder.rs
+++ crates/runtime/src/datafusion/query/builder.rs
@@ -14,9 +14,10 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-use std::{collections::HashSet, sync::Arc, time::Instant};
+use std::{collections::HashSet, sync::Arc};
 
 use datafusion::common::ParamValues;
+use tokio::time::Instant;
 use uuid::Uuid;
 
 use crate::datafusion::{DataFusion, query::QueryMethod};
