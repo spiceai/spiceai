8718cc8d2a7a9337486efc78b1fd94c987fd67c1
jeadie <jack@spice.ai>
2025-07-29 16:15:25 +1000

    remove chat

--- crates/llms/src/bedrock/chat/mod.rs
+++
@@ -1,8 +0,0 @@
-
-#[async_trait]
-pub trait BedrockChatFormats {}
-
-
-// let mut input_builder = ConverseInputBuilder::default();
-// input_builder = input_builder.additional_model_request_fields(input);
-pub fn
