4459a6e10a3c9614bb7e94a1d2277348515775d1
peasee <98815791+peasee@users.noreply.github.com>
2025-08-14 12:10:04 +1000

    feat: Add initial rate limiter

---
+++ crates/runtime-rate-limiter/src/lib.rs
@@ -0,0 +1,355 @@
+/*
+Copyright 2024-2025 The Spice.ai OSS Authors
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+     https://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+use std::{sync::Arc, time::Duration};
+
+use governor::{
+    Jitter, Quota, RateLimiter as Governor,
+    clock::DefaultClock,
+    middleware::NoOpMiddleware,
+    state::{InMemoryState, NotKeyed},
+};
+use rand::prelude::*;
+use snafu::ResultExt;
+use snafu::prelude::*;
+use tokio::sync::{Semaphore, SemaphorePermit};
+
+#[derive(Debug, Snafu)]
+pub enum Error {
+    #[snafu(display("Failed to acquire semaphore permit.\n{source}"))]
+    SemaphoreAcquireError { source: tokio::sync::AcquireError },
+}
+
+pub type Result<T, E = Error> = std::result::Result<T, E>;
+
+#[derive(Debug, Default)]
+pub struct JitterConfig {
+    min: Duration,
+    max: Duration,
+}
+
+#[derive(Debug, Default)]
+pub struct RateLimiterBuilder {
+    jitter: Option<JitterConfig>,
+    max_concurrent_requests: Option<usize>,
+    quotas: Vec<Quota>,
+}
+
+impl RateLimiterBuilder {
+    #[must_use]
+    pub fn new() -> Self {
+        Self::default()
+    }
+
+    #[must_use]
+    pub fn with_jitter(mut self, jitter: JitterConfig) -> Self {
+        self.jitter = Some(jitter);
+        self
+    }
+
+    #[must_use]
+    pub fn with_max_concurrent_requests(mut self, max_concurrent_requests: usize) -> Self {
+        self.max_concurrent_requests = Some(max_concurrent_requests);
+        self
+    }
+
+    #[must_use]
+    pub fn with_quota(mut self, quota: Quota) -> Self {
+        self.quotas.push(quota);
+        self
+    }
+
+    #[must_use]
+    pub fn build(self) -> RateLimiter {
+        let jitter = self.jitter;
+        let governors = self
+            .quotas
+            .into_iter()
+            .map(|quota| Arc::new(Governor::direct(quota)))
+            .collect::<Vec<_>>();
+
+        let semaphore = self
+            .max_concurrent_requests
+            .map(|max_concurrent_requests| Arc::new(Semaphore::new(max_concurrent_requests)));
+
+        RateLimiter::new(jitter, governors, semaphore)
+    }
+}
+
+pub struct RateLimiter {
+    jitter_config: JitterConfig,
+    jitter: Jitter,
+    governors: Vec<Arc<Governor<NotKeyed, InMemoryState, DefaultClock, NoOpMiddleware>>>,
+    semaphore: Option<Arc<Semaphore>>,
+}
+
+#[allow(dead_code)] // will be used once the rate limiter is integrated
+pub struct Permit<'a>(Option<SemaphorePermit<'a>>);
+
+impl RateLimiter {
+    fn new(
+        jitter: Option<JitterConfig>,
+        governors: Vec<Arc<Governor<NotKeyed, InMemoryState, DefaultClock, NoOpMiddleware>>>,
+        semaphore: Option<Arc<Semaphore>>,
+    ) -> Self {
+        let jitter_config = jitter.unwrap_or(JitterConfig {
+            min: Duration::ZERO,
+            max: Duration::ZERO,
+        });
+
+        let jitter = Jitter::new(jitter_config.min, jitter_config.max);
+
+        Self {
+            jitter_config,
+            jitter,
+            governors,
+            semaphore,
+        }
+    }
+
+    /// Acquires a permit from the rate limiter.
+    /// Asynchronously waits for the rate limiters to be ready and optionally acquires a semaphore permit for maximum concurrency if configured.
+    ///
+    /// # Errors
+    ///
+    /// If the semaphore has been closed, this will return an error.
+    pub async fn acquire(&self) -> Result<Permit> {
+        // check for concurrency first - we may end up waiting for a concurrent request long enough that the rate limits clear
+        let semaphore = if let Some(semaphore) = &self.semaphore {
+            Some(semaphore.acquire().await.context(SemaphoreAcquireSnafu)?)
+        } else {
+            None
+        };
+
+        // check all of the rate limiters async and wait to re-check
+        futures::future::join_all(
+            self.governors
+                .iter()
+                .map(|governor| governor.until_ready_with_jitter(self.jitter)),
+        )
+        .await;
+
+        if self.governors.is_empty() {
+            // manually add some random jitter if no rate limiters are configured
+            let mut rng = rand::rng();
+            let jitter_wait = rng.random_range(self.jitter_config.min..=self.jitter_config.max);
+            tokio::time::sleep(jitter_wait).await;
+        }
+
+        Ok(Permit(semaphore))
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use std::num::NonZeroU32;
+
+    use super::*;
+
+    #[tokio::test]
+    async fn test_rate_limiter_acquire() {
+        let rate_limiter = RateLimiterBuilder::new()
+            .with_jitter(JitterConfig {
+                min: Duration::from_millis(100),
+                max: Duration::from_millis(200),
+            })
+            .with_max_concurrent_requests(5)
+            .with_quota(Quota::per_second(
+                NonZeroU32::new(10).expect("NonZeroU32 should be non-zero"),
+            ))
+            .build();
+
+        let permit = rate_limiter.acquire().await;
+        assert!(
+            permit.is_ok(),
+            "Failed to acquire permit: {:?}",
+            permit.err()
+        );
+        let permit = permit.expect("should be Ok");
+        assert!(
+            permit.0.is_some(),
+            "Semaphore permit should be Some if semaphore is configured"
+        );
+
+        // Test that semaphore restricts concurrency
+        drop(permit);
+        let permits = (0..5).map(|_| rate_limiter.acquire()).collect::<Vec<_>>();
+        let mut results = futures::future::try_join_all(permits)
+            .await
+            .expect("Should acquire all permits");
+
+        // the next request should block until one of the permits is dropped
+        tokio::select! {
+            _ = rate_limiter.acquire() => {
+                panic!("Expected semaphore to block, but it did not.");
+            },
+            () = tokio::time::sleep(Duration::from_secs(1)) => {}
+        };
+
+        // dropping one permit should allow the next request to immediately acquire a permit
+        drop(
+            results
+                .pop()
+                .expect("Should have at least one permit to drop"),
+        );
+
+        tokio::select! {
+            permit = rate_limiter.acquire() => {
+                assert!(permit.is_ok(), "Failed to acquire permit after dropping one: {:?}", permit.err());
+                let permit = permit.expect("should be Ok");
+                assert!(permit.0.is_some(), "Semaphore permit should be Some if semaphore is configured");
+            },
+            () = tokio::time::sleep(Duration::from_secs(1)) => {
+                panic!("Expected to acquire a permit after dropping one, but timed out.");
+            }
+        }
+    }
+
+    #[tokio::test]
+    async fn test_rate_limiter_per_second() {
+        let rate_limiter = RateLimiterBuilder::new()
+            .with_quota(Quota::per_second(
+                NonZeroU32::new(2).expect("NonZeroU32 should be non-zero"),
+            ))
+            .build();
+
+        // acquire all 10 permits at once, which should exhaust the rate limit
+        futures::future::try_join_all((0..2).map(|_| rate_limiter.acquire()))
+            .await
+            .expect("Should acquire all permits");
+
+        // the next request should wait until free
+        tokio::select! {
+            _ = rate_limiter.acquire() => {
+                panic!("Expected rate limiter to block, but it did not.");
+            },
+            () = tokio::time::sleep(Duration::from_millis(400)) => {}
+        }
+
+        // next permit should occur after the next reset
+        tokio::select! {
+            permit = rate_limiter.acquire() => {
+                assert!(permit.is_ok(), "Failed to acquire permit after rate limit reset: {:?}", permit.err());
+                let permit = permit.expect("should be Ok");
+                assert!(permit.0.is_none(), "Semaphore permit should be None if semaphore is not configured");
+            },
+            () = tokio::time::sleep(Duration::from_millis(400)) => {
+                panic!("Expected to acquire a permit after rate limit reset, but timed out.");
+            }
+        }
+    }
+
+    #[tokio::test]
+    async fn test_rate_limiter_with_multiple_quotas() {
+        let rate_limiter = RateLimiterBuilder::new()
+            .with_quota(Quota::per_second(
+                // purposely set a high per-second limit which should not be hit
+                NonZeroU32::new(100).expect("NonZeroU32 should be non-zero"),
+            ))
+            .with_quota(Quota::per_minute(
+                // should result in per minute quota being hit
+                NonZeroU32::new(10).expect("NonZeroU32 should be non-zero"),
+            ))
+            .build();
+
+        // acquire all 10 permits at once, which should exhaust the per-minute rate limit
+        futures::future::try_join_all((0..10).map(|_| rate_limiter.acquire()))
+            .await
+            .expect("Should acquire all permits");
+
+        // the next request should wait until free
+        tokio::select! {
+            _ = rate_limiter.acquire() => {
+                panic!("Expected rate limiter to block, but it did not.");
+            },
+            // 10/minute is 1 every 6 seconds
+            () = tokio::time::sleep(Duration::from_secs(5)) => {}
+        }
+
+        // next permit should occur after the next reset
+        tokio::select! {
+            permit = rate_limiter.acquire() => {
+                assert!(permit.is_ok(), "Failed to acquire permit after rate limit reset: {:?}", permit.err());
+                let permit = permit.expect("should be Ok");
+                assert!(permit.0.is_none(), "Semaphore permit should be None if semaphore is not configured");
+            },
+            () = tokio::time::sleep(Duration::from_secs(1)) => {
+                panic!("Expected to acquire a permit after rate limit reset, but timed out.");
+            }
+        }
+    }
+
+    #[tokio::test]
+    async fn test_rate_limiter_hits_multiple_quotas() {
+        let rate_limiter = RateLimiterBuilder::new()
+            .with_quota(Quota::per_second(
+                // per-second will get hit first
+                NonZeroU32::new(4).expect("NonZeroU32 should be non-zero"),
+            ))
+            .with_quota(Quota::per_minute(
+                // then per-minute will get hit
+                NonZeroU32::new(6).expect("NonZeroU32 should be non-zero"),
+            ))
+            .build();
+
+        // acquire all 4 permits at once, which should exhaust the per-second rate limit
+        futures::future::try_join_all((0..4).map(|_| rate_limiter.acquire()))
+            .await
+            .expect("Should acquire all permits");
+
+        // the next request should wait until free
+        tokio::select! {
+            _ = rate_limiter.acquire() => {
+                panic!("Expected rate limiter to block, but it did not.");
+            },
+            () = tokio::time::sleep(Duration::from_millis(200)) => {}
+        }
+
+        // next permit should occur after the next reset
+        tokio::select! {
+            permit = rate_limiter.acquire() => {
+                assert!(permit.is_ok(), "Failed to acquire permit after rate limit reset: {:?}", permit.err());
+                let permit = permit.expect("should be Ok");
+                assert!(permit.0.is_none(), "Semaphore permit should be None if semaphore is not configured");
+            },
+            () = tokio::time::sleep(Duration::from_millis(200)) => {
+                panic!("Expected to acquire a permit after rate limit reset, but timed out.");
+            }
+        }
+
+        // now we've hit the per-minute limit
+        // the next request should wait until free
+        tokio::select! {
+            _ = rate_limiter.acquire() => {
+                panic!("Expected rate limiter to block, but it did not.");
+            },
+            // 6/minute is 1 every 10 seconds
+            () = tokio::time::sleep(Duration::from_secs(9)) => {}
+        }
+
+        // next permit should occur after the next reset
+        tokio::select! {
+            permit = rate_limiter.acquire() => {
+                assert!(permit.is_ok(), "Failed to acquire permit after rate limit reset: {:?}", permit.err());
+                let permit = permit.expect("should be Ok");
+                assert!(permit.0.is_none(), "Semaphore permit should be None if semaphore is not configured");
+            },
+            () = tokio::time::sleep(Duration::from_secs(2)) => {
+                panic!("Expected to acquire a permit after rate limit reset, but timed out.");
+            }
+        }
+    }
+}
