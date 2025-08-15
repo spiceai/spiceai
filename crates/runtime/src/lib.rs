c0706af82599be5ea23db8fe2614d3be9c2ba2a6
Viktor Yershov <krinart@gmail.com>
2025-08-14 17:24:08 -0700

    fix lint

--- crates/runtime/src/lib.rs
+++ crates/runtime/src/lib.rs
@@ -825,7 +825,7 @@ impl Runtime {
         let ctx = &self.datafusion().ctx;
         ctx.register_udtf(
             "list_udfs",
-            Arc::new(ListUDFTableFunc::new(Arc::clone(&ctx)))
+            Arc::new(ListUDFTableFunc::new(Arc::clone(&ctx))),
         );
 
         let components = vec![task_history, datasets, catalogs, models_and_evals];
