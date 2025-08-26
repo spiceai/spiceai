aa6b1e1780a8cce419109713faba7f6a8d4fa0cf
Viktor Yershov <viktor@spice.ai>
2025-08-26 12:07:08 -0700

    Properly handle debezium VariableScaleDecimal

--- crates/runtime/src/dataconnector/debezium.rs
+++ crates/runtime/src/dataconnector/debezium.rs
@@ -425,10 +425,18 @@ async fn get_metadata_from_kafka(
     };
 
     let Some(key) = msg.key() else {
+
+        let src = &msg.value().clone().payload.source;
+        let table_name = format!("{}.{}", src.db, src.table);
+
         return Err(super::DataConnectorError::UnableToGetReadProvider {
             dataconnector: "debezium".to_string(),
-            source: "CDC message key is missing. Verify Debezium CDC configuration and try again."
-                .into(),
+            source: format!(
+                "CDC message key is missing. \
+         Most likely, table \"{}\" doesn't have a configured primary key. \
+         Verify Debezium CDC configuration and try again.",
+                table_name
+            ).into(),
             connector_component: ConnectorComponent::from(dataset),
         });
     };
