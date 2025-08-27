e79bb6e43f64715ddd3eddcba870f2889aec2642
peasee <98815791+peasee@users.noreply.github.com>
2025-08-27 15:49:16 +1000

    docs: Re-add ignore comments

--- crates/runtime/tests/models/search.rs
+++ crates/runtime/tests/models/search.rs
@@ -332,7 +332,7 @@ pub(crate) async fn run_search(
 }
 
 #[tokio::test]
-#[ignore]
+#[ignore] // https://github.com/spiceai/spiceai/issues/6815
 async fn test_multi_column_search() -> Result<(), anyhow::Error> {
     let mut ds = catalog_page_tpcds_dataset_w_embeddings(
         "multi_column_search",
@@ -988,7 +988,7 @@ async fn test_text_search_multiple_columns() -> Result<(), anyhow::Error> {
 
 #[cfg(feature = "flightsql")]
 #[tokio::test]
-#[ignore]
+#[ignore] // https://github.com/spiceai/spiceai/issues/6816
 async fn test_multi_column_w_existing_embedding() -> Result<(), anyhow::Error> {
     use spicepod::{acceleration::Acceleration, param::Params};
 
