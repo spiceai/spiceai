2a104cd38eeca301187eb1bc15c9ec5d0a05163a
jeadie <jack@spice.ai>
2025-09-02 14:58:37 -0700

    clppy

--- crates/search/src/generation/text_search/index.rs
+++ crates/search/src/generation/text_search/index.rs
@@ -125,7 +125,7 @@ impl FullTextDatabaseIndex {
         primary_key_override: Option<Vec<String>>,
     ) -> Result<Vec<String>, super::Error> {
         // Use 'primary_key_override', fallback to underlying in table.
-        let pks = match (primary_key_override, get_primary_keys(&inner).await) {
+        let pks = match (primary_key_override, get_primary_keys(inner).await) {
             (Some(pks), _) => pks,
             (None, Ok(pks)) => {
                 if pks.is_empty() {
