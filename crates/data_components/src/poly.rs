66c56fb89255d501f3fb786980260d8203b64d03
Viktor Yershov <viktor@spice.ai>
2025-09-08 14:41:35 -0700

    Fix lint

--- crates/data_components/src/poly.rs
+++ crates/data_components/src/poly.rs
@@ -66,6 +66,7 @@ impl PolyTableProvider {
         adaptor.map(|f| Arc::clone(&f.source))
     }
 
+    #[must_use]
     pub fn get_federated_table_provider(&self) -> Arc<dyn TableProvider> {
         Arc::clone(&self.fed)
     }
