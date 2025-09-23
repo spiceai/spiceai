939f36db1b8baae2e44e0ff50faa14e00deca9bf
David Stancu <david@spice.ai>
2025-09-23 12:08:12 -0400

    include model name in embeddinginput cache key computation (#7275)

--- crates/llms/src/model2vec.rs
+++ crates/llms/src/model2vec.rs
@@ -124,23 +124,32 @@ impl Embed for Model2Vec {
         self.cache.as_ref().map(Arc::clone)
     }
 
+    fn model_name(&self) -> Option<&str> {
+        Some(self.name.as_str())
+    }
+
     async fn embed(
         &self,
         input: EmbeddingInput,
     ) -> Result<Vec<Vec<f32>>, super::embeddings::Error> {
-        if let Some(CachedEmbeddingResult::Vector(cached)) =
-            self.get_cached_embed((&input).into()).await
-        {
+        let cache_key = self.embedding_input_cache_key(&input);
+
+        let cached_response = if let Some(key) = cache_key {
+            self.get_cached_embed(key).await
+        } else {
+            None
+        };
+
+        if let Some(CachedEmbeddingResult::Vector(cached)) = cached_response {
             return Ok(cached);
         }
 
         let vectors = self.embed_sync(input.clone())?;
 
-        self.put_cached_embed(
-            (&input).into(),
-            CachedEmbeddingResult::Vector(vectors.clone()),
-        )
-        .await;
+        if let Some(key) = cache_key {
+            self.put_cached_embed(key, CachedEmbeddingResult::Vector(vectors.clone()))
+                .await;
+        }
 
         Ok(vectors)
     }
