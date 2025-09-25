232e6e3740c265871d66d701b0dd862d9a999ad0
Phillip LeBlanc <phillip@leblanc.tech>
2025-09-25 11:36:06 +0900

    fix

--- crates/data_components/src/s3_vectors/mod.rs
+++ crates/data_components/src/s3_vectors/mod.rs
@@ -14,7 +14,6 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 use arrow::error::ArrowError;
-use opendal::services::S3;
 use s3_vectors::{
     BuildError, CreateIndexError, CreateVectorBucketError, DistanceMetric, Document, GetIndexError,
     GetVectorBucketError, PutVectorsError, QueryVectorsError,
