00433d54b3594a27cbb3a297e7bb5c2fa28194f7
Phillip LeBlanc <phillip@leblanc.tech>
2025-09-25 11:36:33 +0900

    fmt

--- crates/data_components/src/s3_vectors/mod.rs
+++ crates/data_components/src/s3_vectors/mod.rs
@@ -13,6 +13,7 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 */
+
 use arrow::error::ArrowError;
 use s3_vectors::{
     BuildError, CreateIndexError, CreateVectorBucketError, DistanceMetric, Document, GetIndexError,
@@ -20,6 +21,7 @@ use s3_vectors::{
 };
 use s3_vectors_metadata_filter::MetadataFilter;
 use snafu::Snafu;
+use std::fmt::{Display, Formatter};
 
 pub mod list_provider;
 pub mod put_vectors_sink;
