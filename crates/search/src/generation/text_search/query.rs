f5ed04ea63d84ae68b5802b1a9cf8352416bd927
jeadie <jack@spice.ai>
2025-09-29 10:31:01 +1000

    enforce as minimum

--- crates/search/src/generation/text_search/query.rs
+++ crates/search/src/generation/text_search/query.rs
@@ -16,7 +16,10 @@ use std::{any::Any, cmp::min, sync::Arc};
 use crate::{
     SEARCH_SCORE_COLUMN_NAME,
     generation::{
-        text_search::{DEFAULT_BATCH_SIZE, FullTextSearchFieldIndex, exec::FullTextSearchExec},
+        text_search::{
+            DEFAULT_BATCH_SIZE, DEFAULT_LIMIT_MAXIMUM, FullTextSearchFieldIndex,
+            exec::FullTextSearchExec,
+        },
         util::append_fields,
     },
 };
@@ -96,7 +99,9 @@ impl TableProvider for FullTextSearchQuery {
                 self.schema(),
                 projection,
                 filters.to_vec(),
-                self.limit_to_use(limit).unwrap_or(DEFAULT_LIMIT_MAXIMUM),
+                self.limit_to_use(limit)
+                    .unwrap_or(DEFAULT_LIMIT_MAXIMUM)
+                    .min(DEFAULT_LIMIT_MAXIMUM),
             )
             .map_err(|e| DataFusionError::ArrowError(Box::new(e), None))?,
         ))
