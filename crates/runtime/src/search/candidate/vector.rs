aeb7e9efa6de476c3c5739a0c2dd2a29b144f736
Jack Eadie <jack@spice.ai>
2025-09-29 20:30:20 -0700

    Update vector.rs

--- crates/runtime/src/search/candidate/vector.rs
+++ crates/runtime/src/search/candidate/vector.rs
@@ -1,4 +1,3 @@
-use crate::request::{AsyncMarker, RequestContext};
 /*
 Copyright 2024-2025 The Spice.ai OSS Authors
 
@@ -14,6 +13,7 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 */
+use crate::request::{AsyncMarker, RequestContext};
 use crate::search::Error as VectorSearchError;
 use crate::{embedding_col, offset_col};
 use async_openai::types::EmbeddingInput;
