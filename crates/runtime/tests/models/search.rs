4ad1b0d83d9b65a5d70bb9ca9590b813d095a294
peasee <98815791+peasee@users.noreply.github.com>
2025-10-08 18:11:53 +1000

    fix: Update snapshot

--- crates/runtime/tests/models/search.rs
+++ crates/runtime/tests/models/search.rs
@@ -14,7 +14,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-use crate::models::hf::get_model_to_vec_embeddings;
+use crate::models::hf::{get_huggingface_embeddings, get_model_to_vec_embeddings};
 use crate::models::openai::get_openai_embeddings;
 use crate::models::{create_api_bindings_config, get_mega_science_dataset, http_post};
 use crate::utils::{runtime_ready_check, test_request_context};
