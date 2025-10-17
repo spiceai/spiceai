00589d3d1a68732dae91cc40bf25659130accdd7
David Stancu <david@spice.ai>
2025-10-17 15:36:30 -0400

    cluster feature + dependencies app -> serializable ballista -> tracing

--- crates/app/src/lib.rs
+++ crates/app/src/lib.rs
@@ -19,6 +19,7 @@ limitations under the License.
 use std::{collections::HashMap, path::PathBuf, sync::Arc};
 
 use snafu::prelude::*;
+use serde::{Deserialize, Serialize};
 pub use spicepod;
 use spicepod::{
     Spicepod,
@@ -43,7 +44,7 @@ use util::in_tracing_context;
 
 pub mod runtime;
 
-#[derive(Debug, PartialEq, Clone)]
+#[derive(Debug, PartialEq, Clone, Serialize, Deserialize)]
 pub struct App {
     pub name: String,
 
