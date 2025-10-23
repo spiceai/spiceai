eac749a9164080ade8a0054630da2bc0651d5a48
David Stancu <david@spice.ai>
2025-10-23 14:24:18 -0400

    lint

--- crates/runtime/src/accelerated_table/refresh_task.rs
+++ crates/runtime/src/accelerated_table/refresh_task.rs
@@ -80,7 +80,7 @@ use datafusion::{
 };
 use datafusion_expr::{LogicalPlanBuilder, UNNAMED_TABLE, ident};
 use datafusion_federation::{FederatedPlanner, FederatedTableProviderAdaptor};
-use runtime_request_context::{AsyncMarker, Protocol, RequestContext};
+use runtime_request_context::{AsyncMarker, RequestContext};
 use spicepod::metric::Metrics;
 use std::collections::HashSet;
 
