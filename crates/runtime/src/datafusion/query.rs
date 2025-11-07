cc5bae70543464873548d23779d579ded7a205b4
David Stancu <david@spice.ai>
2025-11-07 15:06:59 -0500

    ssb cluster only

--- crates/runtime/src/datafusion/query.rs
+++ crates/runtime/src/datafusion/query.rs
@@ -14,6 +14,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
+#[cfg(feature = "cluster")]
 use datafusion::execution::SessionStateBuilder;
 use std::{fmt::Display, sync::Arc};
 
