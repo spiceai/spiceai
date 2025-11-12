e0b274222404015c929300d6ea13726c69bc5cac
jeadie <jack@spice.ai>
2025-11-13 08:51:28 +1000

    clppy

--- crates/data_components/src/turso.rs
+++ crates/data_components/src/turso.rs
@@ -1309,7 +1309,9 @@ impl SQLExecutor for TursoTableProvider {
         // Default to not federate if [`Self::function_support`] provided, otherwise true.
         self.function_support
             .as_ref()
-            .map(|func_supp| !contains_unsupported_functions(plan, func_supp).unwrap_or(false))
+            .map_or(true, |func_supp| {
+                !contains_unsupported_functions(plan, func_supp).unwrap_or(false)
+            })
             .unwrap_or(true)
     }
 
