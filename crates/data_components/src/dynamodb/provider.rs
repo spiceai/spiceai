6a06867405380076b6f1d3a03e03ae323a1b6d2e
Viktor Yershov <viktor@spice.ai>
2025-11-13 12:17:09 -0800

    Lint

--- crates/data_components/src/dynamodb/provider.rs
+++ crates/data_components/src/dynamodb/provider.rs
@@ -233,7 +233,6 @@ impl DynamoDBTableProvider {
         }
     }
 
-    #[must_use] 
     pub fn changes_stream_from_latest(&self) -> ChangesStream {
         let record_schema = Arc::clone(self.table_schema.schema());
         let changes_schema = changes_schema(&record_schema).clone();
