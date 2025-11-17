ea1b38c6b2dcddea2be6fe726c9f1b2f1361c0b6
Viktor Yershov <viktor@spice.ai>
2025-11-17 12:06:51 -0800

    Add DynamoDB override

--- tools/testoperator/src/args/dataset.rs
+++ tools/testoperator/src/args/dataset.rs
@@ -103,6 +103,8 @@ pub enum QueryOverridesArg {
     DatabricksCatalog,
     #[serde(rename = "spicecloud")]
     Spicecloud,
+    #[serde(rename = "dynamodb")]
+    DynamoDB,
 }
 
 impl From<QuerySetArg> for QuerySet {
@@ -167,6 +169,7 @@ impl From<QueryOverridesArg> for QueryOverrides {
             QueryOverridesArg::Spicecloud => QueryOverrides::Spicecloud,
             QueryOverridesArg::GlueCatalog => QueryOverrides::GlueCatalog,
             QueryOverridesArg::IcebergHadoop => QueryOverrides::IcebergHadoop,
+            QueryOverridesArg::DynamoDB => QueryOverrides::DynamoDB,
         }
     }
 }
