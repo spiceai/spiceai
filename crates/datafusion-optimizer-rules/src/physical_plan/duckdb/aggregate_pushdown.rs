f1d9d3b81d22c26df29431c98b323cd474e7027a
David Stancu <david@spice.ai>
2025-11-17 15:06:56 -0500

    wip / basic e2e rewrite

--- crates/datafusion-optimizer-rules/src/physical_plan/duckdb/aggregate_pushdown.rs
+++ crates/datafusion-optimizer-rules/src/physical_plan/duckdb/aggregate_pushdown.rs
@@ -1,7 +1,7 @@
 use crate::common::plan_node_key::PlanNodeKey;
 use crate::common::search_visitor::SearchVisitor;
 use crate::concrete;
-use crate::physical_plan::duckdb::ConcreteDuckSqlExec;
+use crate::physical_plan::duckdb::{ConcreteDuckSqlExec, PARSER_DIALECT};
 use datafusion::common::{plan_err, DataFusionError, Result};
 use datafusion::config::ConfigOptions;
 use datafusion::logical_expr::sqlparser::ast::{visit_relations, Expr, Function, ObjectName, SetExpr, Statement};
@@ -9,12 +9,15 @@ use datafusion::physical_expr::{expressions, ScalarFunctionExpr};
 use datafusion::physical_optimizer::PhysicalOptimizerRule;
 use datafusion::physical_plan::aggregates::{AggregateExec, AggregateMode};
 use datafusion::physical_plan::{ExecutionPlan, PhysicalExpr};
-use datafusion::sql::sqlparser::ast::{FunctionArg, FunctionArgExpr, FunctionArgumentList, FunctionArguments, Ident, SelectItem};
+use datafusion::sql::sqlparser::ast::{FunctionArg, FunctionArgExpr, FunctionArgumentList, FunctionArguments, GroupByExpr, Ident, SelectItem};
 use datafusion::sql::unparser;
 use datafusion::sql::unparser::Unparser;
 use std::collections::HashMap;
 use std::ops::ControlFlow;
 use std::sync::Arc;
+use datafusion::common::tree_node::{Transformed, TreeNode};
+use datafusion::physical_expr::expressions::Column;
+use datafusion::sql::sqlparser::parser::Parser;
 use datafusion_expr::{col, lit, Literal};
 
 #[derive(Debug)]
@@ -63,12 +66,26 @@ impl DuckDBAggregatePushdownOptimizer {
             .map(|e| Self::physical_expr_to_sql_expr(&unparser, e))
             .collect::<Result<Vec<_>>>()?;
 
+        let ast_gby_exprs = physical_aggregate
+            .group_expr()
+            .expr()
+            .iter()
+            .filter_map(|(exp, alias)| {
+                if let Some(column) = concrete!(exp, Column) {
+                    Some(col(column.name()).alias(alias))
+                } else {
+                    None
+                }
+            })
+            .map(|logical| unparser.expr_to_sql(&logical))
+            .collect::<Result<Vec<_>>>()?;
+
         let mut new_select = select.clone();
         new_select.projection.extend(ast_project_exprs);
-        // new_select.group_by
+        new_select.group_by = GroupByExpr::Expressions(ast_gby_exprs, vec![]);
+
         let mut new_query = query.clone();
 
-        println!("{:#?}", new_select);
         new_query.body = Box::new(SetExpr::Select(new_select));
 
         Ok(Some(Statement::Query(new_query)))
@@ -159,7 +176,7 @@ impl DuckDBAggregatePushdownOptimizer {
         }
     }
 
-    fn process_agg_tree(plan: &Arc<dyn ExecutionPlan>) -> Result<Option<Arc<dyn ExecutionPlan>>> {
+    fn process_agg_tree(plan: &Arc<dyn ExecutionPlan>) -> Result<Option<HashMap<PlanNodeKey, Option<Arc<dyn ExecutionPlan>>>>> {
         let Some(agg) = concrete!(plan, AggregateExec) else {
             return Ok(None)
         };
@@ -186,11 +203,28 @@ impl DuckDBAggregatePushdownOptimizer {
             DataFusionError::Execution(format!("Unable to generate DuckDB SQL: {e}"))
         })?;
 
-        // let Some(statement) = Parser::parse_sql(&DIALECT, sql.as_str())?.first().cloned() else {
-        //     return Ok(plan);
-        // };
+        let Some(statement) = Parser::parse_sql(&PARSER_DIALECT, sql.as_str())?.first().cloned() else {
+            return Ok(None);
+        };
 
-        todo!()
+        let Some(rewritten) = Self::rewrite_statement(&statement, agg)? else {
+            return Ok(None);
+        };
+
+        // Where None = delete
+        let mut rewrites: HashMap<PlanNodeKey, Option<Arc<dyn ExecutionPlan>>> = HashMap::new();
+        for node in &flat {
+            if concrete!(node, AggregateExec).is_some() {
+                rewrites.insert(node.as_ref().into(), None);
+            } else if concrete!(node, ConcreteDuckSqlExec).is_some() {
+                let new_duck_exec = duck_exec.clone().with_optimized_sql(
+                    format!("{rewritten}")
+                );
+                rewrites.insert(node.as_ref().into(), Some(Arc::new(new_duck_exec)));
+            }
+        }
+
+        Ok(Some(rewrites))
     }
 }
 
@@ -207,7 +241,31 @@ impl PhysicalOptimizerRule for DuckDBAggregatePushdownOptimizer {
             return Ok(plan);
         }
 
-        todo!()
+        let all_rewrites = plan_aggs
+            .iter()
+            .map(Self::process_agg_tree)
+            .collect::<Result<Vec<_>>>()?
+            .into_iter()
+            .flatten()
+            .flat_map(|m| m.into_iter())
+            .collect::<HashMap<_, _>>();
+
+        let transformed = plan.transform_down(|p| {
+            match all_rewrites.get(&p.as_ref().into()) {
+                Some(None) => {
+                    match p.children().first() {
+                        Some(child) => Ok(Transformed::yes(Arc::clone(child))),
+                        _ => Ok(Transformed::no(p))
+                    }
+                },
+                Some(Some(new)) => {
+                    Ok(Transformed::yes(Arc::clone(new)))
+                },
+                None => Ok(Transformed::no(p))
+            }
+        })?;
+
+        Ok(transformed.data)
     }
 
     fn name(&self) -> &str {
