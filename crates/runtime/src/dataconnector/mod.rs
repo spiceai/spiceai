e31fad9611ca5bdf7bbe45a264e618595f9c8695
Viktor Yershov <viktor@spice.ai>
2025-11-18 15:49:12 -0800

    Add tracing to refresh_task

--- crates/runtime/src/dataconnector/mod.rs
+++ crates/runtime/src/dataconnector/mod.rs
@@ -599,6 +599,16 @@ pub async fn get_data(
         df = df.filter(filter).map_err(find_datafusion_root)?;
     }
 
+    match df.clone().explain(false, false) {
+        Ok(explained) => match explained.to_string().await {
+            Ok(explained) => {
+                tracing::debug!("get_data df.explain for {}: \n{}", table_name, explained);
+            }
+            _ => {}
+        },
+        _ => {}
+    };
+
     let sql = Unparser::default()
         .plan_to_sql(df.logical_plan())
         .map_err(find_datafusion_root)?;
