3dddacc667ab3565ec748ef69ff2352ec7b6f207
Viktor Yershov <viktor@spice.ai>
2025-12-09 10:13:16 -0800

    Lint

--- crates/dynamodb-streams/src/stream.rs
+++ crates/dynamodb-streams/src/stream.rs
@@ -196,7 +196,11 @@ impl DynamodbStreamProducer {
 
                         if is_batch_empty {
                             // To avoid throttling - wait at least 500ms before polling again
-                            sleep(DEFAULT_SLEEP_DURATION.max(self.interval.unwrap_or(Duration::from_secs(0)))).await;
+                            sleep(
+                                DEFAULT_SLEEP_DURATION
+                                    .max(self.interval.unwrap_or(Duration::from_secs(0))),
+                            )
+                            .await;
                         } else if let Some(duration) = self.interval {
                             sleep(duration).await;
                         }
