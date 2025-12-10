85079ea743c5b1d786b8d1e7a6bb5a67ce3bca9d
peasee <98815791+peasee@users.noreply.github.com>
2025-12-10 15:05:06 +1000

    chore: Clippy

--- crates/cache/src/key.rs
+++ crates/cache/src/key.rs
@@ -196,15 +196,17 @@ mod tests {
     #[test]
     fn test_passthrough_hasher() {
         // validate that `write_u64` and `write` produce the same hash result from a u64 input
-        let mut hasher1 = PassthroughHashBuilder::new(RandomState::default()).build_hasher();
-        hasher1.write_u64(42);
-        let hash1 = hasher1.finish();
+        let hash1 = PassthroughHashBuilder::new(RandomState::default()).hash_one(42);
         assert_eq!(hash1, 42);
 
-        let mut hasher2 = PassthroughHashBuilder::new(RandomState::default()).build_hasher();
-        42.hash(&mut hasher2);
-        let hash2 = hasher2.finish();
+        // explicitly allow this rule, because we're validating that the builtin u64 hash -> .write_u64() path works as expected
+        #[expect(clippy::manual_hash_one)]
+        {
+            let mut hasher2 = PassthroughHashBuilder::new(RandomState::default()).build_hasher();
+            42.hash(&mut hasher2);
+            let hash2 = hasher2.finish();
 
-        assert_eq!(hash1, hash2);
+            assert_eq!(hash1, hash2);
+        }
     }
 }
