7bc9d735e09897a08b96e7f18c6470a45b28f0ca
Viktor Yershov <viktor@spice.ai>
2025-12-09 19:00:47 -0800

    Fix

--- crates/dynamodb-streams/src/lib.rs
+++ crates/dynamodb-streams/src/lib.rs
@@ -46,6 +46,7 @@ pub enum Error {
     #[snafu(display(
         "DynamoDB Streams are not enabled on this table. Enable DynamoDB Streams and retry: https://docs.aws.amazon.com/amazondynamodb/latest/developerguide/Streams.html#Streams.Enabling"
     ))]
+    StreamNotFound,
 
     #[snafu(display("Stream description not found: {stream_arn}"))]
     StreamDescriptionNotFound { stream_arn: String },
