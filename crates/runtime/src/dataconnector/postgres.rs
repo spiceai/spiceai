49dba238cd217c518f06ea6522e1a26963a384e8
Evgenii Khramkov <evgenii@spice.ai>
2025-12-14 10:26:59 +0900

    revert postgres ssl param changes

--- crates/runtime/src/dataconnector/postgres.rs
+++ crates/runtime/src/dataconnector/postgres.rs
@@ -76,16 +76,7 @@ const PARAMETERS: &[ParameterSpec] = &[
     ParameterSpec::component("host"),
     ParameterSpec::component("port"),
     ParameterSpec::component("db"),
-    ParameterSpec::component("sslmode")
-        .description("SSL connection mode.")
-        .one_of(&[
-            "disable",
-            "allow",
-            "prefer",
-            "require",
-            "verify-ca",
-            "verify-full",
-        ]),
+    ParameterSpec::component("sslmode"),
     ParameterSpec::component("sslrootcert"),
     ParameterSpec::component("connection_pool_min_idle")
         .description("The minimum number of idle connections to keep open in the pool.")
