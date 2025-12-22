35fb8842251f626d96bb47b33278572daf29baa3
Sergei Grebnov <sergei.grebnov@gmail.com>
2025-12-22 13:51:08 +0300

    Revert "Restore deprecated open-telemetry flag in spiced (#8629)"
    This reverts commit 95bc6c0da70ec74cedeae3bce6f174c1b113ee2e.

--- bin/spiced/src/main.rs
+++ bin/spiced/src/main.rs
@@ -14,8 +14,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-use clap::parser::ValueSource;
-use clap::{CommandFactory, FromArgMatches};
+use clap::Parser;
 use opentelemetry::global;
 use rustls::crypto::{self, CryptoProvider};
 use telemetry::noop::NoopMeterProvider;
@@ -57,11 +56,7 @@ const fn get_allocator_name() -> Option<&'static str> {
 }
 
 fn main() {
-    let matches = spiced::Args::command().get_matches();
-    let open_telemetry_deprecated =
-        matches.value_source("open_telemetry_bind_address") == Some(ValueSource::CommandLine);
-    let mut args = spiced::Args::from_arg_matches(&matches).unwrap_or_else(|err| err.exit());
-    args.open_telemetry_deprecated = open_telemetry_deprecated;
+    let args = spiced::Args::parse();
 
     if args.version {
         println!("{}", get_version_string());
