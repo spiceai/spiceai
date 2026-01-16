2c6d83d7ca28ecf3970527c2054354082dec64a8
Luke Kim <80174+lukekim@users.noreply.github.com>
2026-01-15 21:39:26 -0800

    Updates

--- crates/runtime/src/datafusion/mod.rs
+++ crates/runtime/src/datafusion/mod.rs
@@ -2273,6 +2273,7 @@ mod tests {
         assert_eq!(cache_provider.item_count().await, 1);
     }
 
+    #[cfg(feature = "duckdb")]
     mod build_snapshot_creation_config_tests {
         use super::*;
         use crate::component::dataset::Dataset;
