236d74c58fc3f66859f30c94738648f1f0b5cad7
jeadie <jack@spice.ai>
2026-01-20 13:46:36 +1000

    fix formula_1 and codebase_community in bird-bench

--- tools/testoperator/src/args/dispatch.rs
+++ tools/testoperator/src/args/dispatch.rs
@@ -315,16 +315,16 @@ pub enum BenchmarkQueryset {
     BirdBenchSmallCaliforniaSchools,
     #[serde(rename = "bird-bench-small[card_games]")]
     BirdBenchSmallCardGames,
-    #[serde(rename = "bird-bench-small[community]")]
-    BirdBenchSmallCommunity,
+    #[serde(rename = "bird-bench-small[codebase_community]")]
+    BirdBenchSmallCodebaseCommunity,
     #[serde(rename = "bird-bench-small[debit_card_specializing]")]
     BirdBenchSmallDebitCardSpecializing,
     #[serde(rename = "bird-bench-small[european_football2]")]
     BirdBenchSmallEuropeanFootball2,
     #[serde(rename = "bird-bench-small[financial]")]
     BirdBenchSmallFinancial,
-    #[serde(rename = "bird-bench-small[formula1]")]
-    BirdBenchSmallFormula1,
+    #[serde(rename = "bird-bench-small[formula_1]")]
+    BirdBenchSmallformula_1,
     #[serde(rename = "bird-bench-small[superhero]")]
     BirdBenchSmallSuperhero,
     #[serde(rename = "bird-bench-small[thrombosis_prediction]")]
