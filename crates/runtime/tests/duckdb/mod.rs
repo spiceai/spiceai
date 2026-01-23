e89f4bc8718eb58be25518471667d4b62d0cdab8
Sergei Grebnov <sergei.grebnov@gmail.com>
2026-01-23 14:47:16 +0300

    Trigger CI

--- crates/runtime/tests/duckdb/mod.rs
+++ crates/runtime/tests/duckdb/mod.rs
@@ -13,7 +13,7 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 */
- 
+
 use std::sync::Arc;
 
 use crate::configure_test_datafusion;
