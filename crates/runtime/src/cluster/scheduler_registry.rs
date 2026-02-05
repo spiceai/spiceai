3acdd7837cb24e34130ac2c5e15218d3a39ba812
peasee <98815791+peasee@users.noreply.github.com>
2026-02-05 14:18:18 +1000

    chore: build

--- crates/runtime/src/cluster/scheduler_registry.rs
+++ crates/runtime/src/cluster/scheduler_registry.rs
@@ -154,7 +154,8 @@ pub async fn start_scheduler_registry(
 
     let job_executor = crate::jobs::JobExecutor::new(Arc::clone(&job_store), rt.datafusion());
     rt.set_job_executor(Arc::new(job_executor)).await;
-    rt.create_job_store_cleanup_schedule(&job_store)
+    Arc::clone(&rt)
+        .create_job_store_cleanup_schedule(&job_store)
         .await
         .context(FailedToStartJobStoreCleanupTaskSnafu)?;
     tracing::info!(
