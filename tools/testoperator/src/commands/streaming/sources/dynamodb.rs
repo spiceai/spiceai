e628780606d040824b8439549f8288a426c8c48e
Viktor Yershov <viktor@spice.ai>
2026-02-12 11:32:32 -0800

    Fix

--- tools/testoperator/src/commands/streaming/sources/dynamodb.rs
+++ tools/testoperator/src/commands/streaming/sources/dynamodb.rs
@@ -1292,26 +1292,20 @@ fn transform_spicepod(
     );
 
     let mut params = Params::default();
-    if let Some(ref key) = snapshot_config.access_key_id {
-        params
-            .data
-            .insert("s3_auth".to_string(), ParamValue::String("key".to_string()));
-        params.data.insert(
-            "s3_key".to_string(),
-            ParamValue::String("${secrets:DYNAMODB_AWS_ACCESS_KEY_ID}".to_string()),
-        );
-    }
-    if let Some(ref secret) = snapshot_config.secret_access_key {
-        params.data.insert(
-            "s3_secret".to_string(),
-            ParamValue::String("${secrets:SNAPSHOT_S3_SECRET_ACCESS_KEY}".to_string()),
-        );
-    }
-    if let Some(ref region) = snapshot_config.region {
-        params
-            .data
-            .insert("s3_region".to_string(), ParamValue::String(region.clone()));
-    }
+    params
+        .data
+        .insert("s3_auth".to_string(), ParamValue::String("key".to_string()));
+    params.data.insert(
+        "s3_key".to_string(),
+        ParamValue::String("${secrets:DYNAMODB_AWS_ACCESS_KEY_ID}".to_string()),
+    );
+    params.data.insert(
+        "s3_secret".to_string(),
+        ParamValue::String("${secrets:SNAPSHOT_S3_SECRET_ACCESS_KEY}".to_string()),
+    );
+    params
+        .data
+        .insert("s3_region".to_string(), ParamValue::String(region.clone()));
 
     spicepod.snapshots = Some(Snapshots {
         enabled: true,
