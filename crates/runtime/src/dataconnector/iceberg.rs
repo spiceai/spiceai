2302959013051eb6fccb756c7d2ff7f5be44bb2d
ewgenius <hey@ewgenius.me>
2026-03-30 10:06:32 +0900

    fix formatting

--- crates/runtime/src/dataconnector/iceberg.rs
+++ crates/runtime/src/dataconnector/iceberg.rs
@@ -147,55 +147,54 @@ impl IcebergDataConnector {
             }
         }
 
-        let storage_factory: Option<Arc<dyn StorageFactory>> = if let Some(endpoint) =
-            props.get("s3.endpoint")
-        {
-            verify_s3_endpoint(endpoint)
+        let storage_factory: Option<Arc<dyn StorageFactory>> =
+            if let Some(endpoint) = props.get("s3.endpoint") {
+                verify_s3_endpoint(endpoint)
+                    .await
+                    .map_err(|e| Error::InvalidConfiguration {
+                        dataconnector: "iceberg".into(),
+                        message: e.to_string(),
+                        connector_component: ConnectorComponent::from(dataset),
+                        source: Box::new(e),
+                    })?;
+
+                let aws_sdk_config = initiate_config_with_credentials(
+                    "IcebergDataConnector",
+                    "s3_region",
+                    "s3_access_key_id",
+                    "s3_secret_access_key",
+                    "s3_session_token",
+                    &self.params,
+                    self.params.get("s3_iam_role_source").expose().ok(),
+                )
                 .await
                 .map_err(|e| Error::InvalidConfiguration {
                     dataconnector: "iceberg".into(),
                     message: e.to_string(),
                     connector_component: ConnectorComponent::from(dataset),
                     source: Box::new(e),
-                })?;
-
-            let aws_sdk_config = initiate_config_with_credentials(
-                "IcebergDataConnector",
-                "s3_region",
-                "s3_access_key_id",
-                "s3_secret_access_key",
-                "s3_session_token",
-                &self.params,
-                self.params.get("s3_iam_role_source").expose().ok(),
-            )
-            .await
-            .map_err(|e| Error::InvalidConfiguration {
-                dataconnector: "iceberg".into(),
-                message: e.to_string(),
-                connector_component: ConnectorComponent::from(dataset),
-                source: Box::new(e),
-            })?
-            .load()
-            .await;
-
-            let custom_loader = S3CredentialProvider::from_config(&aws_sdk_config)
-                .map_err(|e| Error::InvalidConfiguration {
-                    dataconnector: "iceberg".into(),
-                    message: e.to_string(),
-                    connector_component: ConnectorComponent::from(dataset),
-                    source: Box::new(e),
                 })?
-                .into_custom_loader();
+                .load()
+                .await;
 
-            let configured_scheme = s3_scheme_from_url(&source);
+                let custom_loader = S3CredentialProvider::from_config(&aws_sdk_config)
+                    .map_err(|e| Error::InvalidConfiguration {
+                        dataconnector: "iceberg".into(),
+                        message: e.to_string(),
+                        connector_component: ConnectorComponent::from(dataset),
+                        source: Box::new(e),
+                    })?
+                    .into_custom_loader();
 
-            Some(Arc::new(OpenDalStorageFactory::S3 {
-                configured_scheme,
-                customized_credential_load: Some(custom_loader),
-            }) as Arc<dyn StorageFactory>)
-        } else {
-            None
-        };
+                let configured_scheme = s3_scheme_from_url(&source);
+
+                Some(Arc::new(OpenDalStorageFactory::S3 {
+                    configured_scheme,
+                    customized_credential_load: Some(custom_loader),
+                }) as Arc<dyn StorageFactory>)
+            } else {
+                None
+            };
 
         if source.starts_with("file://")
             || source.starts_with("s3://")
