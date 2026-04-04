acaa9a2f02316b4595bf81b71c9e2b12cd5c8fc2
Viktor Yershov <viktor@spice.ai>
2026-04-03 19:01:56 -0700

    Lint

--- crates/spicepod/src/component/runtime.rs
+++ crates/spicepod/src/component/runtime.rs
@@ -605,6 +605,7 @@ impl ApiKey {
         }
     }
 
+    #[must_use] 
     pub fn as_secret(&self) -> SecretString {
         match self {
             ApiKey::ReadOnly { key } | ApiKey::ReadWrite { key } => key.clone().into(),
