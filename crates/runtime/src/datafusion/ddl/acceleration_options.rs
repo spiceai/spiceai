75d4471c163bb8a28e70c0246e4816159ec1b2a1
Luke Kim <80174+lukekim@users.noreply.github.com>
2026-04-13 21:04:10 -0700

    feat: Add acceleration_options module with shared store functionality

---
+++ crates/runtime/src/datafusion/ddl/acceleration_options.rs
@@ -0,0 +1,26 @@
+/*
+Copyright 2026, Spice AI, Inc.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+     https://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+pub use datafusion_ddl::{
+    CreateTableStatementExtension, SharedDdlExtensionStore, parse_ddl_table_options,
+};
+
+use super::super::{SPICE_DEFAULT_CATALOG, SPICE_DEFAULT_SCHEMA};
+
+#[cfg(test)]
+pub fn new_shared_store() -> SharedDdlExtensionStore {
+    datafusion_ddl::new_shared_store(SPICE_DEFAULT_CATALOG, SPICE_DEFAULT_SCHEMA)
+}
