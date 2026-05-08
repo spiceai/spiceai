fe4b35599c0e94050ea12b9f24f19763afdfd835
ewgenius <hey@ewgenius.me>
2026-05-08 16:42:52 +0900

    fix: remove unnecessary Arc::new wrapping of RefreshTask

--- crates/runtime/src/accelerated_table/refresh.rs
+++ crates/runtime/src/accelerated_table/refresh.rs
@@ -1216,7 +1216,7 @@ impl Refresher {
 
         let notifier = self.on_complete_notification.clone();
         tokio::spawn(async move {
-            let refresh_task = Arc::new(refresh_task_builder.build().await);
+            let refresh_task = refresh_task_builder.build().await;
             if let Err(err) = refresh_task
                 .start_changes_stream(
                     refresh,
