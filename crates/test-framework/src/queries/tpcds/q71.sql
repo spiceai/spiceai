254bbb8dedb81744f33dc7fd9d5a1c6917813549
Viktor Yershov <viktor@spice.ai>
2026-08-20 11:25:23 -0700

    Remove limit clause from q71.sql
    Removed the limit clause to allow for all results.

--- crates/test-framework/src/queries/tpcds/q71.sql
+++ crates/test-framework/src/queries/tpcds/q71.sql
@@ -35,4 +35,4 @@ select i_brand_id brand_id, i_brand brand,t_hour,t_minute,
  group by i_brand, i_brand_id,t_hour,t_minute
  order by ext_price desc, i_brand_id,
       t_hour desc, t_minute desc -- added to make results deterministic
- limit 10;
+ ;
